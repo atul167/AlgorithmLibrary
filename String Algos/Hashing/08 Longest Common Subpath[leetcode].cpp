6cee8453c88ec7086ae4e6dfcfb60c82d3e40614
Ashwani Yadav <ashyadavash@gmail.com>
2021-07-19 00:25:56 +0530

    Update 08 Longest Common Subpath[leetcode].cpp

--- String Algos/Hashing/08 Longest Common Subpath[leetcode].cpp
+++ String Algos/Hashing/08 Longest Common Subpath[leetcode].cpp
@@ -1,6 +1,42 @@
 // https://leetcode.com/problems/longest-common-subpath/
 
 /*
+Question:
+Longest common substring given n strings.
+*/
+
+/*
+Problem:
+There is a country of n cities numbered from 0 to n - 1. In this country, there is a road connecting every pair of cities.
+There are m friends numbered from 0 to m - 1 who are traveling through the country. Each one of them will take a path consisting of some cities. 
+Each path is represented by an integer array that contains the visited cities in order. 
+The path may contain a city more than once, but the same city will not be listed consecutively.
+Given an integer n and a 2D integer array paths where paths[i] is an integer array representing the path of the ith friend, 
+return the length of the longest common subpath that is shared by every friend's path, or 0 if there is no common subpath at all.
+A subpath of a path is a contiguous sequence of cities within that path.
+
+Example:
+Input: 
+n = 5, paths = [[0,1,2,3,4],
+                [2,3,4],
+                [4,0,1,2,3]]
+Output: 
+2
+Explanation: The longest common subpath is [2,3].
+
+
+Constraints:
+
+1 <= n <= 10^5
+m == paths.length
+2 <= m <= 10^5
+sum(paths[i].length) <= 10^5
+0 <= paths[i][j] < n
+The same city is not listed multiple times consecutively in paths[i].
+*/
+
+
+/*
 Please note that we have to choose base wisely i.e. base >= 100000.
 I liked the way you used rolling hash in reverse order... removing the use of modular inverse.
 
