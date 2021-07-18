ef455e20212df1e6fa7b750dcf8edfc71584c1d6
Ashwani Yadav <ashyadavash@gmail.com>
2021-07-18 14:28:29 +0530

    Update Using Bellman Ford.cpp

--- Graph/Detecting Negative Weight Cycle/Using Bellman Ford.cpp
+++ Graph/Detecting Negative Weight Cycle/Using Bellman Ford.cpp
@@ -15,128 +15,40 @@ If there is a negative weight cycle, then one more iteration would give a shorte
 */
 
 // This program works only if all vertices are reachable from a source vertex 0.
-#include <bits/stdc++.h>
-using namespace std;
- 
-// a structure to represent a weighted edge in graph
-struct Edge {
-    int src, dest, weight;
-};
- 
-// a structure to represent a connected, directed and weighted graph
-struct Graph {
-    // V-> Number of vertices, E-> Number of edges
-    int V, E;
- 
-    // graph is represented as an array of edges.
-    Edge *edge;
-};
- 
-// Creates a graph with V vertices and E edges
-Graph* createGraph(int V, int E) {
-    Graph* graph = new Graph;
-    graph->V = V;
-    graph->E = E;
-    graph->edge = new Edge[graph->E];
-    return graph;
-}
- 
-// The main function that finds shortest distances
-// from src to all other vertices using Bellman-
-// Ford algorithm.  The function also detects
-// negative weight cycle
-bool isNegCycleBellmanFord(Graph* graph, int src) {
-    int V = graph->V;
-    int E = graph->E;
-    int dist[V];
- 
-    // Step 1: Initialize distances from src
-    // to all other vertices as INFINITE
-    for (int i = 0; i < V; i++) dist[i] = INT_MAX;
-    dist[src] = 0;
- 
-    // Step 2: Relax all edges |V| - 1 times.
-    // A simple shortest path from src to any
-    // other vertex can have at-most |V| - 1
-    // edges
-    for (int i = 1; i <= V - 1; i++) {
-        for (int j = 0; j < E; j++) {
-            int u = graph->edge[j].src;
-            int v = graph->edge[j].dest;
-            int weight = graph->edge[j].weight;
-            dist[v] = min(dist[v], dist[u] + weight);
+class Solution {
+public:
+    #define INF 1e9
+    bool isNegCycleBellmanFord(int src, int n, vector < long long > & dis, vector < vector < int >> & edges) {
+        dis = vector < long long > (n, INF);
+        dis[src] = 0;
+        for (int i = 1; i <= n - 1; i++) {
+            for (auto x: edges) {
+                int u = x[0];
+                int v = x[1];
+                int d = x[2];
+                if (dis[u] != INF && dis[v] > dis[u] + d) {
+                    dis[v] = min(dis[v], dis[u] + d);
+                }
+            }
         }
-    }
- 
-    // Step 3: check for negative-weight cycles.
-    // The above step guarantees shortest distances if graph doesn't contain negative weight cycle.
-    // But if still we get a shorter path, then there is a cycle.
-    for (int i = 0; i < E; i++) {
-        int u = graph->edge[i].src;
-        int v = graph->edge[i].dest;
-        int weight = graph->edge[i].weight;
-        if (dist[u] != INT_MAX && dist[u] + weight < dist[v]) {
-            return true;
+
+        for (auto x: edges) {
+            int u = x[0];
+            int v = x[1];
+            int d = x[2];
+            if (dis[v] > dis[u] + d) {
+                return true;
+            }
         }
+
+        return false;
     }
- 
-    return false;
-}
- 
-// Driver program to test above functions
-int main() {
-    /* Let us create the graph given in above example */
-    int V = 5; // Number of vertices in graph
-    int E = 8; // Number of edges in graph
-    Graph* graph = createGraph(V, E);
- 
-    // add edge 0-1 (or A-B in above figure)
-    graph->edge[0].src = 0;
-    graph->edge[0].dest = 1;
-    graph->edge[0].weight = -1;
- 
-    // add edge 0-2 (or A-C in above figure)
-    graph->edge[1].src = 0;
-    graph->edge[1].dest = 2;
-    graph->edge[1].weight = 4;
- 
-    // add edge 1-2 (or B-C in above figure)
-    graph->edge[2].src = 1;
-    graph->edge[2].dest = 2;
-    graph->edge[2].weight = 3;
- 
-    // add edge 1-3 (or B-D in above figure)
-    graph->edge[3].src = 1;
-    graph->edge[3].dest = 3;
-    graph->edge[3].weight = 2;
- 
-    // add edge 1-4 (or A-E in above figure)
-    graph->edge[4].src = 1;
-    graph->edge[4].dest = 4;
-    graph->edge[4].weight = 2;
- 
-    // add edge 3-2 (or D-C in above figure)
-    graph->edge[5].src = 3;
-    graph->edge[5].dest = 2;
-    graph->edge[5].weight = 5;
- 
-    // add edge 3-1 (or D-B in above figure)
-    graph->edge[6].src = 3;
-    graph->edge[6].dest = 1;
-    graph->edge[6].weight = 1;
- 
-    // add edge 4-3 (or E-D in above figure)
-    graph->edge[7].src = 4;
-    graph->edge[7].dest = 3;
-    graph->edge[7].weight = -3;
- 
-    if (isNegCycleBellmanFord(graph, 0))
-        cout << "Yes";
-    else
-        cout << "No";
- 
-    return 0;
-}
+
+    int isNegativeWeightCycle(int n, vector < vector < int >> edges) {
+        vector < long long > dis(n);
+        return isNegCycleBellmanFord(0, n, dis, edges);
+    }
+};
 
 
 
@@ -157,154 +69,57 @@ The above algorithm and program might not work if the given graph is disconnecte
 It works when all vertices are reachable from source vertex 0.
 To handle disconnected graph, we can repeat the process for vertices for which distance is infinite.
 */
-
-#include <bits/stdc++.h>
-using namespace std;
-
-// a structure to represent a weighted edge in graph
-struct Edge {
-    int src, dest, weight;
-};
-
-// a structure to represent a connected, directed and weighted graph
-struct Graph {
-    // V-> Number of vertices, E-> Number of edges
-    int V, E;
-
-    // graph is represented as an array of edges.
-    Edge* edge;
-};
-
-// Creates a graph with V vertices and E edges
-Graph* createGraph(int V, int E) {
-    Graph* graph = new Graph;
-    graph->V = V;
-    graph->E = E;
-    graph->edge = new Edge[graph->E];
-    return graph;
-}
-
-// The main function that finds shortest distances
-// from src to all other vertices using Bellman-
-// Ford algorithm. The function also detects
-// negative weight cycle
-bool isNegCycleBellmanFord(Graph* graph, int src, int dist[]) {
-    int V = graph->V;
-    int E = graph->E;
-
-    // Step 1: Initialize distances from src
-    // to all other vertices as INFINITE
-    for (int i = 0; i < V; i++) dist[i] = INT_MAX;
-    dist[src] = 0;
-
-    // Step 2: Relax all edges |V| - 1 times.
-    // A simple shortest path from src to any
-    // other vertex can have at-most |V| - 1
-    // edges
-    for (int i = 1; i <= V - 1; i++) {
-        for (int j = 0; j < E; j++) {
-            int u = graph->edge[j].src;
-            int v = graph->edge[j].dest;
-            int weight = graph->edge[j].weight;
-            dist[v] = min(dist[v], dist[u] + weight);
-        }
-    }
-
-    // Step 3: check for negative-weight cycles.
-    // The above step guarantees shortest distances
-    // if graph doesn't contain negative weight cycle.
-    // If we get a shorter path, then there
-    // is a cycle.
-    for (int i = 0; i < E; i++) {
-        int u = graph->edge[i].src;
-        int v = graph->edge[i].dest;
-        int weight = graph->edge[i].weight;
-        if (dist[u] != INT_MAX && dist[u] + weight < dist[v]) return true;
-    }
-
-    return false;
-}
-
-// Returns true if given graph has negative weight
-// cycle.
-bool isNegCycleDisconnected(struct Graph* graph) {
-    int V = graph->V;
-    // To keep track of visited vertices to avoid recomputations.
-    bool visited[V];
-    memset(visited, 0, sizeof(visited));
-
-    // This array is filled by Bellman-Ford
-    int dist[V];
-
-    // Call Bellman-Ford for all those vertices that are not visited
-    for (int i = 0; i < V; i++) {
-        if (!visited[i]) {
-            // If cycle found
-            if (isNegCycleBellmanFord(graph, i, dist)) return true;
-
-            // Mark all vertices that are visited in above call.
-            for (int i = 0; i < V; i++) {
-                if (dist[i] != INT_MAX) {
-                    visited[i] = true;
+class Solution {
+public:
+    #define INF 1e9
+    bool isNegCycleBellmanFord(int src, int n, vector < long long > & dis, vector < vector < int >> & edges) {
+        dis = vector < long long > (n, INF);
+        dis[src] = 0;
+        for (int i = 1; i <= n - 1; i++) {
+            for (auto x: edges) {
+                int u = x[0];
+                int v = x[1];
+                int d = x[2];
+                if (dis[u] != INF && dis[v] > dis[u] + d) {
+                    dis[v] = min(dis[v], dis[u] + d);
                 }
             }
         }
+
+        for (auto x: edges) {
+            int u = x[0];
+            int v = x[1];
+            int d = x[2];
+            if (dis[v] > dis[u] + d) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
-    return false;
-}
+    int isNegativeWeightCycle(int n, vector < vector < int >> edges) {
+        // To keep track of visited vertices to avoid recomputations.
+        vector < int > vis(n, 0);
 
-// Driver program to test above functions
-int main() {
-    /* Let us create the graph given in above example */
-    int V = 5; // Number of vertices in graph
-    int E = 8; // Number of edges in graph
-    Graph* graph = createGraph(V, E);
+        // This array is filled by Bellman-Ford
+        vector < long long > dis(n);
 
-    // add edge 0-1 (or A-B in above figure)
-    graph->edge[0].src = 0;
-    graph->edge[0].dest = 1;
-    graph->edge[0].weight = -1;
+        // Call Bellman-Ford for all those vertices that are not visited
+        for (int i = 0; i < n; i++) {
+            if (!vis[i]) {
+                // If cycle found
+                if (isNegCycleBellmanFord(i, n, dis, edges)) return true;
 
-    // add edge 0-2 (or A-C in above figure)
-    graph->edge[1].src = 0;
-    graph->edge[1].dest = 2;
-    graph->edge[1].weight = 4;
+                // Mark all vertices that are visited in above call.
+                for (int i = 0; i < n; i++) {
+                    if (dis[i] != INF) {
+                        vis[i] = 1;
+                    }
+                }
+            }
+        }
 
-    // add edge 1-2 (or B-C in above figure)
-    graph->edge[2].src = 1;
-    graph->edge[2].dest = 2;
-    graph->edge[2].weight = 3;
-
-    // add edge 1-3 (or B-D in above figure)
-    graph->edge[3].src = 1;
-    graph->edge[3].dest = 3;
-    graph->edge[3].weight = 2;
-
-    // add edge 1-4 (or A-E in above figure)
-    graph->edge[4].src = 1;
-    graph->edge[4].dest = 4;
-    graph->edge[4].weight = 2;
-
-    // add edge 3-2 (or D-C in above figure)
-    graph->edge[5].src = 3;
-    graph->edge[5].dest = 2;
-    graph->edge[5].weight = 5;
-
-    // add edge 3-1 (or D-B in above figure)
-    graph->edge[6].src = 3;
-    graph->edge[6].dest = 1;
-    graph->edge[6].weight = 1;
-
-    // add edge 4-3 (or E-D in above figure)
-    graph->edge[7].src = 4;
-    graph->edge[7].dest = 3;
-    graph->edge[7].weight = -3;
-
-    if (isNegCycleDisconnected(graph))
-        cout << "Yes";
-    else
-        cout << "No";
-
-    return 0;
-}
+        return false;
+    }
+};
