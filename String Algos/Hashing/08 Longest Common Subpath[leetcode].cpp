d4c79cc86cee07647a50772b0594c5e307fce66e
Ashwani Yadav <ashyadavash@gmail.com>
2021-07-19 00:06:43 +0530

    Create 08 Longest Common Subpath[leetcode].cpp

---
+++ String Algos/Hashing/08 Longest Common Subpath[leetcode].cpp
@@ -0,0 +1,176 @@
+// https://leetcode.com/problems/longest-common-subpath/
+
+/*
+Please note that we have to choose base wisely i.e. base >= 100000.
+I liked the way you used rolling hash in reverse order... removing the use of modular inverse.
+
+If we find a common subpath of length k using binary search, then it means we can search for longer common subpaths. 
+If not, then it means that we have to reduce our search space by half. 
+So we check possible length for a max of O(logn) times as that is the max length of the minimum sized path.
+*/
+
+// Method 1: Using 1 hash
+class Solution {
+public:
+    #define ll long long    
+    ll base = 100007, MOD = 1e11L + 7;
+    vector<ll> p;
+    
+    bool check(ll len, ll n, vector<vector<int>>& paths) {
+        unordered_set<ll> s;
+        for(ll i = 0; i < n; i++) {
+            if(paths[i].size() < len) {
+                return false;
+            }
+            unordered_set<ll> tempS;
+            ll hash = 0;
+            for(ll j = 0; j < len; j++) {
+                hash = (hash + (((paths[i][j] + 1) * p[len - 1 - j]) % MOD)) % MOD;
+            }
+            
+            if(i == 0) {
+                s.insert(hash);
+            } else {
+                if(s.find(hash) != s.end()) {
+                    tempS.insert(hash);
+                }
+            }
+            
+            for(ll j = len; j < paths[i].size(); j++) {
+                ll x = ((paths[i][j - len] + 1) * p[len - 1]) % MOD;
+                hash = (hash - x + MOD) % MOD;
+                hash = hash * base % MOD;
+                hash = (hash + paths[i][j] + 1) % MOD;
+                if(i == 0) {
+                    s.insert(hash);
+                } else {
+                    if(s.find(hash) != s.end()) {
+                        tempS.insert(hash);
+                    }
+                }
+            }
+            if(i > 0) {
+                s = tempS;
+            }
+            if(s.size() == 0) {
+                return false;
+            }
+        }
+        return s.size() > 0;
+    }
+    
+    int longestCommonSubpath(int n, vector<vector<int>>& paths) {
+        int m = paths.size();
+        ll lo = 0, hi = 1e9L;
+        for(auto path: paths) {
+            hi = min(hi, (ll)path.size());
+        }
+        p = vector<ll>(hi);
+        p[0] = 1;
+        for(ll i = 1; i < hi; i++) {
+            p[i] = (ll)base * p[i - 1] % MOD;
+        }
+        while(lo < hi) {
+            ll mid = (lo + hi + 1) / 2;
+            if(check(mid, m, paths)) {
+                lo = mid;
+            } else {
+                hi = mid - 1;
+            }
+        }
+        return hi;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+// Method 2: Using 2 hash
+class Solution {
+public:
+    #define ll long long    
+    ll base1 = 100007, base2 = 100009, MOD1 = 1e9L + 7, MOD2 = 1e11L + 7;
+    vector<ll> p1, p2;
+    
+    bool check(ll len, ll n, vector<vector<int>>& paths) {
+        set<pair<ll, ll>> s;
+        for(ll i = 0; i < n; i++) {
+            if(paths[i].size() < len) {
+                return false;
+            }
+            set<pair<ll, ll>> tempS;
+            ll hash1 = 0, hash2 = 0;
+            for(ll j = 0; j < len; j++) {
+                hash1 = (hash1 + (((paths[i][j] + 1) * p1[len - 1 - j]) % MOD1)) % MOD1;
+                hash2 = (hash2 + (((paths[i][j] + 1) * p2[len - 1 - j]) % MOD2)) % MOD2;
+            }
+            
+            if(i == 0) {
+                s.insert({hash1, hash2});
+            } else {
+                if(s.find({hash1, hash2}) != s.end()) {
+                    tempS.insert({hash1, hash2});
+                }
+            }
+            
+            for(ll j = len; j < paths[i].size(); j++) {
+                ll x = ((paths[i][j - len] + 1) * p1[len - 1]) % MOD1;
+                hash1 = (hash1 - x + MOD1) % MOD1;
+                hash1 = (hash1 * base1) % MOD1;
+                hash1 = (hash1 + paths[i][j] + 1) % MOD1;
+                
+                x = ((paths[i][j - len] + 1) * p2[len - 1]) % MOD2;
+                hash2 = (hash2 - x + MOD2) % MOD2;
+                hash2 = (hash2 * base2) % MOD2;
+                hash2 = (hash2 + paths[i][j] + 1) % MOD2;
+                
+                if(i == 0) {
+                    s.insert({hash1, hash2});
+                } else {
+                    if(s.find({hash1, hash2}) != s.end()) {
+                        tempS.insert({hash1, hash2});
+                    }
+                }
+            }
+            if(i > 0) {
+                s = tempS;
+            }
+            if(s.size() == 0) {
+                return false;
+            }
+        }
+        return s.size() > 0;
+    }
+    
+    int longestCommonSubpath(int n, vector<vector<int>>& paths) {
+        int m = paths.size();
+        ll lo = 0, hi = 1e9L;
+        for(auto path: paths) {
+            hi = min(hi, (ll)path.size());
+        }
+        p1 = vector<ll>(hi);
+        p2 = vector<ll>(hi);
+        p1[0] = 1;
+        p2[0] = 1;
+        for(ll i = 1; i < hi; i++) {
+            p1[i] = (ll)base1 * p1[i - 1] % MOD1;
+            p2[i] = (ll)base2 * p2[i - 1] % MOD2;
+        }
+        while(lo < hi) {
+            ll mid = (lo + hi + 1) / 2;
+            if(check(mid, m, paths)) {
+                lo = mid;
+            } else {
+                hi = mid - 1;
+            }
+        }
+        return hi;
+    }
+};
