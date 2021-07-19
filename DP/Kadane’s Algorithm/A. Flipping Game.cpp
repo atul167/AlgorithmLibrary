612f077e5c9952bf3b57b7868ebb395f3d6467f0
Ashwani Yadav <ashyadavash@gmail.com>
2021-07-19 16:19:38 +0530

    Update A. Flipping Game.cpp

--- DP/Kadane’s Algorithm/A. Flipping Game.cpp
+++ DP/Kadane’s Algorithm/A. Flipping Game.cpp
@@ -1,49 +1,8 @@
 // https://codeforces.com/problemset/problem/327/A
 
-#include<bits/stdc++.h>
-using namespace std;
 
-#include <ext/pb_ds/assoc_container.hpp> 
-#include <ext/pb_ds/tree_policy.hpp> 
-using namespace __gnu_pbds;
-typedef tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
-
-typedef long long int ll;
-#define int long long int
-#define IOS ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
-#define FRE freopen("input.txt","r",stdin); freopen("output.txt","w",stdout);
-#define PRECISION(x) cout << setprecision(x); cout << fixed;
-#define debug(x) cout << #x << " is: " << (x) << endl;
-#define debugx(x) cout << #x << " is: " << (x) << endl; exit(0);
-#define f(i,n) for(int i=0;i<n;i++)
-#define fa(i,a,n) for(int i=a;i<n;i++)
-#define loop(i,a, n) for(int i = a; i <= n; i++)
-#define loopb(i,a, n) for(int i = a; i >= n; i--)
-#define pb push_back
-#define pf push_front
-#define F first
-#define S second
-#define all(x) x.begin(), x.end()
-#define uniq(v) (v).erase(unique(all(v)),(v).end())
-#define ppc __builtin_popcount
-#define ppcll __builtin_popcountll
-#define PI 3.1415926535897932384626
-#define MOD 1000000007
-#define MOD2 998244353
-#define INF 1000111000111000111LL
-// comment below line in interactive mode (since endl flushes stdout)
-#define endl "\n"
-typedef vector<vector<int>> matrix;
-int dx[] = {0, 1, 0, -1, -1, 1, -1, 1};
-int dy[] = {-1, 0, 1, 0, -1, 1, 1, -1};
-const int MAXN = 1e5+5;
-const int N = 1e5+5;
-int n, m;
-
-
-
+// Method 1
 void solve() {
-    int u, v;
     cin >> n;
     int a[n];
     f(i, n) cin >> a[i];
@@ -67,11 +26,51 @@ void solve() {
     }
 }
 
-int32_t main() {
-    IOS
-    int T = 1;
-    // cin >> T;
-    while(T--)
-    solve();
-    return 0;
+
+
+
+
+
+// Method 2
+void solve() {
+    int x, y, z;
+    cin >> n;
+    vi a(n);
+    f(i, n) cin >> a[i];
+
+    vi b = a;
+
+    f(i, n) {
+        if(a[i] == 1) a[i] = -1;
+        if(a[i] == 0) a[i] = 1;
+    }
+
+    int max_so_far = 0, max_ending_here = 0;
+
+    int start = 0, end = 0;
+    int beg = 0;
+
+    for (int i = 0; i < n; i++) {
+        max_ending_here = max_ending_here + a[i];
+
+        if (max_ending_here < 0) {
+            max_ending_here = 0;
+            beg = i + 1;
+        }
+
+        if (max_so_far < max_ending_here) {
+            max_so_far = max_ending_here;
+            start = beg;
+            end = i;
+        }
+    }
+
+    // reversing 1 to 0 and 0 to 1 in the found range
+    for(int i = start; i <= end; i++) {
+        b[i] = 1 - b[i];
+    }
+
+    int res = 0;
+    f(i, n) res += b[i];
+    cout << res << endl;
 }
