c1589219614ef0edfcbd2417763f2b33e01be77e
Ashwani Yadav <ashyadavash@gmail.com>
2021-07-22 03:01:11 +0530

    Update 03 Shortest Bridge (leetcode).cpp

--- Graph/Multisource BFS/03 Shortest Bridge (leetcode).cpp
+++ Graph/Multisource BFS/03 Shortest Bridge (leetcode).cpp
@@ -104,18 +104,7 @@ public:
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-// Method 2
+// Method 1 (modified)
 while (!q.empty()) {
     auto x = q.front();
     q.pop();
@@ -141,3 +130,90 @@ for(int i = 0; i < n; i++) {
 
 return mn - 1;
     
+
+
+
+
+
+
+
+
+
+
+// Method 2: (0-1 BFS)
+class Solution {
+public:
+    int n, m;
+    int dx[4] = {0, 1, 0, -1};
+    int dy[4] = {-1, 0, 1, 0};
+    
+    bool isSafeBFS(int i, int j) {
+        if(i < 0 || i >= n || j < 0 || j >= m) 
+            return false;
+        return true;
+    }
+    int bfs_01(pair<int, int> src, vector<vector<int>>& A) {
+        vector<vector<int>> dis(n, vector<int>(m, 1e9));
+        deque<pair<int, int>> dq;
+        dq.push_front(src);
+        dis[src.first][src.second] = 0;
+
+        while (!dq.empty()) {
+            auto x = dq.front();
+            dq.pop_front();
+            int i = x.first, j = x.second;
+            
+            if(A[i][j] == 1) {
+                return dis[i][j] - 1;
+            }
+
+            for(int z = 0; z < 4; z++) {
+                int ni = i + dx[z], nj = j + dy[z];
+                if(!isSafeBFS(ni, nj)) continue;
+                
+                int w = (A[ni][nj] == 2) ? 0 : 1;
+                if (dis[ni][nj] > dis[i][j] + w) {
+                    dis[ni][nj] = dis[i][j] + w;
+                    if (w == 0) {
+                        dq.push_front({ni, nj});
+                    } else {
+                        dq.push_back({ni, nj});
+                    }
+                }
+            }
+        }
+
+        return 0;
+    }
+    
+    bool isSafeDFS(int i, int j, vector<vector<int>>& A) {
+        if(i < 0 || i >= n || j < 0 || j >= m || A[i][j] == 0 || A[i][j] == 2) 
+            return false;
+        return true;
+    }
+    void dfs(int i, int j, vector<vector<int>>& A) {
+        if (!isSafeDFS(i, j, A)) {
+            return;
+        }
+        
+        A[i][j] = 2;
+        for(int z = 0; z < 4; z++) {
+            dfs(i + dx[z], j + dy[z], A);
+        }
+    }
+    
+    int shortestBridge(vector<vector<int>>& A) {
+        n = A.size(), m = A[0].size();
+        
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(A[i][j] == 1) {
+                    dfs(i, j, A);
+                    return bfs_01({i, j}, A);
+                }
+            }
+        }
+        
+        return 0;
+    }
+};
