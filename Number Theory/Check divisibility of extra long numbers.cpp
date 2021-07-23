70f13b5c2235bfc5ea073f33cb459cec3fbd982b
Ashwani Yadav <ashyadavash@gmail.com>
2021-07-23 20:01:02 +0530

    Update Check divisibility of extra long numbers.cpp

--- Number Theory/Check divisibility of extra long numbers.cpp
+++ Number Theory/Check divisibility of extra long numbers.cpp
@@ -4,6 +4,7 @@ number = consists of upto 1e6 digits without leading zero
 divisor = [1, 1e9]
 */
 
+// Method 1
 void solve() {
     string number;
     int divisor;
@@ -24,3 +25,32 @@ void solve() {
         cout << number << " is NOT divisible by " << divisor << endl;
     }
 }
+
+
+
+
+
+
+// Method 2
+void solve() {
+    string number;
+    int divisor;
+    cin >> number;
+    cin >> divisor;
+    n = number.size();
+
+    vector<int> remainder(n);
+    
+    remainder[n - 1] = (number[n - 1] - '0') % divisor;
+    int x = 10;
+    for(int i = n - 2; i >= 0; i--) {
+        remainder[i] = ((number[i] - '0') * x + remainder[i + 1]) % divisor;
+        x = (x * 10) % divisor;
+    }
+
+    if(remainder[0] == 0) {
+        cout << number << " is divisible by " << divisor << endl;
+    } else {
+        cout << number << " is NOT divisible by " << divisor << endl;
+    }
+} 
