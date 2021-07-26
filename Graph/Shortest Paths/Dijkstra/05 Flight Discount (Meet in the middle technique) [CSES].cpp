de144b1f2a452707ce015036c0d0d39138788e8e
Ashwani Yadav <ashyadavash@gmail.com>
2021-07-27 01:24:46 +0530

    Update and rename 05 Flight Discount (CSES) [Meet in the middle technique].cpp to 05 Flight Discount (Meet in the middle technique) [CSES].cpp

--- Graph/Shortest Paths/Dijkstra/05 Flight Discount (Meet in the middle technique) [CSES].cpp
+++ Graph/Shortest Paths/Dijkstra/05 Flight Discount (Meet in the middle technique) [CSES].cpp
@@ -3,15 +3,12 @@
 /*
 Question: (NOTE: edges are directed)
 Your task is to find a minimum-price flight route from city 1 to city n. 
-You have one discount coupon, using which you can halve the price of any single flight during the route. 
-However, you can only use the coupon once.
+You have one discount coupon, using which you can halve the price of any single flight during the route. However, you can only use the coupon once.
 
-The first input line has two integers n and m: 
-the number of cities and flight connections. The cities are numbered 1,2,...,n.
-After this there are m lines describing the flights. 
-Each line has three integers a, b, and c: a flight begins at city a, ends at city b, and its price is c. 
-Each flight is unidirectional.
-You can assume that it is always possible to get from city 1 to city n.
+Input:
+The first input line has two integers n and m: the number of cities and flight connections. The cities are numbered 1,2,...,n.
+After this there are m lines describing the flights. Each line has three integers a, b, and c: a flight begins at city a, ends at city b, and its price is c. 
+Each flight is unidirectional. You can assume that it is always possible to get from city 1 to city n.
 */
 
 /*
@@ -30,49 +27,8 @@ through all edges and ans is minimum of (FRM1[edge[i].start + edge.weight/2 + TO
 
 
 
-
-
-#include<bits/stdc++.h>
-using namespace std;
-
-typedef long long int ll;
-#define int long long int
-#define ld long double
-#define IOS ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
-#define FRE freopen("input.txt","r",stdin); freopen("output.txt","w",stdout);
-#define PRECISION(x) cout << setprecision(x); cout << fixed;
-#define UNIQUE(a) sort(a.begin(), a.end() ); a.erase( unique( a.begin(), a.end() ), a.end() );
-#define debug(x) cout << #x << " is: " << (x) << endl;
-#define debug2(x,y) cout << (#x) <<", "<<(#y)<< " are: " << (x) <<", "<<(y)<< endl;
-#define debugx(x) cout << #x << " is: " << (x) << endl; exit(0);
-#define kickstart(testcase, res) cout << "Case #" << testcase << ": " << res << endl;
-#define f(i,n) for(int i=0;i<n;i++)
-#define fa(i,a,n) for(int i=a;i<n;i++)
-#define loop(i,a, n) for(int i = a; i <= n; i++)
-#define loopb(i,a, n) for(int i = a; i >= n; i--)
-#define pb push_back
-#define pf push_front
-#define F first
-#define S second
-#define all(x) x.begin(), x.end()
-#define uniq(v) (v).erase(unique(all(v)),(v).end())
-#define PI 3.1415926535897932384626
-#define MOD 1000000007
-#define MOD2 998244353
-#define INT_INF 1011111111
-#define INF 1000111000111000111LL
-// comment below line in interactive mode (since endl flushes stdout)
-// #define endl "\n"
-typedef vector<int> vi;
-typedef vector<pair<int, int>> vpii;
-typedef pair<int, int> pii;
-typedef vector<vector<int>> matrix;
-int dx[] = {0, 1, 0, -1, -1, 1, -1, 1};
-int dy[] = {-1, 0, 1, 0, -1, 1, 1, -1};
-const int MAXN = 1e5+5;
 const int N = 2e5+5;
 int n, m;
-
 
 vector<pair<int,int>> g[N];
 int dp[N];
@@ -144,14 +100,4 @@ void solve() {
     }
 
     cout << ans << endl;
-}
-
-
-signed main() {
-    IOS
-    int t = 1;
-    // cin >> t;
-    while(t--) {
-        solve();
-    }
 }
