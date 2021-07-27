2711f9901a4fdcd8e1c036bea39c52605301bd41
Ashwani Yadav <ashyadavash@gmail.com>
2021-07-28 02:07:47 +0530

    Update 03 Tree Distances II (CSES).cpp

--- Graph/Trees/DP/03 Tree Distances II (CSES).cpp
+++ Graph/Trees/DP/03 Tree Distances II (CSES).cpp
@@ -1,7 +1,80 @@
 // https://youtu.be/nGhE4Ekmzbc
 // https://cses.fi/problemset/task/1133/
 
-// Method 1 (my leetcode soln)
+
+
+// https://youtu.be/lWCZOjUOjRc
+// Method 1 (Re-rooting DP)
+const int N = 2e5+5;
+int n, m;
+
+vector<int> g[N];
+int subtreeSize[N];
+int subtreeRes[N];
+int res[N];
+
+void getSubtreeSize(int u, int par) {
+    subtreeSize[u] = 1;
+    subtreeRes[u] = 0;
+
+    for(int v: g[u]) {
+        if(v == par) continue;
+
+        getSubtreeSize(v, u);
+
+        subtreeSize[u] += subtreeSize[v];
+        subtreeRes[u] += subtreeSize[v] + subtreeRes[v];
+    }
+}
+
+void getRes(int u, int par, int parRes) {
+    res[u] = subtreeRes[u] + (parRes + (n - subtreeSize[u]));
+    for(int v: g[u]) {
+        if(v == par) continue;
+        getRes(v, u, res[u] - (subtreeRes[v] + subtreeSize[v]));
+    }
+}
+
+void solve() {
+    int u, v;
+    cin >> n;
+    f(i, n-1) cin >> u >> v, g[u].pb(v), g[v].pb(u);
+
+    getSubtreeSize(1, -1);
+
+    getRes(1, -1, 0);
+
+    loop(i, 1, n) {
+        cout << res[i] << " ";
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Method 2 (my leetcode soln)
 const int N = 2e5+5;
 int n, m;
 
@@ -44,78 +117,5 @@ void solve() {
 
     loop(i, 1, n) {
         cout << res[i] << " ";
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Method 2 (kartik arora)
-const int N = 2e5+5;
-int n, m;
-
-vector<int> g[N];
-int subtreeSize[N];
-int subtreeRes[N];
-int res[N];
-
-void getSubtreeSize(int u, int par) {
-	subtreeSize[u] = 1;
-	subtreeRes[u] = 0;
-
-    for(int v: g[u]) {
-        if(v == par) continue;
-
-        getSubtreeSize(v, u);
-
-        subtreeSize[u] += subtreeSize[v];
-        subtreeRes[u] += subtreeSize[v] + subtreeRes[v];
-    }
-}
-
-void getRes(int u, int par, int parRes) {
-	res[u] = subtreeRes[u] + (parRes + (n - subtreeSize[u]));
-    for(int v: g[u]) {
-        if(v == par) continue;
-        getRes(v, u, res[u] - (subtreeRes[v] + subtreeSize[v]));
-    }
-}
-
-void solve() {
-	int u, v;
-	cin >> n;
-	f(i, n-1) cin >> u >> v, g[u].pb(v), g[v].pb(u);
-
-	getSubtreeSize(1, -1);
-
-    getRes(1, -1, 0);
-
-    loop(i, 1, n) {
-    	cout << res[i] << " ";
     }
 }
