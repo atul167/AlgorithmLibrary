7a02c018ccfabc8f6738bac5b601c5eb9429d3a0
Ashwani Yadav <ashyadavash@gmail.com>
2021-07-31 03:21:01 +0530

    Update 07 E. Little Artem and Time Machine(2D BIT).cpp

--- Fenwick Tree-BIT/07 E. Little Artem and Time Machine(2D BIT).cpp
+++ Fenwick Tree-BIT/07 E. Little Artem and Time Machine(2D BIT).cpp
@@ -3,36 +3,36 @@
 
 
 const int N = 1e9+5;
-int n, m, k;
+int n, m;
  
 map<int, map<int, int>> BIT;
-void update(int time, int val, int cnt) {
-	while(time < N) {
-		BIT[time][val] += cnt;
-		time += (time&-time);
-	}
+void update(int time, int i, int val) {
+    while(time < N) {
+        BIT[time][i] += val;
+        time += (time&-time);
+    }
 }
 
-int query(int time, int val) {
-	int sum = 0;
-	while(time > 0) {
-		sum += BIT[time][val];
-		time -= (time&-time);
-	}
-	return sum;
+int query(int time, int i) {
+    int sum = 0;
+    while(time > 0) {
+        sum += BIT[time][i];
+        time -= (time&-time);
+    }
+    return sum;
 }
 
 void solve() {
-	int x, y;
-	cin >> n;
-	int type, time, val;
-	while(n--) {
-		cin >> type >> time >> val;
-		if(type == 1)
-			update(time, val, 1);
-		else if(type == 2)
-			update(time, val, -1);
-		else
-			cout << query(time, val) << endl;
-	}
+    int x, y;
+    cin >> n;
+    int type, time, val;
+    while(n--) {
+        cin >> type >> time >> val;
+        if(type == 1)
+            update(time, val, 1);
+        else if(type == 2)
+            update(time, val, -1);
+        else
+            cout << query(time, val) << endl;
+    }
 }
