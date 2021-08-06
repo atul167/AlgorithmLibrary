b97ce541a8f1836455c01e05a4a26396d0fd5c31
Ashwani Yadav <ashyadavash@gmail.com>
2021-08-06 22:36:17 +0530

    Update 02 Palindrome Partitioning II (min cuts required).cpp

--- DP/Matrix Chain Multiplication/02 Palindrome Partitioning II (min cuts required).cpp
+++ DP/Matrix Chain Multiplication/02 Palindrome Partitioning II (min cuts required).cpp
@@ -1,7 +1,7 @@
 // https://leetcode.com/problems/palindrome-partitioning-ii/
 // https://www.geeksforgeeks.org/palindrome-partitioning-dp-17/
 
-// TLE solution
+// TLE solution (MCM - memoized)
 class Solution {
 public:
     vector<vector<bool>> isPalindrome;
@@ -55,5 +55,69 @@ public:
         
         memset(dp, -1, sizeof dp);
         return go(0, n - 1);
+    }
+};
+
+
+
+
+
+// TLE solution (MCM - gap method)
+class Solution {
+public:
+    vector<vector<bool>> isPalindrome;
+    int n;
+    
+    void precomputeIsPalindrome(string s) {
+        isPalindrome.resize(n, vector<bool>(n));
+        for(int gap = 0; gap < n; gap++) {
+            for(int i=0, j=gap;  j<n;  i++, j++) {
+                isPalindrome[i][j] = false;
+                if(gap == 0) {
+                    isPalindrome[i][j] = true;
+                } else if(gap == 1) {
+                    if(s[i] == s[j]) isPalindrome[i][j] = true;
+                    else isPalindrome[i][j] = false;
+                } else {
+                    if(s[i] == s[j] && isPalindrome[i + 1][j - 1]) {
+                        isPalindrome[i][j] = true;
+                    } else {
+                        isPalindrome[i][j] = false;
+                    }
+                }
+            }
+        }
+    }
+
+    int minCut(string s) {
+        n = s.size();
+        precomputeIsPalindrome(s);
+        
+        // dp[i][j] = min cuts required for substring 1...j
+        vector<vector<int>> dp(n, vector<int>(n));
+
+        for (int gap = 0; gap < n; gap++) {
+            for (int i = 0, j = gap; j < n; i++, j++) {
+                if (gap == 0) {
+                    dp[i][j] = 0;
+                } else if (gap == 1) {
+                    if(s[i] == s[j]) dp[i][j] = 0;
+                    else dp[i][j] = 1;
+                } else {
+                    if(isPalindrome[i][j]) dp[i][j] = 0;
+                    else {
+                        dp[i][j] = INT_MAX;
+                        for (int k = i; k < j; k++) {
+                            int leftCost = dp[i][k];
+                            int rightCost = dp[k + 1][j];
+
+                            dp[i][j] = min(dp[i][j], leftCost + rightCost + 1);
+                        }
+                    }
+                }
+            }
+        }
+
+        return dp[0][n - 1];
     }
 };
