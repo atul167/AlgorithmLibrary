6fca3880bc9fe97e6d0f573f57c650aa7bcba653
Ashwani Yadav <ashyadavash@gmail.com>
2021-08-06 21:55:21 +0530

    Update 04 Scramble String [leetcode hard].cpp

--- DP/Matrix Chain Multiplication/04 Scramble String [leetcode hard].cpp
+++ DP/Matrix Chain Multiplication/04 Scramble String [leetcode hard].cpp
@@ -15,9 +15,11 @@ Given two strings s1 and s2 of the same length, return true if s2 is a scrambled
 class Solution {
 public:
     unordered_map<string, bool> dp;
+   
     bool solve(string s1, string s2) {
         if(s1.size() == 1) return s1 == s2;
         if(s1 == s2) return true;
+       
         string key = s1 + s2;
         if(dp.find(key) != dp.end()) return dp[key];
         
@@ -39,6 +41,7 @@ public:
         }
         return dp[key] = false;
     }
+   
     bool isScramble(string s1, string s2) {
         return solve(s1,s2);
     }
