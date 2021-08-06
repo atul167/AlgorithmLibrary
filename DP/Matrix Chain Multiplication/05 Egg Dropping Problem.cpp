096ace3885c11289139f4a89dc4122889b04896b
Ashwani Yadav <ashyadavash@gmail.com>
2021-08-07 01:50:19 +0530

    Update 05 Egg Dropping Problem.cpp

--- DP/Matrix Chain Multiplication/05 Egg Dropping Problem.cpp
+++ DP/Matrix Chain Multiplication/05 Egg Dropping Problem.cpp
@@ -1,13 +1,23 @@
-// https://practice.geeksforgeeks.org/problems/egg-dropping-puzzle-1587115620/1
+// https://leetcode.com/problems/super-egg-drop/
 // https://www.geeksforgeeks.org/egg-dropping-puzzle-dp-11/
-
 // https://youtu.be/UvksR0hR9nA
 
+/*
+You are given k identical eggs and you have access to a building with n floors labeled from 1 to n.
+You know that there exists a floor f where 0 <= f <= n such that any egg dropped at a floor higher than f will break, 
+and any egg dropped at or below floor f will not break.
+Each move, you may take an unbroken egg and drop it from any floor x (where 1 <= x <= n). 
+If the egg breaks, you can no longer use it. However, if the egg does not break, you may reuse it in future moves.
+Return the minimum number of moves that you need to determine with certainty what the value of f is.
+*/
 
+
+
+// TLE Solution
 class Solution {
 public:
-    //Function to find minimum number of attempts needed in order to find the critical floor.
-    int eggDrop(int eggs, int floors) {
+    int superEggDrop(int k, int n) {
+        int eggs = k, floors = n;
         int dp[eggs + 1][floors + 1];
         memset(dp, 0, sizeof dp);
 
