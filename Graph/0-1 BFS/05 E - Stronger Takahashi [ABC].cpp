226e92c18295d24a0fb6d2b7cd42c023b4f1fe3f
Ashwani Yadav <ashyadavash@gmail.com>
2021-08-09 16:44:20 +0530

    Update 05 E - Stronger Takahashi [ABC].cpp

--- Graph/0-1 BFS/05 E - Stronger Takahashi [ABC].cpp
+++ Graph/0-1 BFS/05 E - Stronger Takahashi [ABC].cpp
@@ -1,4 +1,19 @@
+// https://atcoder.jp/contests/abc213/tasks/abc213_e
+
 /*
+Problem:
+There is a town divided into a grid of cells with H rows and W columns. 
+The cell at (i, j) is a passable space if S[i][j] is "." and a block if S[i][j] is "#". 
+Takahashi will go from his house to a fish market. 
+His house is in the cell at the top-left corner, and the fish market is in the cell at the bottom-right corner. 
+Takahashi can move one cell up, down, left, or right to a passable cell. He cannot leave the town. He cannot enter a block, either. 
+However, his physical strength allows him to destroy all blocks in a square region with 2 × 2 cells of his choice with one punch, making these cells passable. 
+Find the minimum number of punches needed for Takahashi to reach the fish market.
+*/
+
+
+/*
+Editorial:
 This problem can be solved with a 01-BFS
 Assuming that he does not punch until needed, we can assume that a 2 × 2 region of destroying blocks is adjacent to the square Takahashi is at.
 
@@ -20,6 +35,107 @@ Similar problem: ABC176D “Wizard in Maze”
 */
 
 
+// Method 1
+const int N = 5e2+5;
+int n, m;
+ 
+string g[N];
+int srcX, srcY, destX, destY;
+ 
+bool isSafe1(int x, int y) {
+    if(x < 0 || x >= n || y < 0 || y >= m || g[x][y] == '#')
+        return false;
+    return true;
+}
+ 
+bool isSafe2(int x, int y) {
+    if(x < 0 || x >= n || y < 0 || y >= m)
+        return false;
+    return true;
+}
+ 
+void bfs01() {
+    vector<vector<int>> dist(n, vector<int>(m, INF));
+ 
+    deque<pair<int, int>> q;
+    q.push_back({srcX, srcY});
+    dist[srcX][srcY] = 0;
+ 
+    while(!q.empty()) {
+        auto it = q.front();
+        q.pop_front();
+        int x = it.first, y = it.second;
+ 
+        for(int z = 0; z < 4; z++) {
+            int nx = x + dx[z], ny = y + dy[z];
+            if(!isSafe1(nx, ny)) continue;
+ 
+            int w = 0;
+            if(dist[nx][ny] > dist[x][y] + w) {
+                dist[nx][ny] = dist[x][y] + w;
+                q.push_front({nx, ny});
+            }
+        }
+ 
+        for(int nx = x - 2; nx <= x + 2; nx++) {
+            for(int ny = y - 2; ny <= y + 2; ny++) {
+                if(!isSafe2(nx, ny)) continue;
+                // we cannot go to these 4 boundary points
+                if((nx == x-2 && ny == y-2) || (nx == x-2 && ny == y+2) || (nx == x+2 && ny == y-2) || (nx == x+2 && ny == y+2)) continue;
+ 
+                int w = 1;
+                if(dist[nx][ny] > dist[x][y] + w) {
+                    dist[nx][ny] = dist[x][y] + w;
+                    q.push_back({nx, ny});
+                }
+            }
+        }
+    }
+ 
+    cout << dist[destX][destY];
+}
+ 
+void solve() {
+    int x, y, z;
+    cin >> n >> m;
+    
+    f(i, n) cin >> g[i];
+ 
+    srcX = 0, srcY = 0, destX = n-1, destY = m-1;
+    bfs01();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Method 2
 const int N = 5e2+5;
 int n, m;
 
