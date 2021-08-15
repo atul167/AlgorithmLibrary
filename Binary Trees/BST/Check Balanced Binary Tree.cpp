6f7e2fe9e8a3a9be7c5d01960624c7d4a5f7f13e
Ashwani Yadav <ashyadavash@gmail.com>
2021-08-16 01:01:18 +0530

    Update Check Balanced Binary Tree.cpp

--- Binary Trees/BST/Check Balanced Binary Tree.cpp
+++ Binary Trees/BST/Check Balanced Binary Tree.cpp
@@ -7,6 +7,17 @@ To check if a tree is height-balanced, get the height of left and right subtrees
 Return true if difference between heights is not more than 1 and left and right subtrees are balanced, otherwise return false. 
 */
 
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     bool res;
@@ -14,12 +25,12 @@ public:
     int check(TreeNode* root) {
         if(!root) return 0;
         
-        int l = check(root->left);
-        int r = check(root->right);
+        int leftH = check(root->left);
+        int rightH = check(root->right);
         
-        if(abs(l - r) > 1) res = false;
+        if(abs(leftH - rightH) > 1) res = false;
         
-        return 1 + max(l, r);
+        return 1 + max(leftH, rightH);
     }
     
     bool isBalanced(TreeNode* root) {
