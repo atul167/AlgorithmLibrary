b657f776cac57504dceea316c84c177944e00d7c
Ashwani Yadav <ashyadavash@gmail.com>
2021-08-16 01:07:08 +0530

    Update and rename 03 Height Balanced BST from sorted array.cpp to 03 Convert Sorted Array to Height Balanced BST.cpp

--- Binary Trees/BST/03 Convert Sorted Array to Height Balanced BST.cpp
+++ Binary Trees/BST/03 Convert Sorted Array to Height Balanced BST.cpp
@@ -1,4 +1,43 @@
-// https://youtu.be/lbxvfL1Oxlk
+// https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/
+
+/*
+Given an integer array nums where the elements are sorted in ascending order, convert it to a height-balanced binary search tree.
+A height-balanced binary tree is a binary tree in which the depth of the two subtrees of every node never differs by more than one.
+*/
+
+class Solution {
+public:
+    TreeNode* fun(int l, int r, vector<int>& nums) {
+        // base case
+        if(l > r) return NULL;
+
+        int mid = ((l + r) >> 1);
+        TreeNode *root = new TreeNode(nums[mid]);
+        root->left = fun(l, mid - 1, nums);
+        root->right = fun(mid + 1, r, nums);
+
+        return root;
+    }
+    
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        return fun(0, nums.size() - 1, nums);
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 class BstNode {
 private:
