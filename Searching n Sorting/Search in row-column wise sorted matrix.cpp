568b90f8eebb645492c7bd40771b7a8ca0c83332
Ashwani Yadav <ashyadavash@gmail.com>
2021-08-16 18:40:53 +0530

    Update Search in row-column wise sorted matrix.cpp

--- Searching n Sorting/Search in row-column wise sorted matrix.cpp
+++ Searching n Sorting/Search in row-column wise sorted matrix.cpp
@@ -1,11 +1,11 @@
 // https://practice.geeksforgeeks.org/problems/search-in-a-matrix17201720/1
 
 class Solution{
-public:	
-	int matSearch (vector <vector <int>> &mat, int n, int m, int x) {
-	    if(x < mat[0][0] || x > mat[n-1][m-1]) {
+public: 
+    int matSearch (vector <vector <int>> &mat, int n, int m, int x) {
+        if(x < mat[0][0] || x > mat[n-1][m-1]) {
             return 0;
-	    }
+        }
     
         int i = 0, j = m-1;
     
@@ -20,5 +20,5 @@ public:
         }
         
         return 0;
-	}
+    }
 };
