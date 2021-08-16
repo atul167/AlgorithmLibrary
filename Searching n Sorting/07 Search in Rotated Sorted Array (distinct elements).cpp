1ca83c3c55dccdfc6db0d18dc3b528587e9f1da8
Ashwani Yadav <ashyadavash@gmail.com>
2021-08-17 01:36:37 +0530

    Update 07 Search in Rotated Sorted Array (distinct elements).cpp

--- Searching n Sorting/07 Search in Rotated Sorted Array (distinct elements).cpp
+++ Searching n Sorting/07 Search in Rotated Sorted Array (distinct elements).cpp
@@ -12,6 +12,7 @@ public:
         int left = 0, right = n - 1;
         while (left <= right) {
             int mid = left + (right - left) / 2;
+            
             if (target == nums[mid]) {
                 return mid;
             }
