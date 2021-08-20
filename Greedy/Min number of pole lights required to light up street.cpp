05bf099e4dd98dc83f7afb2c4ac589d51a18519d
Ashwani Yadav <ashyadavash@gmail.com>
2021-08-20 20:21:25 +0530

    Update Min number of pole lights required to light up street.cpp

--- Greedy/Min number of pole lights required to light up street.cpp
+++ Greedy/Min number of pole lights required to light up street.cpp
@@ -7,6 +7,13 @@ A sprinkler with range x at partition i can water all partitions from i-x to i+x
 Given an array gallery[] consisting of n integers, where gallery[i] is the range of sprinkler at partition i (power == -1 indicates no sprinkler attached), 
 return the minimum number of sprinklers that need to be turned on to water the complete gallery.
 If there is no possible way to water the full length using the given sprinklers, print -1.
+
+Input:
+n = 6
+gallery[ ] = {-1, 2, 2, -1, 0, 0}
+Output:
+2
+Explanation: Sprinklers at index 2 and 5 can water thefull gallery, span of sprinkler at index 2 = [0,4] and sprinkler at index 5 = [5,5].
 */
 
 
