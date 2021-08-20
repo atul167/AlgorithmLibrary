2a7e56892ff12a35f085bfb6e3cb7956a88297ef
Ashwani Yadav <ashyadavash@gmail.com>
2021-08-20 14:03:30 +0530

    Update 14 Score of Parentheses [leetcode].cpp

--- Stack/14 Score of Parentheses [leetcode].cpp
+++ Stack/14 Score of Parentheses [leetcode].cpp
@@ -1,5 +1,26 @@
 // https://leetcode.com/problems/score-of-parentheses/
 
+/*
+Given a balanced parentheses string s, return the score of the string.
+The score of a balanced parentheses string is based on the following rule:
+1.) "()" has score 1.
+2.) AB has score A + B, where A and B are balanced parentheses strings.
+3.) (A) has score 2 * A, where A is a balanced parentheses string.
+
+Input: s = "()"
+Output: 1
+
+Input: s = "(())"
+Output: 2
+
+Input: s = "()()"
+Output: 2
+
+Input: s = "(()(()))"
+Output: 6
+*/
+
+
 // Time: O(n), Space: O(1)
 class Solution {
 public:
