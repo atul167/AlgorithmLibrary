5c1ddd16d8c725b73dbf33241a59af43714184f6
Ashwani Yadav <ashyadavash@gmail.com>
2021-08-23 12:29:57 +0530

    Update 08 Binary Tree Cameras [leetcode].cpp

--- Graph/Trees/DP/08 Binary Tree Cameras [leetcode].cpp
+++ Graph/Trees/DP/08 Binary Tree Cameras [leetcode].cpp
@@ -8,18 +8,6 @@ Return the minimum number of cameras needed to monitor all nodes of the tree.
 */
 
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
- 
 
 class Solution {
 public:
@@ -50,6 +38,62 @@ public:
                 int op2 = fun(root->right, 1, 0) + min(fun(root->left, 0, 0), fun(root->left, 1, 0));
                 z = min(op1, op2);
             }
+        }
+        
+        return dp[{root, {isCam, isParCam}}] = min({x, y, z});
+    }
+    
+    int minCameraCover(TreeNode* root) {
+        dp.clear();
+        return min(fun(root, 0, 0), fun(root, 1, 0));
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Solution {
+public:
+    #define inf 1e9
+    map<pair<TreeNode*, pair<bool, bool>>, int> dp;
+    
+    int fun(TreeNode* root, bool isCam, bool isParCam) {
+        if(!root) {
+            if(isCam) return inf;
+            return 0;
+        }
+        if(!root->left && !root->right) {
+            if(isCam) return 1;
+            if(isParCam) return 0;
+            return inf;
+        }
+ 
+        if(dp.count({root, {isCam, isParCam}})) return dp[{root, {isCam, isParCam}}];
+        
+        int x = inf, y = inf, z = inf;
+        if(isCam) {
+            x = 1 + min(fun(root->left, 0, 1), fun(root->left, 1, 1)) + min(fun(root->right, 0, 1), fun(root->right, 1, 1));
+        } else  if(isParCam) {
+            y = min(fun(root->left, 0, 0), fun(root->left, 1, 0)) + min(fun(root->right, 0, 0), fun(root->right, 1, 0));
+        } else {
+            int op1 = fun(root->left, 1, 0) + min(fun(root->right, 0, 0), fun(root->right, 1, 0));
+            int op2 = fun(root->right, 1, 0) + min(fun(root->left, 0, 0), fun(root->left, 1, 0));
+            z = min(op1, op2);
         }
         
         return dp[{root, {isCam, isParCam}}] = min({x, y, z});
