e1c7cd06d0c8d267b07c6780f440be88df4a4b2c
Ashwani Yadav <ashyadavash@gmail.com>
2021-08-24 01:43:44 +0530

    Update 04 Rabin Karp [cp-algo].cpp

--- String Algos/Hashing/04 Rabin Karp [cp-algo].cpp
+++ String Algos/Hashing/04 Rabin Karp [cp-algo].cpp
@@ -1,37 +1,87 @@
-vector<int> rabin_karp(string const& str, string const& pat) {
-    const int p = 31; 
-    const int mod = 1e9 + 9;
-    int n = str.size(), m = pat.size();
+/*
+Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+*/
 
-    vector<long long> p_pow(max(n, m)); 
-    p_pow[0] = 1; 
-    for (int i = 1; i < (int)p_pow.size(); i++) {
-        p_pow[i] = (p_pow[i-1] * p) % mod;
-    }
-
-    vector<long long> h_str(n + 1, 0); 
-    for (int i = 0; i < n; i++) {
-        h_str[i+1] = (h_str[i] + (str[i] - 'a' + 1) * p_pow[i]) % mod; 
-    }
-
-    long long h_pat = 0; 
-    for (int i = 0; i < m; i++) {
-        h_pat = (h_pat + (pat[i] - 'a' + 1) * p_pow[i]) % mod; 
-    }
-
-    vector<int> occurences;
-    for (int i = 0; i <= n - m; i++) { 
-        long long cur_h = (h_str[i+m] - h_str[i] + mod) % mod; 
-        // (cur_h / p_pow[i]) = h_pat
-        if (cur_h == h_pat * p_pow[i] % mod) {
-            occurences.push_back(i);
+class Solution {
+public:
+    #define ll long long
+    
+    struct Hashing {
+        ll add(ll a, ll b, ll mod) {
+            ll res = (a + b) % mod;
+            if (res < 0) res += mod;
+            return res;
         }
-    }
-    return occurences;
-}
+        ll mult(ll a, ll b, ll mod) {
+            ll res = (a * 1LL * b) % mod;
+            if (res < 0) res += mod;
+            return res;
+        }
+        ll power(ll a, ll b, ll mod) {
+            a %= mod;
+            ll res = 1;
+            while (b) {
+                if (b & 1) res = (res * a) % mod;
+                a = (a * a) % mod;
+                b >>= 1;
+            }
+            return res;
+        }
 
-void solve() {
-    string str, pat;
-    cin >> str >> pat;
-    for(auto i: rabin_karp(str, pat)) cout << i << " ";
-}
+        ll n;
+        ll base = 256, mod = 1e9L + 9;
+        vector<ll> p, p_inv, hash;
+
+        void init(string s) {
+            n = s.size();
+            p = vector<ll>(n);
+            p_inv = vector<ll>(n);
+            hash = vector<ll>(n);
+
+            p[0] = 1;
+            p_inv[0] = 1;
+            ll pw_inv = power(base, mod - 2, mod);
+            for(ll i = 1; i <= n-1; i++) {
+                p[i] = mult(p[i - 1], base, mod);
+                p_inv[i] = mult(p_inv[i - 1], pw_inv, mod);
+            }
+
+            for(ll i = 0; i < n; i++) {
+                hash[i] = add((i == 0) ? 0 : hash[i - 1], mult(p[n - 1 - i], s[i] - 'a' + 1, mod), mod);
+            }
+        }
+
+        ll getHash(ll l, ll r) {
+            ll h = hash[r];
+            if(l > 0) h -= hash[l - 1];
+            if(h < 0) h += mod;
+            h = mult(h, p_inv[n - 1 - r], mod);
+            return h;
+        }
+    };
+
+    
+    int strStr(string haystack, string needle) {
+        int n = haystack.size();
+        int m = needle.size();
+        
+        if(m == 0) return 0;
+        if(n == 0) return -1;
+
+        Hashing hashStr, hashPat;
+        hashStr.init(haystack);
+        hashPat.init(needle);
+
+
+        ll patternHash = hashPat.getHash(0, m - 1);
+
+        for (int i = 0; i <= n - m; i++) {
+            ll stringHash = hashStr.getHash(i, i + m - 1);
+            if(stringHash == patternHash) {
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+};
