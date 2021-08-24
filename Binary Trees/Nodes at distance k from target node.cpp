4f51e04f8c433cb512a81aac62c539dd3d9783f5
Ashwani Yadav <ashyadavash@gmail.com>
2021-08-24 16:06:17 +0530

    Update Nodes at distance k from target node.cpp

--- Binary Trees/Nodes at distance k from target node.cpp
+++ Binary Trees/Nodes at distance k from target node.cpp
@@ -1,11 +1,20 @@
 // https://leetcode.com/problems/all-nodes-distance-k-in-binary-tree/
 // https://www.geeksforgeeks.org/print-nodes-distance-k-given-node-binary-tree/
 
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
 class Solution {
 public:
     vector<int> res;
     
-    void printkdistanceNodeDown(TreeNode *root, int k) {
+    void kdistanceNodeDown(TreeNode *root, int k) {
         if (!root || k < 0)  return;
 
         if (k == 0) {
@@ -13,15 +22,15 @@ public:
             return;
         }
 
-        printkdistanceNodeDown(root->left, k-1);
-        printkdistanceNodeDown(root->right, k-1);
+        kdistanceNodeDown(root->left, k-1);
+        kdistanceNodeDown(root->right, k-1);
     }
 
     // Prints all nodes at distance k from a given target node.
     // The k distant nodes may be upward or downward.  This function
     // Returns distance of root from target node, it returns -1 if target
     // node is not present in tree rooted with root.
-    int printkdistanceNode(TreeNode* root, TreeNode* target , int k) {
+    int kdistanceNode(TreeNode* root, TreeNode* target , int k) {
         // Base Case 1: If tree is empty, return -1
         if (!root) return -1;
 
@@ -29,12 +38,12 @@ public:
         // to print all nodes at distance k in subtree rooted with
         // target or root
         if (root == target) {
-            printkdistanceNodeDown(root, k);
+            kdistanceNodeDown(root, k);
             return 0;
         }
 
         // Recur for left subtree
-        int dl = printkdistanceNode(root->left, target, k);
+        int dl = kdistanceNode(root->left, target, k);
 
         // Check if target node was found in left subtree
         if (dl != -1) {
@@ -46,7 +55,7 @@ public:
              // Else go to right subtree and print all k-dl-2 distant nodes
              // Note that the right child is 2 edges away from left child
              else
-                printkdistanceNodeDown(root->right, k-dl-2);
+                kdistanceNodeDown(root->right, k-dl-2);
 
              // Add 1 to the distance and return value for parent calls
              return 1 + dl;
@@ -54,12 +63,12 @@ public:
 
         // MIRROR OF ABOVE CODE FOR RIGHT SUBTREE
         // Note that we reach here only when node was not found in left subtree
-        int dr = printkdistanceNode(root->right, target, k);
+        int dr = kdistanceNode(root->right, target, k);
         if (dr != -1) {
              if (dr + 1 == k)
                 res.push_back(root->val);
              else
-                printkdistanceNodeDown(root->left, k-dr-2);
+                kdistanceNodeDown(root->left, k-dr-2);
              return 1 + dr;
         }
 
@@ -68,7 +77,7 @@ public:
     }
     vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
         res.clear();
-        printkdistanceNode(root, target, k);
+        kdistanceNode(root, target, k);
         return res;
     }
 };
