e83ad95f000764c76fcdde80ca682c064db4b95e
Ashwani Yadav <ashyadavash@gmail.com>
2021-08-25 15:08:15 +0530

    Update Longest path in a DAG [atcoder dp].cpp

--- Graph/DP/Longest path in a DAG [atcoder dp].cpp
+++ Graph/DP/Longest path in a DAG [atcoder dp].cpp
@@ -39,9 +39,6 @@ void solve() {
 
 
 
-
-
-
 // Method 1.2
 const int N = 3e5+5;
 int n, m;
@@ -126,7 +123,63 @@ void solve() {
 
 
 
-// Method 2
+// Method 2: Topological Sort
+const int N = 3e5+5;
+int n, m;
+
+vi g[N];
+int dp[N], indegree[N];
+
+void kahn() {
+    queue<int> q;
+    loop(i, 1, n) {
+        if(indegree[i] == 0) q.push(i);
+    }
+
+    int path_len = 0;
+    while(!q.empty()) {
+        int sz = q.size();
+        f(i, sz) {
+            int u = q.front();
+            q.pop();
+
+            for(int v: g[u]) {
+                indegree[v]--;
+                if(indegree[v] == 0) q.push(v);
+            }
+        }
+        path_len += 1;
+    }
+    cout << path_len - 1 << endl;
+}
+
+void solve() {
+    int u, v;
+    cin >> n >> m;
+    f(i, m) {
+        cin >> u >> v;
+        g[u].pb(v);
+        indegree[v]++;
+    }
+    kahn();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Method 3
 const int N = 3e5+5;
 int n, m;
 vi g[N];
