7a99be52c63251219f00ce1de88e7413e92a18d2
Ashwani Yadav <ashyadavash@gmail.com>
2021-08-27 20:54:21 +0530

    Create Bin Search.cpp

---
+++ Bin Search.cpp
@@ -0,0 +1,94 @@
+// FFFFFFFFTTTTTTTTTT
+
+void solve() {
+    cin >> n;
+    vi a(n); f(i, n) cin >> a[i];
+
+    int lo = 0, hi = n;
+    while(lo < hi) {
+        int mid = (lo + hi) / 2;
+        if(a[mid]) {
+            hi = mid;
+        } else {
+            lo = mid + 1;
+        }
+    }
+
+    debug2(lo, hi)
+}
+
+/*
+5
+0 0 1 1 1
+
+lo, hi are: 2, 2
+
+
+
+5
+0 0 0 0 0
+
+lo, hi are: 5, 5
+
+
+5
+1 1 1 1 1
+
+lo, hi are: 0, 0
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// TTTTTTTTFFFFFFFFF
+
+void solve() {
+    cin >> n;
+    vi a(n); f(i, n) cin >> a[i];
+
+    int lo = -1, hi = n - 1;
+    while(lo < hi) {
+        int mid = (lo + hi + 1) / 2;
+        if(a[mid]) {
+            lo = mid;
+        } else {
+            hi = mid - 1;
+        }
+    }
+
+    debug2(lo, hi)
+}
+
+/*
+5
+1 1 0 0 0
+
+lo, hi are: 1, 1
+
+
+
+5
+0 0 0 0 0
+
+lo, hi are: -1, -1
+
+
+5
+1 1 1 1 1
+
+lo, hi are: 4, 4
+*/
