338759a59233dc2ba59992462b84121a0d8075d0
Ashwani Yadav <ashyadavash@gmail.com>
2021-08-28 03:09:29 +0530

    Update Smallest Positive missing number.cpp

--- Arrays/Smallest Positive missing number.cpp
+++ Arrays/Smallest Positive missing number.cpp
@@ -1,3 +1,63 @@
+// https://leetcode.com/problems/first-missing-positive/
+// https://youtu.be/Whhpvk2k5qk
+
+/*
+Given an unsorted integer array nums, return the smallest missing positive integer.
+NOTE: You must implement an algorithm that runs in O(n) time and uses constant extra space.
+
+Example 1:
+Input: nums = [1,2,0]
+Output: 3
+
+Example 2:
+Input: nums = [3,4,-1,1]
+Output: 2
+
+Example 3:
+Input: nums = [7,8,9,11,12]
+Output: 1
+*/
+
+// Time = O(n), Space = O(1)
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int n = nums.size();
+    
+        for(int i = 0; i < n; i++) {
+            while(nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i])
+                swap(nums[i], nums[nums[i] - 1]);
+        }
+
+        for(int i = 0; i < n; i++) {
+            if(nums[i] != (i + 1)) return i + 1;
+        }
+
+        return n + 1;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // https://www.geeksforgeeks.org/find-the-smallest-positive-number-missing-from-an-unsorted-array/
 // https://practice.geeksforgeeks.org/problems/smallest-positive-missing-number-1587115621/1
 
