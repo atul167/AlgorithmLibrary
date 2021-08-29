83bd38dd09d7948564f052ead814d88e60bc2027
Ashwani Yadav <ashyadavash@gmail.com>
2021-08-29 17:43:48 +0530

    Update 09 Time Needed to Inform All Employees [leetcode].cpp

--- Graph/Topological Sort/09 Time Needed to Inform All Employees [leetcode].cpp
+++ Graph/Topological Sort/09 Time Needed to Inform All Employees [leetcode].cpp
@@ -13,6 +13,7 @@ The i-th employee needs informTime[i] minutes to inform all of his direct subord
 Return the number of minutes needed to inform all the employees about the urgent news.
 */
 
+// Method 1.1
 class Solution {
 public:
     int n, m;
@@ -86,19 +87,7 @@ public:
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Method 1.2
 class Solution {
 public:
     int n, m;
@@ -157,5 +146,159 @@ public:
             }
         }
         return topologicalSort(informTime);
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Method 2.1
+class Solution {
+public:
+    int n, m;
+    vector<vector<int>> g;
+    vector<int> dp;
+
+    int dfs(int u, vector<int>& informTime) {
+        if(dp[u]) return dp[u];
+
+        for (int v: g[u]) {
+            dp[u] = max(dp[u], informTime[u] + dfs(v, informTime));
+        }
+
+        return dp[u];
+    }
+
+    int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
+        this->n = n;
+        g = vector<vector<int>>(n);
+        dp = vector<int>(n, 0);
+        
+        for(int i = 0; i < n; i++) {
+            int u = manager[i];
+            int v = i;
+            if(u != -1) {
+                g[u].push_back(v);
+            }
+        }
+        
+        int res = 0;
+        for (int i = 0; i < n; i++) {
+            res = max(res, dfs(i, informTime));
+        }
+
+        return res;
+    }
+};
+
+
+
+
+
+
+
+// Method 2.2
+class Solution {
+public:
+    int n, m;
+    vector<vector<int>> g;
+    vector<int> dp;
+
+    void dfs(int u, vector<int>& informTime) {
+        if(dp[u]) return;
+
+        for (int v: g[u]) {
+            dfs(v, informTime);
+            dp[u] = max(dp[u], informTime[u] + dp[v]);
+        }
+    }
+
+    int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
+        this->n = n;
+        g = vector<vector<int>>(n);
+        dp = vector<int>(n, 0);
+        
+        for(int i = 0; i < n; i++) {
+            int u = manager[i];
+            int v = i;
+            if(u != -1) {
+                g[u].push_back(v);
+            }
+        }
+        
+        for (int i = 0; i < n; i++) {
+            dfs(i, informTime);
+        }
+        
+        int res = 0;
+        for (int i = 0; i < n; i++) {
+            res = max(res, dp[i]);
+        }
+
+        return res;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+// Method 2.3
+class Solution {
+public:
+    int n, m;
+    vector<vector<int>> g;
+    vector<int> dp;
+
+    void dfs(int u, vector<int>& informTime) {
+        if(dp[u] != -1) return;
+        dp[u] = informTime[u];
+
+        for (int v: g[u]) {
+            dfs(v, informTime);
+            dp[u] = max(dp[u], informTime[u] + dp[v]);
+        }
+    }
+
+    int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
+        this->n = n;
+        g = vector<vector<int>>(n);
+        dp = vector<int>(n, -1);
+        
+        for(int i = 0; i < n; i++) {
+            int u = manager[i];
+            int v = i;
+            if(u != -1) {
+                g[u].push_back(v);
+            }
+        }
+        
+        for (int i = 0; i < n; i++) {
+            dfs(i, informTime);
+        }
+        
+        int res = 0;
+        for (int i = 0; i < n; i++) {
+            res = max(res, dp[i]);
+        }
+
+        return res;
     }
 };
