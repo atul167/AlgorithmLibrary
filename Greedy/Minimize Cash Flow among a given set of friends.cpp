a5b337987991746992b5966ac150e614a9611c30
Ashwani Yadav <ashyadavash@gmail.com>
2021-08-31 00:43:46 +0530

    Update Minimize Cash Flow among a given set of friends.cpp

--- Greedy/Minimize Cash Flow among a given set of friends.cpp
+++ Greedy/Minimize Cash Flow among a given set of friends.cpp
@@ -7,6 +7,19 @@ Given a number of friends who have to give or take some amount of money from one
 Design an algorithm by which the total cash flow among all the friends is minimized. 
 */
 
+/*
+The following is detailed algorithm.
+Do following for every person Pi where i is from 0 to n-1. 
+
+1) Compute the net amount (balance) for every person. The net amount for person ‘i’ can be computed by subtracting sum of all debts from sum of all credits.
+2) Find the two persons that are maximum creditor and maximum debtor. 
+   Let the maximum amount to be credited maximum creditor be maxCredit and maximum amount to be debited from maximum debtor be maxDebit. 
+   Let the maximum debtor be Pd and maximum creditor be Pc.
+3) Find the minimum of maxDebit and maxCredit. Let minimum of two be x. Debit ‘x’ from Pd and credit this amount to Pc
+4) If x is equal to maxCredit, then remove Pc from set of persons and recur for remaining (n-1) persons.
+5) If x is equal to maxDebit, then remove Pd from set of persons and recur for remaining (n-1) persons.
+*/
+
 
 
 // Given the list of M transacations b/w N people, find out the minimum amount of cash flow required to settle it up!
