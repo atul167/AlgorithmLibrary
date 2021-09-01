162d35f74945e399c87b6f4656c8d20ea88d04fb
Ashwani Yadav <ashyadavash@gmail.com>
2021-09-01 13:55:08 +0530

    Update 00 Furthest Building You Can Reach [leetcode].cpp

--- Heaps/00 Furthest Building You Can Reach [leetcode].cpp
+++ Heaps/00 Furthest Building You Can Reach [leetcode].cpp
@@ -30,14 +30,15 @@ class Solution {
 public:
     int furthestBuilding(vector<int>& heights, int bricks, int ladders) {
         int n = heights.size();
-        priority_queue<int> pq;
+        priority_queue<int, vector<int>, greater<int>> minHeap;
+        
         for (int i = 0; i < n - 1; i++) {
             int d = heights[i + 1] - heights[i];
             if (d > 0)
-                pq.push(-d); // minHeap = reverse maxHeap
-            if (pq.size() > ladders) {
-                bricks += pq.top();
-                pq.pop();
+                minHeap.push(d); // minHeap = reverse maxHeap
+            if (minHeap.size() > ladders) {
+                bricks -= minHeap.top();
+                minHeap.pop();
             }
             if (bricks < 0)
                 return i;
