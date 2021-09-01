da32734bf2a1c746ec08246edaa46a302f6f33a1
Ashwani Yadav <ashyadavash@gmail.com>
2021-09-01 16:31:59 +0530

    Update Longest path in a DAG [atcoder dp].cpp

--- Graph/DP/Longest path in a DAG [atcoder dp].cpp
+++ Graph/DP/Longest path in a DAG [atcoder dp].cpp
@@ -2,45 +2,7 @@
 // https://www.geeksforgeeks.org/longest-path-in-a-directed-acyclic-graph-dynamic-programming/
 
 
-// Method 1.1
-const int N = 3e5+5;
-int n, m;
-vi g[N];
-int dp[N];
-
-int dfs(int u) {
-    if(dp[u]) return dp[u];
-    dp[u] = 0;
-
-    for (int v: g[u]) {
-        dp[u] = max(dp[u], 1 + dfs(v));
-    }
-
-    return dp[u];
-}
-
-void solve() {
-    int u, v;
-    cin >> n >> m;
-    f(i, m) cin >> u >> v, g[u].pb(v);
-
-    memset(dp, 0, sizeof dp);
-
-    int res = 0;
-    for (int i = 1; i <= n; i++) {
-        res = max(res, dfs(i));
-    }
-
-    cout << res << endl;
-}
-
-
-
-
-
-
-
-// Method 1.2
+// Method 1
 const int N = 3e5+5;
 int n, m;
 vi g[N];
@@ -51,7 +13,8 @@ int dfs(int u) {
     dp[u] = 0;
 
     for (int v: g[u]) {
-        dp[u] = max(dp[u], 1 + dfs(v));
+        dfs(v);
+        dp[u] = max(dp[u], 1 + dp[v]);
     }
 
     return dp[u];
@@ -63,42 +26,6 @@ void solve() {
     f(i, m) cin >> u >> v, g[u].pb(v);
 
     memset(dp, -1, sizeof dp);
-
-    int res = 0;
-    for (int i = 1; i <= n; i++) {
-        res = max(res, dfs(i));
-    }
-
-    cout << res << endl;
-}
-
-
-
-
-
-
-// Method 1.3
-const int N = 3e5+5;
-int n, m;
-vi g[N];
-int dp[N];
-
-void solve() {
-    int u, v;
-    cin >> n >> m;
-    f(i, m) cin >> u >> v, g[u].pb(v);
-
-    memset(dp, 0, sizeof dp);
-
-    std::function<int(int)> dfs = [&] (int u) {
-        if (dp[u]) return dp[u];
-        
-        for (int v: g[u]) {
-            dp[u] = max(dp[u], 1 + dfs(v));
-        }
-        
-        return dp[u];
-    };
 
     int res = 0;
     for (int i = 1; i <= n; i++) {
