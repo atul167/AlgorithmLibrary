d6e4b48ba5ce67b16a9e24e698103e671e8df54e
Ashwani Yadav <ashyadavash@gmail.com>
2021-09-04 14:27:47 +0530

    Create Virtual Destructor in C++.cpp

---
+++ OOPS/Virtual Destructor in C++.cpp
@@ -0,0 +1,82 @@
+/*
+Deleting a derived class object using a pointer of base class type that has a non-virtual destructor results in undefined behavior. 
+To correct this situation, the base class should be defined with a virtual destructor. 
+*/
+
+/*
+CPP program without virtual destructor causing undefined behavior due to early binding concept.
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class base {
+public:
+    base() {
+        cout << "Constructing base \n";
+    }
+    ~base() {
+        cout << "Destructing base \n";
+    }
+};
+
+class derived: public base {
+public:
+    derived() { 
+        cout << "Constructing derived \n"; 
+    }
+    ~derived() { 
+        cout << "Destructing derived \n"; 
+    }
+};
+
+int main() {
+    derived *d = new derived();
+    base *b = d;
+    delete b;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+Correction by using virtual destructor in base class
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class base {
+public:
+    base() {
+        cout << "Constructing base \n";
+    }
+    virtual ~base() {
+        cout << "Destructing base \n";
+    }
+};
+
+class derived: public base {
+public:
+    derived() { 
+        cout << "Constructing derived \n"; 
+    }
+    ~derived() { 
+        cout << "Destructing derived \n"; 
+    }
+};
+
+int main() {
+    derived *d = new derived();
+    base *b = d;
+    delete b;
+}
