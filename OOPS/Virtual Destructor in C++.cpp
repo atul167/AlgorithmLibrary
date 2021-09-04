00d6e4f78960ea6c890c5f898257a100093b13bc
Ashwani Yadav <ashyadavash@gmail.com>
2021-09-04 14:34:10 +0530

    Update Virtual Destructor in C++.cpp

--- OOPS/Virtual Destructor in C++.cpp
+++ OOPS/Virtual Destructor in C++.cpp
@@ -1,3 +1,5 @@
+// https://www.geeksforgeeks.org/virtual-destructor/
+
 /*
 Deleting a derived class object using a pointer of base class type that has a non-virtual destructor results in undefined behavior. 
 To correct this situation, the base class should be defined with a virtual destructor. 
