ce69a10441032553d9a6172db5e1f771d46c5122
Ashwani Yadav <ashyadavash@gmail.com>
2021-09-04 22:58:56 +0530

    Update 00 Delete a node by key.cpp

--- Binary Trees/BST/00 Delete a node by key.cpp
+++ Binary Trees/BST/00 Delete a node by key.cpp
@@ -34,7 +34,7 @@ In this particular case, inorder successor can be obtained by finding the minimu
 
 
 
-
+// Method 1.1
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -218,13 +218,90 @@ Inorder traversal:
 
 
 
+
+
+
+
+
+
+// Method 1.2
+TreeNode* deleteNode(TreeNode *root, int data) {
+	// base case
+	if (!root) return root;
+
+	// If the data to be deleted is smaller than the root's data
+	if (data < root->data) {
+		root->left = deleteNode(root->left, data);
+		return root;
+	}
+
+	// If the data to be deleted is greater than the root's data
+	else if (data > root->data) {
+		root->right = deleteNode(root->right, data);
+		return root;
+	}
+
+	// if data is same as root's data, then this is the node to be deleted
+	else {
+		// Case 1: node has no child
+		if (!root->left and !root->right) {
+			root = NULL;
+		}
+
+		// Case 2: node with only one child
+		else if (!root->left) {
+			TreeNode* temp = root->right;
+			*root = *temp;
+		} else if (!root->right) {
+			TreeNode* temp = root->left;
+			*root = *temp;
+		}
+
+		// Case 3: node with two children:
+		// Get the inorder successor (smallest in the right subtree)
+		else {
+			TreeNode* temp = minValueNode(root->right);
+
+			// Copy the inorder successor's content to this node
+			root->data = temp->data;
+
+			// Delete the inorder successor
+			root->right = deleteNode(root->right, temp->data);
+		}
+
+		return root;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Method 2
 /*
 Optimization to above code for two children case: 
 In the above recursive code, we recursively call delete() for the successor. 
 We can avoid recursive calls by keeping track of the parent node of the successor 
 so that we can simply remove the successor by making the child of a parent NULL. We know that the successor would always be a leaf node.
 */
-
 
 #include <bits/stdc++.h>
 using namespace std;
