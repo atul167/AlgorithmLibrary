e260a86e7bf08fffc7a4bce46c39e850259aa153
Ashwani Yadav <ashyadavash@gmail.com>
2021-09-04 14:38:49 +0530

    Create Virtual Function Eg1.cpp

---
+++ OOPS/Virtual Function Eg1.cpp
@@ -0,0 +1,65 @@
+// Not correct behaviour due to early binding
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class base {
+public:
+    void fun() {
+        cout << "base fun\n";
+    }
+};
+
+class derived: public base {
+public:
+    void fun() {
+        cout << "derived fun\n";
+    }
+};
+
+int main() {
+    derived d;
+    base *b = &d;
+    b->fun();
+}
+
+/*
+base fun
+*/
+
+
+
+
+
+
+
+
+
+// Correct behaviour by late binding
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class base {
+public:
+    virtual void fun() {
+        cout << "base fun\n";
+    }
+};
+
+class derived: public base {
+public:
+    void fun() {
+        cout << "derived fun\n";
+    }
+};
+
+int main() {
+    derived d;
+    base *b = &d;
+    b->fun();
+}
+
+/*
+derived fun
+*/
