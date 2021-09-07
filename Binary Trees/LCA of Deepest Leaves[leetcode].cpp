ae70a554ba49fb3121725fb312af0061cebe3117
Ashwani Yadav <ashyadavash@gmail.com>
2021-09-08 00:33:39 +0530

    Update LCA of Deepest Leaves[leetcode].cpp

--- Binary Trees/LCA of Deepest Leaves[leetcode].cpp
+++ Binary Trees/LCA of Deepest Leaves[leetcode].cpp
@@ -5,6 +5,17 @@ Given the root of a binary tree, return the lowest common ancestor of its deepes
 */
 
 
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     unordered_map<TreeNode*, int> depth;
@@ -19,11 +30,13 @@ public:
     
     TreeNode* lcaDeepestLeaves(TreeNode* root) {
         if(!root) return NULL;
-        int lcount = countDepth(root->left);
-        int rcount = countDepth(root->right);
-        if(lcount == rcount)
-            return root;
-        else if(lcount < rcount)
+        
+        int ldepth = countDepth(root->left);
+        int rdepth = countDepth(root->right);
+        
+        if(ldepth == rdepth) return root;
+        
+        if(ldepth < rdepth)
             return lcaDeepestLeaves(root->right);
         else
             return lcaDeepestLeaves(root->left);
