ebfbc935e151f4125e35153caf1e99981e59ce6f
Ashwani Yadav <ashyadavash@gmail.com>
2021-09-08 00:31:03 +0530

    Update LCA of Deepest Leaves[leetcode].cpp

--- Binary Trees/LCA of Deepest Leaves[leetcode].cpp
+++ Binary Trees/LCA of Deepest Leaves[leetcode].cpp
@@ -1,29 +1,24 @@
 // https://leetcode.com/problems/lowest-common-ancestor-of-deepest-leaves/
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+/*
+Given the root of a binary tree, return the lowest common ancestor of its deepest leaves.
+*/
+
+
 class Solution {
 public:
     unordered_map<TreeNode*, int> depth;
+    
     int countDepth(TreeNode* root){
-        if(!root)
-            return 0;
-        if(!depth.count(root))
-            depth[root] = 1 + max(countDepth(root->left), countDepth(root->right));
-        return depth[root];
+        if(!root) return 0;
+        
+        if(depth.count(root)) depth[root];
+        
+        return depth[root] = 1 + max(countDepth(root->left), countDepth(root->right));
     }
+    
     TreeNode* lcaDeepestLeaves(TreeNode* root) {
-        if(!root)
-            return NULL;
+        if(!root) return NULL;
         int lcount = countDepth(root->left);
         int rcount = countDepth(root->right);
         if(lcount == rcount)
