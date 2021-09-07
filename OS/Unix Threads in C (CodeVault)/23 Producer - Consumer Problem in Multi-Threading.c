0a96a3b120cda8313fbaad49316cf04a2ca5953e
Ashwani Yadav <ashyadavash@gmail.com>
2021-09-07 12:28:39 +0530

    Update 23 Producer - Consumer Problem in Multi-Threading.c

--- OS/Unix Threads in C (CodeVault)/23 Producer - Consumer Problem in Multi-Threading.c
+++ OS/Unix Threads in C (CodeVault)/23 Producer - Consumer Problem in Multi-Threading.c
@@ -1,7 +1,110 @@
 // https://youtu.be/l6zkaJFjUbM
 
 
-// Method 1 (7 producers, 1 consumer)
+// Method 1 (1 producer, 1 consumer)
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <time.h>
+#include <semaphore.h>
+
+sem_t semEmpty;
+sem_t semFull;
+
+pthread_mutex_t mutexBuffer;
+
+#define bufferCapacity 10
+int buffer[bufferCapacity];
+int count = 0;
+
+void* producerFun(void* args) {
+    while (1) {
+        // Produce
+        int x = rand() % 100;
+        sleep(1);
+
+        // Add to the buffer
+        sem_wait(&semEmpty);
+        pthread_mutex_lock(&mutexBuffer);
+        buffer[count] = x;
+        count++;
+        pthread_mutex_unlock(&mutexBuffer);
+        sem_post(&semFull);
+    }
+}
+
+void* consumerFun(void* args) {
+    while (1) {
+        int y;
+
+        // Remove from the buffer
+        sem_wait(&semFull);
+        pthread_mutex_lock(&mutexBuffer);
+        y = buffer[count - 1];
+        count--;
+        pthread_mutex_unlock(&mutexBuffer);
+        sem_post(&semEmpty);
+
+        // Consume
+        printf("Got %d\n", y);
+        sleep(1);
+    }
+}
+
+int main(int argc, char* argv[]) {
+    srand(time(NULL));
+    pthread_t producer, consumer;
+    pthread_mutex_init(&mutexBuffer, NULL);
+    sem_init(&semEmpty, 0, bufferCapacity);
+    sem_init(&semFull, 0, 0);
+
+    if (pthread_create(&producer, NULL, &producerFun, NULL) != 0) {
+        perror("Failed to create thread");
+    }
+    if (pthread_create(&consumer, NULL, &consumerFun, NULL) != 0) {
+        perror("Failed to create thread");
+    }
+
+    if (pthread_join(producer, NULL) != 0) {
+        perror("Failed to join thread");
+    }
+    if (pthread_join(consumer, NULL) != 0) {
+        perror("Failed to join thread");
+    }
+
+    sem_destroy(&semEmpty);
+    sem_destroy(&semFull);
+    pthread_mutex_destroy(&mutexBuffer);
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Method 2 (7 producers, 1 consumer)
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -105,7 +208,7 @@ int main(int argc, char* argv[]) {
 
 
 
-// Method 2 (1 producer, 1 consumer)
+// Method 3 (1 producer, 1 consumer)
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -181,108 +284,6 @@ int main(int argc, char* argv[]) {
             perror("Failed to join thread");
         }
     }
-    sem_destroy(&semEmpty);
-    sem_destroy(&semFull);
-    pthread_mutex_destroy(&mutexBuffer);
-    return 0;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Method 3 (1 producer, 1 consumer)
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <pthread.h>
-#include <unistd.h>
-#include <time.h>
-#include <semaphore.h>
-
-sem_t semEmpty;
-sem_t semFull;
-
-pthread_mutex_t mutexBuffer;
-
-#define bufferCapacity 10
-int buffer[bufferCapacity];
-int count = 0;
-
-void* producerFun(void* args) {
-    while (1) {
-        // Produce
-        int x = rand() % 100;
-        sleep(1);
-
-        // Add to the buffer
-        sem_wait(&semEmpty);
-        pthread_mutex_lock(&mutexBuffer);
-        buffer[count] = x;
-        count++;
-        pthread_mutex_unlock(&mutexBuffer);
-        sem_post(&semFull);
-    }
-}
-
-void* consumerFun(void* args) {
-    while (1) {
-        int y;
-
-        // Remove from the buffer
-        sem_wait(&semFull);
-        pthread_mutex_lock(&mutexBuffer);
-        y = buffer[count - 1];
-        count--;
-        pthread_mutex_unlock(&mutexBuffer);
-        sem_post(&semEmpty);
-
-        // Consume
-        printf("Got %d\n", y);
-        sleep(1);
-    }
-}
-
-int main(int argc, char* argv[]) {
-    srand(time(NULL));
-    pthread_t producer, consumer;
-    pthread_mutex_init(&mutexBuffer, NULL);
-    sem_init(&semEmpty, 0, bufferCapacity);
-    sem_init(&semFull, 0, 0);
-
-    if (pthread_create(&producer, NULL, &producerFun, NULL) != 0) {
-        perror("Failed to create thread");
-    }
-    if (pthread_create(&consumer, NULL, &consumerFun, NULL) != 0) {
-        perror("Failed to create thread");
-    }
-
-    if (pthread_join(producer, NULL) != 0) {
-        perror("Failed to join thread");
-    }
-    if (pthread_join(consumer, NULL) != 0) {
-        perror("Failed to join thread");
-    }
-
     sem_destroy(&semEmpty);
     sem_destroy(&semFull);
     pthread_mutex_destroy(&mutexBuffer);
