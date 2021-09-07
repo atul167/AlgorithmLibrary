a2ab72550a432bf70d2f9177dfa52715b5ad9323
Ashwani Yadav <ashyadavash@gmail.com>
2021-09-08 00:36:28 +0530

    Update LCA of Deepest Leaves[leetcode].cpp

--- Binary Trees/LCA of Deepest Leaves[leetcode].cpp
+++ Binary Trees/LCA of Deepest Leaves[leetcode].cpp
@@ -1,21 +1,19 @@
 // https://leetcode.com/problems/lowest-common-ancestor-of-deepest-leaves/
+// https://leetcode.com/problems/lowest-common-ancestor-of-deepest-leaves/
+
+// Smallest Subtree with all the Deepest Nodes
+// https://leetcode.com/problems/smallest-subtree-with-all-the-deepest-nodes/
+
+/*
+2 questions same solution.
+*/
+
+
 
 /*
 Given the root of a binary tree, return the lowest common ancestor of its deepest leaves.
 */
 
-
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     unordered_map<TreeNode*, int> depth;
@@ -40,5 +38,58 @@ public:
             return lcaDeepestLeaves(root->right);
         else
             return lcaDeepestLeaves(root->left);
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+Given the root of a binary tree, the depth of each node is the shortest distance to the root.
+Return the smallest subtree such that it contains all the deepest nodes in the original tree.
+A node is called the deepest if it has the largest depth possible among any node in the entire tree.
+The subtree of a node is tree consisting of that node, plus the set of all descendants of that node.
+*/
+
+class Solution {
+public:
+    unordered_map<TreeNode*, int> depth;
+    
+    int countDepth(TreeNode* root){
+        if(!root) return 0;
+        
+        if(depth.count(root)) depth[root];
+        
+        return depth[root] = 1 + max(countDepth(root->left), countDepth(root->right));
+    }
+    
+    TreeNode* subtreeWithAllDeepest(TreeNode* root) {
+        if(!root) return NULL;
+        
+        int ldepth = countDepth(root->left);
+        int rdepth = countDepth(root->right);
+        
+        if(ldepth == rdepth) return root;
+        
+        if(ldepth < rdepth)
+            return subtreeWithAllDeepest(root->right);
+        else
+            return subtreeWithAllDeepest(root->left);
     }
 };
