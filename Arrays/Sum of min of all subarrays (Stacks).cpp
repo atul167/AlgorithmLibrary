90246d5d08b9371cbb7af950dcf398a0d60c77e6
Ashwani Yadav <ashyadavash@gmail.com>
2021-09-08 23:47:24 +0530

    Update Sum of min of all subarrays (Stacks).cpp

--- Arrays/Sum of min of all subarrays (Stacks).cpp
+++ Arrays/Sum of min of all subarrays (Stacks).cpp
@@ -4,6 +4,7 @@
 /*
 Given an array of integers arr, find the sum of min(b), where b ranges over every (contiguous) subarray of nums. 
 Since the answer may be large, return the answer modulo 10^9 + 7.
+
 Example 1:
 Input: arr = [3,1,2,4]
 Output: 17
@@ -11,10 +12,12 @@ Explanation:
 Subarrays are [3], [1], [2], [4], [3,1], [1,2], [2,4], [3,1,2], [1,2,4], [3,1,2,4]. 
 Minimums are 3, 1, 2, 4, 1, 1, 2, 1, 1, 1.
 Sum is 17.
+
 Example 2:
 Input: arr = [11,81,94,43,3]
 Output: 444
  
+
 Constraints:
 1 <= arr.length <= 3 * 10^4
 1 <= arr[i] <= 3 * 10^4
@@ -24,6 +27,7 @@ Constraints:
 /*
 Efficient Approach: 
 The general intuition for solution to the problem is to find sum(A[i] * f(i)), where f(i) is the number of subarrays in which A[i] is the minimum.
+
 In order to find f[i], we need to find out: 
 left[i], the length of strictly larger numbers on the left of A[i], 
 right[i], the length of larger numbers on the right of A[i].
@@ -31,20 +35,27 @@ We make two arrays left[ ] and right[ ] such that:
 left[i] + 1 equals to the number of subarrays ending with A[i], and A[i] is only single minimum. 
 Similarly, right[i] + 1 equals to the number of subarrays starting with A[i], and A[i] is first minimum.
 Finally, f(i) = (left[i]) * (right[i]), where f[i] equals total number of subarrays in which A[i] is minimum.
-Below is the implementation of above approach 
+Below is the implementation of above approach.
+
+The last thing that needs to be mentioned for handling duplicate elements:
+Method: Set strict less/greater and non-strict less/greater. The order doesn't matter.
 */
 
 
 
 /*
 Note equality and inequality can be included in either left or right side but not on both sides.
+
 To understand why we cannot take equality on both sides:
 nums = {1, 1, 1}
+
 If we take equality on both sides:
 res = 1*3 + 2*2 + 3*1 = 10
 which is wrong
+
 Correct (equality on right side)
 res = 1*3 + 1*2 + 1*1 = 6
+
 Correct (equality on left side)
 res = 1*1 + 1*2 + 1*3 = 6
 */
