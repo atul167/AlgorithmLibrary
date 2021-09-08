c34652cb0fd2e0dadfca6aab84d15dc0498159e5
Ashwani Yadav <ashyadavash@gmail.com>
2021-09-08 22:16:35 +0530

    Update Max of all subarrays of size k (Using Deque).cpp

--- Arrays/Max of all subarrays of size k (Using Deque).cpp
+++ Arrays/Max of all subarrays of size k (Using Deque).cpp
@@ -25,51 +25,51 @@ using namespace std;
 // A Dequeue (Double ended queue) based method for printing maximum element of all subarrays of size k
 void printKMax(int arr[], int n, int k) {
 
-    // Create a Double Ended Queue, Qi that will store indexes of array elements
+    // Create a Double Ended Queue, MX that will store indexes of array elements
     // The queue will store indexes of useful elements in every window 
-    // It will maintain decreasing order of values from front to rear in Qi, 
-    // i.e., arr[Qi.front[]] to arr[Qi.rear()] are sorted in decreasing order
-    deque<int> Qi(k);
+    // It will maintain decreasing order of values from front to rear in MX, 
+    // i.e., arr[MX.front[]] to arr[MX.rear()] are sorted in decreasing order
+    deque<int> MX;
 
     /* Process first k (or first window) elements of array */
     int i;
     for (i = 0; i < k; ++i) {
-        // For every element, the previous smaller elements are useless so remove them from Qi
-        while ((!Qi.empty()) && arr[i] >= arr[Qi.back()]) {
+        // For every element, the previous smaller elements are useless so remove them from MX
+        while ((!MX.empty()) && arr[i] >= arr[MX.back()]) {
             // Remove from rear
-            Qi.pop_back();
+            MX.pop_back();
         }
 
         // Add new element at rear of queue
-        Qi.push_back(i);
+        MX.push_back(i);
     }
 
     // The element at the front of
     // the queue is the largest element of
     // previous window, so print it
-    cout << arr[Qi.front()] << " ";
+    cout << arr[MX.front()] << " ";
 
     // Process rest of the elements, i.e., from arr[k] to arr[n-1]
     for (; i < n; ++i) {
         // Remove the elements which
         // are out of this window
-        while ((!Qi.empty()) && Qi.front() <= i - k) {
+        while ((!MX.empty()) && MX.front() <= i - k) {
             // Remove from front of queue
-            Qi.pop_front();
+            MX.pop_front();
         }
 
 
         // Remove all elements smaller than the currently being added element 
         // (remove useless elements)
-        while ((!Qi.empty()) && arr[i] >= arr[Qi.back()]) {
-            Qi.pop_back();
+        while ((!MX.empty()) && arr[i] >= arr[MX.back()]) {
+            MX.pop_back();
         }
 
-        // Add current element at the rear of Qi
-        Qi.push_back(i);
+        // Add current element at the rear of MX
+        MX.push_back(i);
 
         // Print the maximum element of last window
-        cout << arr[Qi.front()] << " ";
+        cout << arr[MX.front()] << " ";
     }
 }
 
