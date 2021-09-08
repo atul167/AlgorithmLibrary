b7237d62a9f51c7e64543b68247b7b6bb5242ae7
Ashwani Yadav <ashyadavash@gmail.com>
2021-09-08 17:53:04 +0530

    Create 16 Min time to burn a Tree.cpp

---
+++ Binary Trees/16 Min time to burn a Tree.cpp
@@ -0,0 +1,90 @@
+// https://www.codingninjas.com/codestudio/problems/time-to-burn-tree_630563
+
+/*
+You have been given a binary tree of unique nodes and a Start node from where the tree will start to burn. 
+Given that the Start node will always exist in the tree, your task is to print the time (in minutes) that it will take to burn the whole tree.
+It is given that it takes 1 minute for the fire to travel from the burning node to its adjacent node and burn down the adjacent node.
+For Example :
+For the given binary tree: [1, 2, 3, -1, -1, 4, 5, -1, -1, -1, -1]
+Start Node: 3
+
+    1
+   / \
+  2   3
+     / \
+    4   5
+
+Output: 2
+*/
+
+
+template<typename T>
+class BinaryTreeNode {
+public :
+    T data;
+    BinaryTreeNode < T > *left;
+    BinaryTreeNode < T > *right;
+    BinaryTreeNode(T data) {
+        this -> data = data;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+int res;   
+map<BinaryTreeNode<int>*, BinaryTreeNode<int>*> parent;
+map<BinaryTreeNode<int>*, int> vis;
+
+BinaryTreeNode<int>* x;
+
+void findParent(BinaryTreeNode<int>* node, int start) {
+    if(!node ) return;
+    
+    if(node->data == start) x = node;
+
+    if(node->left) {
+        parent[node->left] = node;
+        findParent(node->left, start);
+    }
+    if(node->right) {
+        parent[node->right] = node;
+        findParent(node->right, start);
+    }
+}
+
+void bfs() {
+    queue<pair<BinaryTreeNode<int>*, int>> q;
+    q.push({x, 0});
+    vis[x] = 1;
+    
+    while(!q.empty()) {
+        auto it = q.front();
+        q.pop();
+        BinaryTreeNode<int> *temp = it.first;
+        int dis = it.second;
+        debug2(temp->data, dis)
+        res = max(res, dis);
+        
+        if(temp->left && !vis[temp->left]) {
+            vis[temp->left] = 1;
+            q.push({temp->left, dis + 1});
+        }
+        if(temp->left && vis[temp->right]) {
+            vis[temp->right] = 1;
+            q.push({temp->right, dis + 1});
+        }
+        BinaryTreeNode<int>* par = parent[temp];
+        if(par && !vis[par]) {
+            vis[par] = 1;
+            q.push({par, dis + 1});
+        }
+    }
+}
+
+int timeToBurnTree(BinaryTreeNode<int>* root, int start) {
+    if( !root ) return 0;
+    findParent(root, start);
+    res = 0;
+    bfs();
+    return res;
+}
