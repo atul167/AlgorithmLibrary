815514a5a245d94159c49c41e4d5a583eeee2977
Ashwani Yadav <ashyadavash@gmail.com>
2021-09-09 09:16:25 +0530

    Update next_permutation.cpp

--- C++Inbuilt/next_permutation.cpp
+++ C++Inbuilt/next_permutation.cpp
@@ -1,11 +1,50 @@
-// Usage:
-
-/*
-next_permutation(s.begin(), s.end())
-*/
-
-
 // Print all permutaions of string
+
+
+// Method 1: My own nextPermutation
+bool nextPermutation(string& str) {
+    int n = str.size();
+    if(n == 0) return false;
+    
+    int i = n - 1;
+    while(i > 0 && str[i] <= str[i - 1]) {
+        i--;
+    }
+    
+    // no next permutation, i.e. already largest
+    if(i == 0) return false;
+    
+    int j = n - 1;
+    while(str[j] <= str[i - 1]) {
+        j--;
+    }
+    
+    swap(str[i - 1], str[j]);
+    reverse(str.begin() + i, str.end());
+    return true;
+}
+
+void solve() {
+    string s;
+    cin >> s;
+
+    // sorting is required
+    sort(s.begin(), s.end());
+    do {
+        cout << s << endl;
+    } while (nextPermutation(s));
+}
+
+
+
+
+
+
+
+
+
+
+// Method 2: Inbuilt next_permutation
 void solve() {
     string s;
     cin >> s;
