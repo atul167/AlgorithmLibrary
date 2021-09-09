8163aade76fcf5d4d5140d8ef2138939936fbf09
Ashwani Yadav <ashyadavash@gmail.com>
2021-09-09 09:33:43 +0530

    Update next_permutation.cpp

--- C++Inbuilt/next_permutation.cpp
+++ C++Inbuilt/next_permutation.cpp
@@ -5,22 +5,32 @@
 bool nextPermutation(string& str) {
     int n = str.size();
     if(n == 0) return false;
-    
+    // Find longest non-increasing suffix
     int i = n - 1;
-    while(i > 0 && str[i] <= str[i - 1]) {
+    while (i > 0 && str[i - 1] >= str[i]) {
         i--;
     }
+    // Now i is the head index of the suffix
     
-    // no next permutation, i.e. already largest
-    if(i == 0) return false;
+    // Are we at the last permutation already?
+    if (i == 0)
+        return false;
     
+    // Let str[i - 1] be the pivot
+    // Find rightmost element greater than the pivot
     int j = n - 1;
-    while(str[j] <= str[i - 1]) {
+    while (str[j] <= str[i - 1]) {
         j--;
     }
+    // Now the value str[j] will become the new pivot
+    // Assertion: j >= i
     
+    // Swap the pivot with j
     swap(str[i - 1], str[j]);
+    
+    // Reverse the suffix
     reverse(str.begin() + i, str.end());
+    // Successfully computed the next permutation
     return true;
 }
 
