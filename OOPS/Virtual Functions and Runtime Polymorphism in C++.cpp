16d5976cadf390bb56e0dded1bc04b05f3db8774
Ashwani Yadav <ashyadavash@gmail.com>
2021-09-09 13:37:07 +0530

    Update Virtual Functions and Runtime Polymorphism in C++.cpp

--- OOPS/Virtual Functions and Runtime Polymorphism in C++.cpp
+++ OOPS/Virtual Functions and Runtime Polymorphism in C++.cpp
@@ -1,6 +1,10 @@
 // https://www.geeksforgeeks.org/virtual-functions-and-runtime-polymorphism-in-c-set-1-introduction/
 
+
+
+
 // Real-life use case
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -130,6 +134,105 @@ Level:	3
 
 
 
+// Using Abstract Class
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Employee {
+public:
+    string name;
+    int salary;
+    int level;
+    Employee(string n, int s, int l) {
+        name = n;
+        salary = s;
+        level = l;
+    }
+    
+    virtual void raiseSalary() = 0;
+
+    virtual void promote() = 0;
+
+    void print() { 
+        cout << "Name:\t" << name << endl;
+        cout << "Salary:\t" << salary << endl;
+        cout << "Level:\t" << level << endl;
+        cout << endl;
+    }
+};
+
+class Manager : public Employee {
+public:
+    Manager(string n, int s, int l) : Employee(n, s, l) {}
+    virtual void raiseSalary() {
+        /* Manager specific raise salary code, may contain increment of manager specific incentives*/
+        cout << "Manager raise salary.\n";
+        salary += 100;
+
+    }
+    virtual void promote() {
+        /* Manager specific promote */
+        cout << "Manager promote.\n";
+        level += 2;
+    }
+};
+
+class Developer : public Employee {
+public:
+    Developer(string n, int s, int l) : Employee(n, s, l) {}
+    virtual void raiseSalary() {
+        /* Developer specific raise salary code, may contain increment of developer specific incentives*/
+        cout << "Developer raise salary.\n";
+        salary += 50;
+    }
+    virtual void promote() {
+        /* Developer specific promote */
+        cout << "Developer promote.\n";
+        level += 1;
+    }
+};
+
+// Similarly, there may be other types of employees
+
+
+// We need a very simple function to increment salary and promote of all employees
+// Note that emp is a vector of pointers and actual pointed objects can be any type of employees.
+// This function should ideally be in a class like Organization, we have made it global to keep things simple
+void globalRaiseSalary(vector<Employee*> &emp) {
+    for (int i = 0; i < emp.size(); i++) {
+        // Polymorphic Call: Calls raiseSalary() and promote() according to the actual object, not according to the type of pointer
+        emp[i]->raiseSalary();
+        emp[i]->promote();
+        emp[i]->print();
+    }
+}
+
+
+int main(void) {
+    Manager *m1 = new Manager("manager1", 200, 1);
+    Manager *m2 = new Manager("manager2", 500, 3);
+    Developer *d1 = new Developer("developer1", 100, 1);
+    Developer *d2 = new Developer("developer1", 400, 2);
+    
+    vector<Employee*> a = {m1, m2, d1, d2};
+    globalRaiseSalary(a);
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -204,11 +307,6 @@ int main(void) {
 Parent Class: Area = 25
 Parent Class: Area = 20
 */
-
-
-
-
-
 
 
 
