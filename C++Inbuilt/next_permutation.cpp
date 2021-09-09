4b658287f629005c2c2c31634cb16578cc0ef806
Ashwani Yadav <ashyadavash@gmail.com>
2021-09-09 09:50:22 +0530

    Update next_permutation.cpp

--- C++Inbuilt/next_permutation.cpp
+++ C++Inbuilt/next_permutation.cpp
@@ -102,11 +102,61 @@ NOTE: stoi  = for int
       stoll = for long long 
 */
 
+// Method 1
 class Solution {
 public:
     int nextGreaterElement(int n) {
         string str = to_string(n);
         next_permutation(str.begin(), str.end());
+        auto res = stoll(str);
+        
+        /*
+        if n = 554321 (non increasing type)
+        in this case next_permutation will return false and not modify str
+        and res will be equal to str
+        */ 
+        return (res > INT_MAX || res <= n) ? -1 : res;
+    }
+};
+
+
+// Method 2
+class Solution {
+public:
+    bool nextPermutation(string& str) {
+        int n = str.size();
+        if(n == 0) return false;
+        // Find longest non-increasing suffix
+        int i = n - 1;
+        while (i > 0 && str[i - 1] >= str[i]) {
+            i--;
+        }
+        // Now i is the head index of the suffix
+
+        // Are we at the last permutation already?
+        if (i == 0)
+            return false;
+
+        // Let str[i - 1] be the pivot
+        // Find rightmost element greater than the pivot
+        int j = n - 1;
+        while (str[j] <= str[i - 1]) {
+            j--;
+        }
+        // Now the value str[j] will become the new pivot
+        // Assertion: j >= i
+
+        // Swap the pivot with j
+        swap(str[i - 1], str[j]);
+
+        // Reverse the suffix
+        reverse(str.begin() + i, str.end());
+        // Successfully computed the next permutation
+        return true;
+    }
+    int nextGreaterElement(int n) {
+        string str = to_string(n);
+        nextPermutation(str);
         auto res = stoll(str);
         
         /*
