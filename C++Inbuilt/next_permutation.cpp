1f113c6ca8e9019dabfd42d957d46ecbc0d3abbe
Ashwani Yadav <ashyadavash@gmail.com>
2021-09-09 09:49:15 +0530

    Update next_permutation.cpp

--- C++Inbuilt/next_permutation.cpp
+++ C++Inbuilt/next_permutation.cpp
@@ -49,11 +49,6 @@ void solve() {
 
 
 
-
-
-
-
-
 // Method 2: Inbuilt next_permutation
 void solve() {
     string s;
@@ -64,3 +59,61 @@ void solve() {
         cout << s << endl;
     } while (next_permutation(s.begin(), s.end()));
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// https://leetcode.com/problems/next-greater-element-iii/
+// Next Greater Element III
+
+/*
+Given a positive integer n, find the smallest integer which has exactly the same digits existing in the integer n and is greater in value than n. 
+If no such positive integer exists, return -1.
+Note that the returned integer should fit in 32-bit integer, if there is a valid answer but it does not fit in 32-bit integer, return -1.
+
+Example 1:
+Input: n = 12
+Output: 21
+
+Example 2:
+Input: n = 21
+Output: -1
+
+Constraints:
+1 <= n <= 2^31 - 1
+
+
+NOTE: stoi  = for int
+      stoll = for long long 
+*/
+
+class Solution {
+public:
+    int nextGreaterElement(int n) {
+        string str = to_string(n);
+        next_permutation(str.begin(), str.end());
+        auto res = stoll(str);
+        
+        /*
+        if n = 554321 (non increasing type)
+        in this case next_permutation will return false and not modify str
+        and res will be equal to str
+        */ 
+        return (res > INT_MAX || res <= n) ? -1 : res;
+    }
+};
