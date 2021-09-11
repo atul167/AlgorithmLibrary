083a8381c0fd8453a8b9fd8fd351eeaa46e69b2c
Ashwani Yadav <ashyadavash@gmail.com>
2021-09-11 20:22:00 +0530

    Update 08 Maximum Product Subarray.cpp

--- Arrays/08 Maximum Product Subarray.cpp
+++ Arrays/08 Maximum Product Subarray.cpp
@@ -10,9 +10,14 @@ minp = minimum product of suffix (i, i+1, ..., j) ending at index j (i.e. includ
      = (arr[i] * arr[i+1] * ... * arr[j]), where i <= j
 
 (last index j is included same as we do in dp LIS) 
+
+
+NOTE: 
+Same problem can be extended to calculate Minimum Product Subarray
 */
 
 
+// Maximum Product Subarray
 class Solution {
 public:
     long long maxProduct(vector<int> arr, int n) {
@@ -24,6 +29,28 @@ public:
             maxp = max<long long int>(maxp * arr[i], arr[i]);
             minp = min<long long int>(minp * arr[i], arr[i]);
             ans = max<long long>(ans, maxp);
+        }
+        return ans;
+    }
+};
+
+
+
+
+
+
+// Minimum Product Subarray
+class Solution {
+public:
+    long long maxProduct(vector<int> arr, int n) {
+        long long ans, maxp, minp;
+        ans = maxp = minp = arr[0];
+        for (int i = 1; i < n; i++) {
+            if (arr[i] < 0) swap(maxp, minp);
+
+            maxp = max<long long int>(maxp * arr[i], arr[i]);
+            minp = min<long long int>(minp * arr[i], arr[i]);
+            ans = min<long long>(ans, minp);
         }
         return ans;
     }
