aa5639f7aadb188d0f19afb47505ed2e54519706
Ashwani Yadav <ashyadavash@gmail.com>
2021-09-12 21:43:37 +0530

    Update Max sum using no 2 consecutive ele.cpp

--- DP/00 Standard DP Que/Max sum using no 2 consecutive ele.cpp
+++ DP/00 Standard DP Que/Max sum using no 2 consecutive ele.cpp
@@ -1,44 +1,78 @@
+// Method 1: Space = O(1)
+class Solution {
+    public:
+    //Function to find the maximum money the thief can get.
+    int FindMaxSum(int arr[], int n)
+    {
+        // Your code here
+        int inc = arr[0];
+        int exc = 0;
+        
+        for(int i = 1; i < n; i++) {
+            int newExc = max(inc, exc);
+            inc = exc + arr[i];
+            exc = newExc;
+        }
+        
+        return max(inc, exc);
+    }
+};
 
-int goDp[N][2];
 
-int go(int pos, int state, vi& a) {
-	if(pos >= n) return 0;
 
-	if(goDp[pos][state] != -1) return goDp[pos][state]; 
 
-	int x = 0, y = 0;
-	if(state == 0) {
-		x = a[pos] + go(pos+1, 1, a);
-	}
-	y = go(pos+1, 0, a);
 
-	return goDp[pos][state] = max(x, y);
-}
 
-void solve() {
-	cin >> n;
-	vi a(n);
-	f(i, n) cin >> a[i];
-	// memset(goDp, -1, sizeof goDp);
-	// cout << go(0, 0, a) << endl;
 
-  	// DP soln
-	if(n == 1) {
-		cout << a[0] << endl;
-		return;
-	}
-	if(n == 2) {
-		cout << max(a[0], a[1]) << endl;
-		return;
-	}
-	
-	int dp[n+1] = {0};
-	dp[0] = a[0];
-	dp[1] = max(a[0], a[1]);
-	
-	fa(i, 2, n) {
-		dp[i] = max({dp[i-1], a[i] + dp[i-2]});
-	}
 
-	cout << dp[n-1] << endl;
-}
+// Method 2: Space = O(n)
+class Solution {
+    public:
+    //Function to find the maximum money the thief can get.
+    int FindMaxSum(int arr[], int n) {
+        if(n == 1) return arr[0];
+    	if(n == 2) return max(arr[0], arr[1]);
+    	
+    	int dp[n] = {0};
+    	dp[0] = arr[0];
+    	dp[1] = max(arr[0], arr[1]);
+    	
+    	for(int i = 2; i < n; i++) {
+    		dp[i] = max({dp[i-1], arr[i] + dp[i-2]});
+    	}
+    
+    	return dp[n-1];
+    }
+};
+
+
+
+
+// Method 2: Space = O(n)
+class Solution {
+public:
+    static const int N = 1e5+5;
+    int dp[N][2];
+    int n;
+
+    int go(int pos, int state, int a[]) {
+    	if(pos >= n) return 0;
+    
+    	if(dp[pos][state] != -1) return dp[pos][state]; 
+    
+    	int x = 0, y = 0;
+    	if(state == 0) {
+    		x = a[pos] + go(pos+1, 1, a);
+    	}
+    	y = go(pos+1, 0, a);
+    
+    	return dp[pos][state] = max(x, y);
+    }
+
+    //Function to find the maximum money the thief can get.
+    int FindMaxSum(int arr[], int n) {
+        this->n = n;
+        memset(dp, -1, sizeof dp);
+	    return go(0, 0, arr);
+    }
+};
