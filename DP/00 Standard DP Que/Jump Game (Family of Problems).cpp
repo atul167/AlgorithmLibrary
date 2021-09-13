5d6975f7b570b31a3934d81b8b43f2ca2b421bd9
Ashwani Yadav <ashyadavash@gmail.com>
2021-09-13 17:56:16 +0530

    Update Jump Game (Family of Problems).cpp

--- DP/00 Standard DP Que/Jump Game (Family of Problems).cpp
+++ DP/00 Standard DP Que/Jump Game (Family of Problems).cpp
@@ -1,5 +1,8 @@
 // https://leetcode.com/problems/jump-game/
 // https://leetcode.com/problems/jump-game-ii/
+// https://leetcode.com/problems/jump-game-iii/
+
+
 
 /*
 Jump game I:
@@ -12,18 +15,23 @@ class Solution {
 public:
     bool canJump(vector<int>& nums) {
         int n = nums.size();
-        int maxIndex = 0;
+        int maxReach = 0;
         
         for(int i = 0; i < n; i++) {
-            if(i > maxIndex) {
+            if(i > maxReach) {
                 return false;
             }
-            int reach = i + nums[i];
-            maxIndex = max(maxIndex, reach);
+            maxReach = max(maxReach, i + nums[i]);
         }
         return true;
     }
 };
+
+
+
+
+
+
 
 
 
@@ -94,11 +102,9 @@ public:
 
 
 
-
-
 // Method 3: Time = O(n), Space = O(1)
-
 // https://youtu.be/a9r98S-pDvk
+
 class Solution {
 public:
     int jump(vector<int>& nums) {
@@ -128,5 +134,228 @@ public:
 
         if (curReach < n - 1) return -1;
         return jumps;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+Jump game III:
+Given an array of non-negative integers arr, you are initially positioned at start index of the array. 
+When you are at index i, you can jump to i + arr[i] or i - arr[i], check if you can reach to any index with value 0.
+Notice that you can not jump outside of the array at any time.
+*/
+
+class Solution {
+public:
+    bool dfs(vector<int>& arr, int i) {
+        if (i < 0 || i >= arr.size() || arr[i] < 0) return false;
+        if (arr[i] == 0) return true;
+        
+        // mark visited by making negative
+        arr[i] = -arr[i];
+        
+        return dfs(arr, i + arr[i]) || dfs(arr, i - arr[i]);
+    }
+    
+    bool canReach(vector<int>& arr, int start) {
+        return dfs(arr, start);
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+Jump game IV:
+
+Given an array of integers arr, you are initially positioned at the first index of the array.
+
+In one step you can jump from index i to index:
+a) i + 1 where: i + 1 < arr.length.
+b) i - 1 where: i - 1 >= 0.
+c) j where: arr[i] == arr[j] and i != j.
+
+Return the minimum number of steps to reach the last index of the array.
+Notice that you can not jump outside of the array at any time.
+*/
+
+// Method 1
+class Solution {
+public:
+    int minJumps(vector<int>& arr) {
+        int n = arr.size();
+        unordered_map<int, set<int>> neighbours;
+        
+        for(int i = 0; i < n; i++) {
+            neighbours[arr[i]].insert(i);
+        }
+
+        queue<pair<int, int>> q;
+        q.push({0, 0});
+        
+        vector<int> vis(n);
+        vis[0] = 1;
+        
+        while(!q.empty()) {
+            auto it = q.front();
+            q.pop();
+            int u = it.first, lvl = it.second;
+            
+            if(u == n - 1) return lvl;
+            
+            if(u - 1 >= 0 && !vis[u - 1]) {
+                q.push({u - 1, lvl + 1});
+                vis[u - 1] = 1;
+            }
+            if(u + 1 < n && !vis[u + 1]) {
+                q.push({u + 1, lvl + 1});
+                vis[u + 1] = 1;
+            }
+            
+            for(int i: neighbours[arr[u]]) {
+                if(i != u && !vis[i]) {
+                    q.push({i, lvl + 1});
+                    vis[i] = 1;
+                    neighbours[arr[u]].erase(i);
+                }
+            }
+        }
+        
+        return -1;
+    }
+};
+
+
+
+// Method 2
+class Solution {
+public:
+    int minJumps(vector<int>& arr) {
+        int n = arr.size();
+        unordered_map<int, vector<int>> neighbours;
+        
+        for(int i = 0; i < n; i++) {
+            neighbours[arr[i]].push_back(i);
+        }
+
+        queue<pair<int, int>> q;
+        q.push({0, 0});
+        
+        vector<int> vis(n);
+        vis[0] = 1;
+        
+        while(!q.empty()) {
+            auto it = q.front();
+            q.pop();
+            int u = it.first, lvl = it.second;
+            
+            if(u == n - 1) return lvl;
+            
+            if(u - 1 >= 0 && !vis[u - 1]) {
+                q.push({u - 1, lvl + 1});
+                vis[u - 1] = 1;
+            }
+            if(u + 1 < n && !vis[u + 1]) {
+                q.push({u + 1, lvl + 1});
+                vis[u + 1] = 1;
+            }
+            
+            for(int i: neighbours[arr[u]]) {
+                if(i != u && !vis[i]) {
+                    q.push({i, lvl + 1});
+                    vis[i] = 1;
+                }
+            }
+            
+            // remove for next iterations since already visited
+            neighbours[arr[u]].clear();
+        }
+        
+        return -1;
+    }
+};
+
+
+
+// Method 3
+class Solution {
+public:
+    int minJumps(vector<int>& arr) {
+        int n = arr.size();
+        unordered_map<int, vector<int>> neighbours;
+        
+        for(int i = 0; i < n; i++) {
+            neighbours[arr[i]].push_back(i);
+        }
+
+        queue<pair<int, int>> q;
+        q.push({0, 0});
+        
+        vector<int> vis(n);
+        vis[0] = 1;
+        int level = 0;
+        
+        while(!q.empty()) {
+            int sz = q.size();
+            while(sz--) {
+                auto it = q.front();
+                q.pop();
+                int u = it.first, lvl = it.second;
+
+                if(u == n - 1) return lvl;
+
+                if(u - 1 >= 0 && !vis[u - 1]) {
+                    q.push({u - 1, lvl + 1});
+                    vis[u - 1] = 1;
+                }
+                if(u + 1 < n && !vis[u + 1]) {
+                    q.push({u + 1, lvl + 1});
+                    vis[u + 1] = 1;
+                }
+
+                for(int i: neighbours[arr[u]]) {
+                    if(i != u && !vis[i]) {
+                        q.push({i, lvl + 1});
+                        vis[i] = 1;
+                    }
+                }
+
+                // remove for next iterations since already visited
+                neighbours[arr[u]].clear();
+            }
+            
+            level++;
+        }
+        
+        return -1;
     }
 };
