e60f6acc81befc71c27e2fc66c342f023f357bfe
Ashwani Yadav <ashyadavash@gmail.com>
2021-09-13 16:07:13 +0530

    Update Minimum Window Subsequence.cpp

--- 2 Pointers & Sliding Window/Minimum Window Subsequence.cpp
+++ 2 Pointers & Sliding Window/Minimum Window Subsequence.cpp
@@ -23,6 +23,82 @@ Note: "deb" is not a smaller window because the elements of T in the window must
 */
 
 
+
+
+
+class Solution {
+public:
+    /**
+     * @param S: a string
+     * @param T: a string
+     * @return: the minimum substring of S
+     */
+    string minWindow(string &S, string &T) {
+        // Write your code here
+        int n = S.size(), m = T.size();
+        if (n == 0 ||  m == 0) return "";
+         
+        int dp[n + 1][m + 1];
+        
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = i;
+        }
+
+        for (int j = 1; j <= m; j++) {
+            dp[0][j] = -1;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                dp[i][j] = -1;
+                 
+                if (S[i - 1] == T[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+
+        int minLen = INT_MAX;
+        int startPos = -1;
+
+        for (int i = 1; i <= n; i++) {
+            if (dp[i][m] != -1) {
+                int len = i - dp[i][m];
+                if (len < minLen) {
+                    startPos = dp[i][m];
+                    minLen = len;
+                }
+            }
+        }
+         
+        return startPos == -1 ? "" : S.substr(startPos, minLen);
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 class Solution {
 public:
     /**
