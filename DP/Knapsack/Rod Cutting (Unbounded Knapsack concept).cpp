5bd5c9557de9a61d0a5e44c3b9e1d8c10d264f66
Ashwani Yadav <ashyadavash@gmail.com>
2021-09-14 19:11:53 +0530

    Update Rod Cutting (Unbounded Knapsack concept).cpp

--- DP/Knapsack/Rod Cutting (Unbounded Knapsack concept).cpp
+++ DP/Knapsack/Rod Cutting (Unbounded Knapsack concept).cpp
@@ -43,12 +43,6 @@ class Solution{
 
 
 
-
-
-
-
-
-
 // Method 2: Unbounded Knapsack concept O(m) memory
 class Solution{
   public:
@@ -82,32 +76,23 @@ class Solution{
 
 
 // Method 3: O(m) memory
-class Solution{
-  public:
+class Solution {
+public:
     int cutRod(int price[], int n) {
         // dp[i] denotes the max value of rod length i, so answer = dp[n]
-        vector<int> dp(n+1, 0);
-        
+        vector<int> dp(n + 1, 0);
+
         dp[0] = 0; // when no element is included
-        dp[1] = price[0]; // when only first element is included
-        
-        for(int i = 2; i <= n; i++) {
-            for(int j = 0; j < i; j++) {
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
                 dp[i] = max(dp[i], dp[j] + price[i - j - 1]);
             }
         }
-        
+
         return dp[n];
     }
 };
-
-
-
-
-
-
-
-
 
 
 
