707e1864f65cbcd1a787d81d086bd82a31e017c1
Ashwani Yadav <ashyadavash@gmail.com>
2021-09-14 21:13:54 +0530

    Create Burst Balloons.cpp

---
+++ DP/Matrix Chain Multiplication/Burst Balloons.cpp
@@ -0,0 +1,50 @@
+// https://leetcode.com/problems/burst-balloons/
+
+/*
+You are given n balloons, indexed from 0 to n - 1. Each balloon is painted with a number on it represented by an array nums. 
+You are asked to burst all the balloons.
+If you burst the ith balloon, you will get nums[i - 1] * nums[i] * nums[i + 1] coins. 
+If i - 1 or i + 1 goes out of bounds of the array, then treat it as if there is a balloon with a 1 painted on it.
+Return the maximum coins you can collect by bursting the balloons wisely.
+
+Example 1:
+Input: nums = [3,1,5,8]
+Output: 167
+Explanation:
+nums = [3,1,5,8] --> [3,5,8] --> [3,8] --> [8] --> []
+coins =  3*1*5    +   3*5*8   +  1*3*8  + 1*8*1 = 167
+
+Example 2:
+Input: nums = [1,5]
+Output: 10
+*/
+
+
+class Solution {
+public:
+    int maxCoins(vector<int>& nums) {
+        int n = nums.size();
+
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+
+        for (int gap = 0; gap < n; gap++) {
+            for (int i = 0, j = gap;  j < n;  i++, j++) {
+                if (gap == 0) {
+                    dp[i][j] = (i == 0 ? 1 : nums[i - 1]) * nums[i] * (j == n - 1 ? 1 : nums[j + 1]);
+                } else {
+                    dp[i][j] = 0;
+                    for (int k = i; k <= j; k++) {
+                        int left  = (k == i) ? 0 : dp[i][k - 1];
+                        int right = (k == j) ? 0 : dp[k + 1][j];
+
+                        int curr = nums[k] * ((i == 0) ? 1 : nums[i - 1]) * ((j == n - 1) ? 1 : nums[j + 1]);
+
+                        dp[i][j]  = max(dp[i][j], left + curr + right);
+                    }
+                }
+            }
+        }
+
+        return dp[0][n - 1];
+    }
+};
