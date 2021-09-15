931fc7155aab4efeeebd1fb49ce207fdaf180631
Ashwani Yadav <ashyadavash@gmail.com>
2021-09-15 20:40:31 +0530

    Update 13 Subtraction 2 Linkedlist (O(1) extra space).cpp

--- LinkedList/13 Subtraction 2 Linkedlist (O(1) extra space).cpp
+++ LinkedList/13 Subtraction 2 Linkedlist (O(1) extra space).cpp
@@ -27,15 +27,13 @@ Node* reverseList(Node* head) {
     return head;
 }
 
-Node* Subtract(struct Node* h1, struct Node* h2) {
-    if (!h1 or !h2) return h1;
-
-    Node* rH1 = reverseList(h1);
-    Node* rH2 = reverseList(h2);
-
-    Node* temp1 = rH1;
-    Node* temp2 = rH2;
-
+struct Node* Subtract(struct Node* h1, struct Node* h2) {
+    if (!h1 || !h2) return h1;
+    Node *rH1 = reverseList(h1);
+    Node *rH2 = reverseList(h2);
+    Node *temp1 = rH1;
+    Node *temp2 = rH2;
+    
     int carry = 0;
     while (temp1 && temp2) {
         int cur1 = (temp1 -> data);
@@ -48,32 +46,47 @@ Node* Subtract(struct Node* h1, struct Node* h2) {
             carry = 1;
             cur1 += 10;
         }
+
         int res = cur1 - cur2;
         temp1 -> data = res;
         temp1 = temp1 -> next;
         temp2 = temp2 -> next;
     }
-    if (carry) {
-        temp1 -> data = (temp1 -> data) - 1;
+    while (temp1) {
+        int cur1 = (temp1 -> data);
+        int cur2 = 0;
+        if (carry) {
+            cur1--;
+            carry = 0;
+        }
+        if (cur1 < cur2) {
+            carry = 1;
+            cur1 += 10;
+        }
+
+        int res = cur1 - cur2;
+        temp1 -> data = res;
+        temp1 = temp1 -> next;
     }
     rH1 = reverseList(rH1);
-    while (rH1 != NULL and rH1 -> data == 0) {
+    while (rH1 -> next && rH1 -> data == 0) {
         rH1 = rH1 -> next;
     }
     return rH1;
 }
+
 int32_t main() {
-    Node* head1 = newNode(9);
+    Node* head1 = newNode(1);
     head1->next = newNode(2);
     head1->next->next = newNode(3);
 
-    Node* head2 = newNode(9);
+    Node* head2 = newNode(1);
     head2->next = newNode(2);
     head2->next->next = newNode(0);
 
     head1 = Subtract(head1, head2);
 
-    if (head1 == NULL) cout << 0 << endl;
+    if (!head1) cout << 0 << endl;
 
     while (head1) {
         cout << head1 -> data << " ";
