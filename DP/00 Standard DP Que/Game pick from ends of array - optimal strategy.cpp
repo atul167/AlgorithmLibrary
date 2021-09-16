855ed9686efdabadf62e7d2c638af838f215ec4b
Ashwani Yadav <ashyadavash@gmail.com>
2021-09-16 22:55:06 +0530

    Update Game pick from ends of array - optimal strategy.cpp

--- DP/00 Standard DP Que/Game pick from ends of array - optimal strategy.cpp
+++ DP/00 Standard DP Que/Game pick from ends of array - optimal strategy.cpp
@@ -1,3 +1,4 @@
+// Que 1:
 // https://leetcode.com/problems/predict-the-winner/
 
 /*
@@ -64,10 +65,10 @@ public:
 
 
 
+// Que 2:
+// https://practice.geeksforgeeks.org/problems/optimal-strategy-for-a-game-1587115620/1
+
 /*
-https://practice.geeksforgeeks.org/problems/optimal-strategy-for-a-game-1587115620/1
-
-
 You are given an array A of size N. The array contains integers and is of even length. 
 The elements of the array represent N coin of values V1, V2, ....Vn. You play against an opponent in an alternating way.
 In each turn, a player selects either the first or last coin from the row, removes it from the row permanently, and receives the value of the coin.
@@ -89,6 +90,8 @@ Output: 22
 Explanation: The user collects maximum value as 22(7 + 15)
 */
 
+
+// Method 1
 class Solution {
 public:
     #define ll long long
@@ -132,5 +135,48 @@ public:
         */
 
         return (sum + x) / 2;
+    }
+};
+
+
+
+
+
+
+
+// Method 2
+class Solution {
+public:
+#define ll long long
+
+    // minimax returns player1score - player2score
+    ll minimax(int left, int right, bool isMax, int nums[], vector<vector<vector<ll>>>& dp) {
+        // Terminating condition
+        if (left > right) {
+            return 0;
+        }
+
+        if (dp[left][right][isMax] != -1) return dp[left][right][isMax];
+
+        //  If current move is maximizer, find the maximum attainable value
+        if (isMax) {
+            ll l = nums[left] + minimax(left + 1, right, false, nums, dp);
+            ll r = nums[right] + minimax(left, right - 1, false, nums, dp);
+            return dp[left][right][isMax] = max(l, r);
+        }
+
+        // Else (If current move is Minimizer), find the minimum attainable value
+        else {
+            ll l = minimax(left + 1, right, true, nums, dp);
+            ll r = minimax(left, right - 1, true, nums, dp);
+            return dp[left][right][isMax] = min(l, r);
+        }
+    }
+
+    long long maximumAmount(int arr[], int n) {
+        vector<vector<vector<ll>>> dp(n + 5, vector<vector<ll>>(n + 5, vector<ll>(2, -1)));
+        ll x = minimax(0, n - 1, 1, arr, dp);
+
+        return x;
     }
 };
