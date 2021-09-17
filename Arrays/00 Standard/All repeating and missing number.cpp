47f7c5b2a4bba02bc916b50e4e5e8d21bd1dfdb2
Ashwani Yadav <ashyadavash@gmail.com>
2021-09-17 17:25:12 +0530

    Create All repeating and missing number.cpp

---
+++ Arrays/00 Standard/All repeating and missing number.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void printRepeatingAndMissing(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        while (arr[i] != i + 1) {
+            if (arr[i] == arr[arr[i] - 1]) {
+                // cout << "Repeating: " << arr[i] << endl;
+                break;
+            }
+            swap(arr[i], arr[arr[i] - 1]);
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] != i + 1) {
+            cout << "Repeating: " << arr[i] << endl;
+            cout << "Missing: " << i + 1 << endl;
+        }
+    }
+}
+
+int main() {
+    int arr[] = { 1, 2, 3, 8, 2, 3, 5, 1 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+    printRepeatingAndMissing(arr, n);
+}
