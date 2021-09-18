ac0fd5f8950f5aa83e68e2c9367e1c26c9b83360
Ashwani Yadav <ashyadavash@gmail.com>
2021-09-18 21:32:20 +0530

    Update 03 Longest Sub-Array with Sum K.cpp

--- Arrays/03 Longest Sub-Array with Sum K.cpp
+++ Arrays/03 Longest Sub-Array with Sum K.cpp
@@ -30,3 +30,47 @@ public:
         return maxLen;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Solution {
+public:
+    int lenOfLongSubarr(int arr[],  int n, int k)  {
+        unordered_map<int, int> mp;
+        int sum = 0, maxLen = 0, startIdx, endIdx;
+
+        mp[0] = -1;
+
+        // traverse the given array
+        for (int i = 0; i < n; i++) {
+            sum += arr[i];
+  
+            if (mp.find(sum - k) != mp.end()) {
+                if (maxLen < (i - mp[sum - k])) {
+                    maxLen = i - mp[sum - k];
+                    startIdx = mp[sum - k] + 1;
+                    endIdx = i;
+                }
+            }
+
+            if (mp.find(sum) == mp.end()) mp[sum] = i;
+        }
+
+        // required maximum length
+        return maxLen;
+    }
+};
