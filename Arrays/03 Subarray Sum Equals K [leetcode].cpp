8efdaf141cfdc9b088a9c4004f13218ba0063b18
Ashwani Yadav <ashyadavash@gmail.com>
2021-09-18 22:44:07 +0530

    Update 03 Subarray Sum Equals K [leetcode].cpp

--- Arrays/03 Subarray Sum Equals K [leetcode].cpp
+++ Arrays/03 Subarray Sum Equals K [leetcode].cpp
@@ -16,12 +16,36 @@ public:
         int prefSum = 0;
         map<int, int> mp;
         // to take into account those subarrays that begin with index 0
-        mp[0]++;
+        mp[0] = 1;
         
         int res = 0;
         
         for(int i = 0; i < nums.size(); i++){
             prefSum += nums[i];
+            res += mp[prefSum - k];
+            mp[prefSum]++;
+        }
+        return res;
+    }
+};
+
+
+
+
+
+
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int prefSum = 0;
+        map<int, int> mp;
+        
+        int res = 0;
+        
+        for(int i = 0; i < nums.size(); i++){
+            prefSum += nums[i];
+            if(prefSum == k) res++;
+            
             res += mp[prefSum - k];
             mp[prefSum]++;
         }
