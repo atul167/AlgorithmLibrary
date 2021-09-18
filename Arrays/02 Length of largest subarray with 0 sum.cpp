deec7373b82ef5dc23bbb921d547ae64a6a13dec
Ashwani Yadav <ashyadavash@gmail.com>
2021-09-18 23:15:35 +0530

    Update 02 Length of largest subarray with 0 sum.cpp

--- Arrays/02 Length of largest subarray with 0 sum.cpp
+++ Arrays/02 Length of largest subarray with 0 sum.cpp
@@ -1,38 +1,63 @@
 // https://www.geeksforgeeks.org/find-the-largest-subarray-with-0-sum/
+// https://practice.geeksforgeeks.org/problems/largest-subarray-with-0-sum/1
 
 
-void solve() {
-    cin >> n;
-    vector<int> arr(n);
-    f(i, n) cin >> arr[i];
+class Solution {
+public:
+    int maxLen(vector<int>& arr, int n) {
+        // Map to store the previous sums
+        unordered_map<int, int> prevSums;
 
-    // Map to store the previous sums 
-    unordered_map<int, int> prevSums; 
-  
-    int sum = 0; // Initialize the sum of elements 
-    int max_len = 0; // Initialize result 
-  
-    // Traverse through the given array 
-    for (int i = 0; i < n; i++) { 
-        // Add current element to sum 
-        sum += arr[i]; 
-  
-        if (arr[i] == 0 && max_len == 0) {
-            max_len = 1; 
+        int sum = 0;
+        int max_len = 0;
+
+        // Traverse through the given array
+        for (int i = 0; i < n; i++) {
+            sum += arr[i];
+
+            if (sum == 0) {
+                max_len = i + 1;
+            }
+
+            if (prevSums.find(sum) != prevSums.end()) {
+                max_len = max(max_len, i - prevSums[sum]);
+            }  else {
+                prevSums[sum] = i;
+            }
         }
-        if (sum == 0) {
-            max_len = i + 1; 
+
+        return max_len;
+    }
+};
+
+
+
+
+
+
+
+class Solution {
+public:
+    int maxLen(vector<int>& arr, int n) {
+        // Map to store the previous sums
+        unordered_map<int, int> prevSums;
+        
+        prevSums[0] = -1;
+
+        int sum = 0;
+        int max_len = 0;
+
+        // Traverse through the given array
+        for (int i = 0; i < n; i++) {
+            sum += arr[i];
+
+            if (prevSums.find(sum) != prevSums.end()) {
+                max_len = max(max_len, i - prevSums[sum]);
+            }  else {
+                prevSums[sum] = i;
+            }
         }
-  
-        // Look for this sum in Hash table 
-        if (prevSums.find(sum) != prevSums.end()) { 
-            // If this sum is seen before, then update max_len 
-            max_len = max(max_len, i - prevSums[sum]); 
-        }  else { 
-            // Else insert this sum with index in hash table 
-            prevSums[sum] = i; 
-        } 
-    } 
-  
-    cout << max_len; 
-}
+
+        return max_len;
+    }
+};
