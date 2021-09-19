3e6432bcbb93afd10a1e02d74f1f31d1f0f8ab05
Ashwani Yadav <ashyadavash@gmail.com>
2021-09-19 18:05:29 +0530

    Update 02 Koko Eating Bananas.cpp

--- Searching n Sorting/02 Koko Eating Bananas.cpp
+++ Searching n Sorting/02 Koko Eating Bananas.cpp
@@ -1,12 +1,21 @@
 // https://leetcode.com/problems/koko-eating-bananas/
 
+/*
+Koko loves to eat bananas. There are n piles of bananas, the ith pile has piles[i] bananas. The guards have gone and will come back in h hours.
+Koko can decide her bananas-per-hour eating speed of k. Each hour, she chooses some pile of bananas and eats k bananas from that pile. 
+If the pile has less than k bananas, she eats all of them instead and will not eat any more bananas during this hour.
+Koko likes to eat slowly but still wants to finish eating all the bananas before the guards return.
+Return the minimum integer k such that she can eat all the bananas within h hours.
+*/
+
+
 // FFFFFFTTTTTTTTTT
 
 class Solution {
 public:
     bool check(int mid, int H, vector<int>& piles) {
         int total = 0;
-        for (int p : piles) {
+        for (int p: piles) {
             total += (p + mid - 1) / mid;
         }
 
