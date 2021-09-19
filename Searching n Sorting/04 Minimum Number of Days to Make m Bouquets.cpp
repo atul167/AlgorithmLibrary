326096c11141f110b9d00790471309f47e7bc9b7
Ashwani Yadav <ashyadavash@gmail.com>
2021-09-19 18:15:59 +0530

    Update 04 Minimum Number of Days to Make m Bouquets.cpp

--- Searching n Sorting/04 Minimum Number of Days to Make m Bouquets.cpp
+++ Searching n Sorting/04 Minimum Number of Days to Make m Bouquets.cpp
@@ -16,28 +16,28 @@ public:
         int n = bloomDay.size();
         int consecutiveFlowers = 0, noOfBouq = 0;
         for (int i = 0; i < n; i++) {
-            if (bloomDay[i] > mid) {
-                consecutiveFlowers = 0;
-            } else {
+            if (bloomDay[i] <= mid) {
                 consecutiveFlowers++;
                 if (consecutiveFlowers == k) {
                     noOfBouq++;
                     consecutiveFlowers = 0;
                 }
+            } else {
+                consecutiveFlowers = 0;
             }
         }
         return noOfBouq >= m;
     }
-    
+
     int minDays(vector<int>& bloomDay, int m, int k) {
         int n = bloomDay.size();
         if (m * k > n) return -1;
-        
+
         int lo = 1, hi = *max_element(bloomDay.begin(), bloomDay.end());
-        
+
         while (lo < hi) {
             int mid = (lo + hi) / 2;
-            
+
             if (check(mid, bloomDay, m, k)) {
                 hi = mid;
             } else {
