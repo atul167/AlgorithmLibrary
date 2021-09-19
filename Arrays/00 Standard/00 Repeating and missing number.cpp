da4e02efa1d4e0b3e20ac5fe0d83c8211aa54930
Ashwani Yadav <ashyadavash@gmail.com>
2021-09-19 12:23:26 +0530

    Update 00 Repeating and missing number.cpp

--- Arrays/00 Standard/00 Repeating and missing number.cpp
+++ Arrays/00 Standard/00 Repeating and missing number.cpp
@@ -5,6 +5,21 @@ Aditya Verma Playlist
 Swap Sort Concept
 */
 
+/*
+Given an unsorted array of size n. Array elements are in the range from 1 to n. 
+One number from set {1, 2, …n} is missing and one number occurs twice in the array. Find these two numbers.
+
+Input: 
+arr[] = {3, 1, 3}
+Output: 
+Missing = 2, Repeating = 3
+
+Input: 
+arr[] = {4, 3, 6, 2, 1, 1}
+Output: 
+Missing = 5, Repeating = 1
+*/
+
 
 #include <bits/stdc++.h>
 using namespace std;
