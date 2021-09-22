d0b2ae36e9ec3bab2375bff19ca8689929f5914f
Ashwani Yadav <ashyadavash@gmail.com>
2021-09-23 00:40:28 +0530

    Update Max Sum Subarray with at least Size k.cpp

--- DP/Kadane’s Algorithm/Max Sum Subarray with at least Size k.cpp
+++ DP/Kadane’s Algorithm/Max Sum Subarray with at least Size k.cpp
@@ -33,25 +33,25 @@ int maxSumWithK(int a[], int n, int k) {
     for (int i = 0; i < k; i++) {
         sum += a[i];
     }
+    int res = sum;
 
     // Use the concept of sliding window
-    int result = sum;
     for (int i = k; i < n; i++) {
         // Compute sum of k elements ending with a[i].
         sum = sum + a[i] - a[i - k];
 
-        // Update result if required
-        result = max(result, sum);
+        // Update res if required
+        res = max(res, sum);
 
         // Include maximum sum till [i - k] also if it increases overall max.
-        result = max(result, sum + maxSum[i - k]);
+        res = max(res, sum + maxSum[i - k]);
     }
 
-    return result;
+    return res;
 }
 
 int main() {
-    int a[] = {1, 2, 3, -10, -3};
+    int a[] = {1, 2, 3, -5, -3};
     int k = 4;
     int n = sizeof(a) / sizeof(a[0]);
     cout << maxSumWithK(a, n, k);
