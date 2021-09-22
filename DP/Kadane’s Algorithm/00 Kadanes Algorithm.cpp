02ceba6aa99ba6fd70c50f5f2089213340f6cd23
Ashwani Yadav <ashyadavash@gmail.com>
2021-09-23 00:34:34 +0530

    Update 00 Kadanes Algorithm.cpp

--- DP/Kadane’s Algorithm/00 Kadanes Algorithm.cpp
+++ DP/Kadane’s Algorithm/00 Kadanes Algorithm.cpp
@@ -1,17 +1,10 @@
 // https://www.techiedelight.com/print-continuous-subarray-with-maximum-sum/
 
-// Function to print contiguous subarray with the largest sum in a given set of integers
 void kadane() {
     vector < int > arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
     int n = arr.size();
 
-    // stores maximum sum subarray found so far
-    int max_so_far = 0;
-
-    // stores the maximum sum of subarray ending at the current position
-    int max_ending_here = 0;
-
-    // stores endpoints of maximum sum subarray found so far
+    int max_so_far = INT_MIN, max_ending_here = 0;
     int start = 0, end = 0;
 
     // stores starting index of a positive-sum sequence
@@ -22,24 +15,52 @@ void kadane() {
         // update the maximum sum of subarray "ending" at index `i`
         max_ending_here = max_ending_here + arr[i];
 
-        // if the maximum sum is negative, set it to 0
-        if (max_ending_here < 0) {
-            max_ending_here = 0; // empty subarray
-            beg = i + 1;
-        }
-
         // update result if the current subarray sum is found to be greater
         if (max_so_far < max_ending_here) {
             max_so_far = max_ending_here;
             start = beg;
             end = i;
         }
+
+        // if the maximum sum is negative, set it to 0
+        if (max_ending_here < 0) {
+            max_ending_here = 0;
+            beg = i + 1;
+        }
     }
 
-    cout << "The sum of contiguous subarray with the largest sum is " << max_so_far << endl;
+    cout << "Sum = " << max_so_far << endl;
 
-    cout << "The contiguous subarray with the largest sum is ";
+    cout << "Subarray = ";
     for (int i = start; i <= end; i++) {
         cout << arr[i] << " ";
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Method 2
+class Solution {
+public:
+    long long maxSubarraySum(int a[], int n) {
+        long long max_so_far = a[0];
+        long long curr_max = a[0];
+
+        for (int i = 1; i < n; i++) {
+            curr_max = max<long long>(a[i], curr_max + a[i]);
+            max_so_far = max<long long>(max_so_far, curr_max);
+        }
+
+        return max_so_far;
+    }
+};
