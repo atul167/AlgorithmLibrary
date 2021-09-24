747764b2baa92405897a66ca6731f7d0b2423553
Ashwani Yadav <ashyadavash@gmail.com>
2021-09-24 12:45:35 +0530

    Update Max sum using no more than k consecutive ele.cpp

--- DP/00 Standard DP Que/Max sum using no more than k consecutive ele.cpp
+++ DP/00 Standard DP Que/Max sum using no more than k consecutive ele.cpp
@@ -107,7 +107,7 @@ void solve() {
 // https://www.hackerrank.com/challenges/billboards/editorial
 
 /*
-Instead of getting the maximum profit, it is equivalent to consider losing the minimum profit. Here, an Time = O(n * k) DP solution is straightforward.
+Instead of getting the maximum profit, it is equivalent to consider losing the minimum profit.
 
 Let dp[i] denote, for given the first i billboards, the minimum profit we have to sacrifice. 
 Obviously, dp[i] = 0 if i <= k. 
@@ -115,6 +115,55 @@ When i > k, we have to give up some billboards. Specifically, among the rightmos
 (For a concrete example, see the figure below, where n = 8, and k = 2. Then, at least one of the three red billboards has to be abandoned)
 */
 
+// Time = O(n * k), Space = O(n)
+const int N = 1e5 + 5;
+int n, m, k;
+
+void solve() {
+    cin >> n >> k;
+    vector<int> a = vector<int>(n + 1);
+    int sum = 0;
+    for (int i = 1; i <= n; i++) cin >> a[i], sum += a[i];
+
+    vector<int> dp(n + 1, INF);
+    dp[0] = 0;
+    for (int i = 1; i <= k; i++) {
+        dp[i] = 0;
+    }
+
+    for (int i = k + 1; i <= n; i++) {
+        for (int j = i; j >= i - k; j--) {
+            dp[i] = min(dp[i], a[j] + dp[j - 1]);
+        }
+    }
+
+    cout << sum - dp[n] << endl;
+}
+
+
+
+
+
+
+/*
+An O(n) solution using monotone priority queue.
+Luckily, there exists a much simpler (also faster) solution by using a monotone priority queue instead. 
+A monotone priority queue, say dq, is simply a deque with the following property.
+
+When adding g[1], g[2], g[3], ... to dq:
+a) The most recent data is at the tail.
+b) The least recent data is at the head.
+c) If the head element is already out of date w.r.t. the current dp[i], remove it from the head.
+d) Before adding new element from the tail, we check out whether that element is less than or equal to dp.tail. 
+   If so, remove that element from the tail (since that element is old and can never be better than the one to be inserted, hence is useless). 
+   Repeat this process until tail element is strictly less than the one to be added or dq is empty. 
+   This will guarantee that the data in dq is always an increasing sequence. Hence, at any time, dq.head is simply the answer w.r.t. the current dp[i].
+
+That is it! And if you want to know the runtime, it is indeed O(n) by a simple amortized analysis, 
+i.e., each element will be inserted into (deleted from) the queue for at most once, hence O(n).
+*/
+
+// Time = O(n), Space = O(n)
 const int N = 1e5 + 5;
 int n, m, k;
 
@@ -125,16 +174,25 @@ void solve() {
     for (int i = 1; i <= n; i++) cin >> a[i], sum += a[i];
 
     vector<int> dp(n + 1);
-    dp[0] = 0;
-    for (int i = 1; i <= k; i++) {
-        dp[i] = 0;
-    }
 
-    for (int i = k + 1; i <= n; i++) {
-        dp[i] = INF;
-        for (int j = i; j >= i - k; j--) {
-            dp[i] = min(dp[i], a[j] + dp[j - 1]);
+    function<int(int)> get = [&a, &dp](int i)->int {
+        return a[i] + dp[i - 1];
+    };
+
+    deque<int> dq;
+    for (int i = 1; i <= k; i++) {
+        while (!dq.empty() && get(i) <= get(dq.back())) {
+            dq.pop_back();
         }
+        dq.push_back(i);
+    }
+    for (int i = k + 1; i <= n; i++) {
+        while (!dq.empty() && get(i) <= get(dq.back())) {
+            dq.pop_back();
+        }
+        dq.push_back(i);
+        dp[i] = get(dq.front());
+        if (dq.front() == i - k) dq.pop_front();
     }
 
     cout << sum - dp[n] << endl;
