aa2ebbd0abee471c418ddf6593a77220ae14b874
Ashwani Yadav <ashyadavash@gmail.com>
2021-09-25 19:51:51 +0530

    Create Find majority element (> half freq) [Boyer–Moore Majority Vote Algorithm].cpp

---
+++ Arrays/00 Interview Standard/Find majority element (> half freq) [Boyer–Moore Majority Vote Algorithm].cpp
@@ -0,0 +1,68 @@
+// https://leetcode.com/problems/majority-element/
+// https://www.techiedelight.com/find-majority-element-in-an-array-boyer-moore-majority-vote-algorithm/
+// https://www.geeksforgeeks.org/majority-element/
+
+
+/*
+Given an integer array containing duplicates, return the majority element if present. 
+A majority element appears more than ⌊n / 2⌋ times (rounded down), where n is the array size.
+*/
+
+
+
+// Time = O(n), Space = O(n)
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int arr[] = {2, 2, 2, 2, 5, 5, 2, 3, 3};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    unordered_map<int, int> mp;
+    for (int i = 0; i < n; i++) {
+        mp[arr[i]]++;
+        if (mp[arr[i]] > n / 2) {
+            cout << "Majority found :- " << arr[i] << endl;
+            return 0;
+        }
+    }
+    cout << "No Majority element" << endl;
+
+    return 0;
+}
+
+
+
+
+
+// Assumption: Frequency of majority element > n / 2
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        return nums[n / 2];
+    }
+};
+
+
+
+
+// Boyer–Moore majority vote algorithm
+// Assumption: Frequency of majority element > n / 2
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int major = nums[0], count = 1;
+        for (int i = 1; i < nums.size(); i++) {
+            if (count == 0) {
+                major = nums[i];
+                count++;
+            } else {
+                if (major == nums[i]) count++;
+                else count--;
+            }
+        }
+        return major;
+    }
+};
