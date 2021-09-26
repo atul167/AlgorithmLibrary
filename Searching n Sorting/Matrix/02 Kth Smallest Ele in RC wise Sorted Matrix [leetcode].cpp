fa479bb0a3a529f087f654257fd76e69feeea4ed
Ashwani Yadav <ashyadavash@gmail.com>
2021-09-26 22:53:48 +0530

    Update 02 Kth Smallest Ele in RC wise Sorted Matrix [leetcode].cpp

--- Searching n Sorting/Matrix/02 Kth Smallest Ele in RC wise Sorted Matrix [leetcode].cpp
+++ Searching n Sorting/Matrix/02 Kth Smallest Ele in RC wise Sorted Matrix [leetcode].cpp
@@ -12,26 +12,26 @@ public:
     // count no of elements less than or equal to target in row-column wise sorted matrix
     int countNoOfEleUptoTarget(vector<vector<int>> &mat, int target) {
         int n = mat.size(), m = mat[0].size();
-        int i = n-1, j = 0, cnt = 0;
-        while(i >= 0 && j < m) {
-            if(mat[i][j] <= target) {
-                cnt += i + 1;
-                j++;
+        int i = 0, j = m - 1, cnt = 0;
+        while (i < n && j >= 0) {
+            if (mat[i][j] <= target) {
+                cnt += j + 1;
+                i++;
             } else {
-                i--;
+                j--;
             }
         }
         return cnt;
     }
-    
+
     int kthSmallest(vector<vector<int>>& mat, int k) {
         int n = mat.size();
-        int lo = mat[0][0], hi = mat[n-1][n-1];
-        
-        while(lo < hi) {
+        int lo = mat[0][0], hi = mat[n - 1][n - 1];
+
+        while (lo < hi) {
             int mid = lo + (hi - lo) / 2;
             int cnt = countNoOfEleUptoTarget(mat, mid);
-            if(cnt >= k)
+            if (cnt >= k)
                 hi = mid;
             else
                 lo = mid + 1;
