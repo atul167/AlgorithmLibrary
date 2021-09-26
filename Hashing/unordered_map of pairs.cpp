54c20fcf440a33b11b0630709c4d7df1c2eb26bf
Ashwani Yadav <ashyadavash@gmail.com>
2021-09-27 01:11:04 +0530

    Update unordered_map of pairs.cpp

--- Hashing/unordered_map of pairs.cpp
+++ Hashing/unordered_map of pairs.cpp
@@ -10,13 +10,21 @@ using namespace std;
 struct hash_pair {
     template <class T1, class T2>
     size_t operator()(const pair<T1, T2>& p) const {
-        auto hash1 = hash<T1>{}(p.first);
-        auto hash2 = hash<T2>{}(p.second);
+        auto hash1 = hash<T1> {}(p.first);
+        auto hash2 = hash<T2> {}(p.second);
         return hash1 ^ hash2;
     }
 };
 
-// struct hash_pair2 {
+// struct hash_pair {
+//     size_t operator()(const pair<int, int>& p) const {
+//         auto hash1 = hash<int>{}(p.first);
+//         auto hash2 = hash<int>{}(p.second);
+//         return hash1 ^ hash2;
+//     }
+// };
+
+// struct hash_pair {
 //     template <class T1, class T2>
 //     size_t operator()(const pair<T1, T2>& p) const {
 //         auto hash1 = (p.first);
@@ -33,13 +41,13 @@ int main() {
     pair<int, int> p1(10, 20);
     pair<int, int> p2(20, 30);
     pair<int, int> p3(25, 35);
-    
+
     // Inserting values in the unordered_map.
     um[p1] = 'x';
     um[p2] = 'a';
     um[p3] = 'x';
 
-    for (auto it: um) {
+    for (auto it : um) {
         cout << "[" << (it.first).first << ", " << (it.first).second << "] => " << it.second << "\n";
     }
 
