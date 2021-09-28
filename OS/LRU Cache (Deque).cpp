ec151924a706b2c089157df131cd244241d4a701
Ashwani Yadav <ashyadavash@gmail.com>
2021-09-29 01:44:32 +0530

    Update and rename LRU Cache (Deque) [Page Replacement Algorithm].cpp to LRU Cache (Deque).cpp

--- OS/LRU Cache (Deque).cpp
+++ OS/LRU Cache (Deque).cpp
@@ -1,6 +1,7 @@
 // https://leetcode.com/problems/lru-cache/
 // https://www.geeksforgeeks.org/lru-cache-implementation/
 // https://youtu.be/xDEuM5qa0zg -> Take U Forward
+// Page Replacement Algorithm
 
 /*
 Design a data structure that follows the constraints of a Least Recently Used (LRU) cache.
