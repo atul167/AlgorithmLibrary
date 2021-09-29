192adf4627a24b69795571ab23c205d29fc29955
Ashwani Yadav <ashyadavash@gmail.com>
2021-09-29 18:59:36 +0530

    Create 13 K-th Element of Two Sorted Arrays.cpp

---
+++ Arrays/13 K-th Element of Two Sorted Arrays.cpp
@@ -0,0 +1,98 @@
+// https://www.geeksforgeeks.org/k-th-element-two-sorted-arrays/
+// https://practice.geeksforgeeks.org/problems/k-th-element-of-two-sorted-array1317/1
+
+
+class Solution {
+public:
+    int kthElement(int arr1[], int arr2[], int n, int m, int k) {
+        int cnt = 0, i = 0, j = 0;
+        while (i < n && j < m) {
+            if (arr1[i] < arr2[j]) {
+                cnt++;
+                if (cnt == k) return arr1[i];
+                i++;
+            } else {
+                cnt++;
+                if (cnt == k)
+                    return arr2[j];
+                j++;
+            }
+        }
+        while (i < n) {
+            cnt++;
+            if (cnt == k) return arr1[i];
+            i++;
+        }
+        while (j < m) {
+            cnt++;
+            if (cnt == k) return arr2[j];
+            j++;
+        }
+        return -1;
+    }
+};
+
+
+
+
+
+
+class Solution {
+public:
+    int kthElement(int arr1[], int arr2[], int n, int m, int k) {
+        int lo = min(arr1[0], arr2[0]), hi = max(arr1[n - 1], arr2[m - 1]);
+
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+
+            int cnt = upper_bound(arr1, arr1 + n, mid) - arr1;
+            cnt += upper_bound(arr2, arr2 + m, mid) - arr2;
+
+            if (cnt >= k)
+                hi = mid;
+            else
+                lo = mid + 1;
+        }
+        return lo;
+    }
+};
+
+
+
+
+
+
+class Solution {
+public:
+    int kthElement(int arr1[], int arr2[], int n, int m, int k) {
+        priority_queue<array<int, 3>, vector<array<int, 3>>, greater<array<int, 3>> > pq;
+
+        pq.push({ arr1[0], 1, 0 });
+        pq.push({ arr2[0], 2, 0 });
+        int res;
+
+        while (!pq.empty() && k--) {
+            auto cur = pq.top();
+            pq.pop();
+
+            res = cur[0];
+            // i = arr1 or arr2
+            // j = idx in that arr
+            int i = cur[1];
+            int j = cur[2];
+
+            if (i == 1) {
+                if (j + 1 < n) {
+                    pq.push({ arr1[j + 1], i, j + 1 });
+                }
+            }
+            if (i == 2) {
+                if (j + 1 < m) {
+                    pq.push({ arr2[j + 1], i, j + 1 });
+                }
+            }
+        }
+
+        return res;
+    }
+};
