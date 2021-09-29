334981dbd4439b16aaae4f8fc55985fe5e02877f
Ashwani Yadav <ashyadavash@gmail.com>
2021-09-30 00:09:55 +0530

    Update 13 K-th largest ele of two sorted arrays.cpp

--- Arrays/13 K-th largest ele of two sorted arrays.cpp
+++ Arrays/13 K-th largest ele of two sorted arrays.cpp
@@ -1,5 +1,6 @@
 // https://www.geeksforgeeks.org/k-th-element-two-sorted-arrays/
 // https://practice.geeksforgeeks.org/problems/k-th-element-of-two-sorted-array1317/1
+// https://youtu.be/nv7F4PiLUzo
 // Google interview
 
 /*
@@ -21,6 +22,51 @@ You may assume k is always valid, 1 ≤ k ≤ m + n.
 Follow-up
 Can you do it in O(logk) time?
 */
+
+
+
+// Time = O(log(min(n, m)))
+
+class Solution{
+public:
+    int kthElement(int arr1[], int arr2[], int n, int m, int k) {
+        if(n > m) {
+            return kthElement(arr2, arr1, m, n, k); 
+        }
+        
+        // min no of elements that can be taken from arr1
+        int low = max(0, k - m);
+        // max no of elements that can be taken from arr1
+        int high = min(k, n);
+        
+        while(low <= high) {
+            // no of elements taken from arr1 from starting
+            int cut1 = (low + high) >> 1; 
+            // no of elements taken from arr2 from starting
+            int cut2 = k - cut1; 
+            int l1 = cut1 == 0 ? INT_MIN : arr1[cut1 - 1]; 
+            int l2 = cut2 == 0 ? INT_MIN : arr2[cut2 - 1];
+            int r1 = cut1 == n ? INT_MAX : arr1[cut1]; 
+            int r2 = cut2 == m ? INT_MAX : arr2[cut2]; 
+            
+            if(l1 <= r2 && l2 <= r1) {
+                return max(l1, l2);
+            }
+            
+            if (l1 > r2) {
+                high = cut1 - 1;
+            } else {
+                low = cut1 + 1; 
+            }
+        }
+        return -1;
+    }
+};
+
+
+
+
+
 
 
 class Solution {
