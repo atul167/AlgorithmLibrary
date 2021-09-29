6ecbc8d31e91fa43096335e6422f22adbc549d0e
Ashwani Yadav <ashyadavash@gmail.com>
2021-09-30 01:17:44 +0530

    Update and rename 13 K-th largest ele of two sorted arrays.cpp to 13 K-th element of two sorted arrays.cpp

--- Arrays/13 K-th element of two sorted arrays.cpp
+++ Arrays/13 K-th element of two sorted arrays.cpp
@@ -29,15 +29,15 @@ Can you do it in O(logk) time?
 
 class Solution{
 public:
-    int kthElement(int arr1[], int arr2[], int n, int m, int k) {
-        if(n > m) {
-            return kthElement(arr2, arr1, m, n, k); 
+    int kthElement(int arr1[], int arr2[], int n1, int n2, int k) {
+        if(n1 > n2) {
+            return kthElement(arr2, arr1, n2, n1, k); 
         }
         
         // min no of elements that can be taken from arr1
-        int low = max(0, k - m);
+        int low = max(0, k - n2);
         // max no of elements that can be taken from arr1
-        int high = min(k, n);
+        int high = min(k, n1);
         
         while(low <= high) {
             // no of elements taken from arr1 from starting
@@ -46,8 +46,8 @@ public:
             int cut2 = k - cut1; 
             int l1 = cut1 == 0 ? INT_MIN : arr1[cut1 - 1]; 
             int l2 = cut2 == 0 ? INT_MIN : arr2[cut2 - 1];
-            int r1 = cut1 == n ? INT_MAX : arr1[cut1]; 
-            int r2 = cut2 == m ? INT_MAX : arr2[cut2]; 
+            int r1 = cut1 == n1 ? INT_MAX : arr1[cut1]; 
+            int r2 = cut2 == n2 ? INT_MAX : arr2[cut2]; 
             
             if(l1 <= r2 && l2 <= r1) {
                 return max(l1, l2);
@@ -103,7 +103,7 @@ public:
 
 
 
-
+// kth element in row-wise sorted matrix concept
 class Solution {
 public:
     int kthElement(int arr1[], int arr2[], int n, int m, int k) {
