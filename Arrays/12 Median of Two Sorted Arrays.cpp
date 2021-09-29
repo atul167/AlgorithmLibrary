73504d0fa312768ff00a98c36dbe740607cf753b
Ashwani Yadav <ashyadavash@gmail.com>
2021-09-30 00:51:14 +0530

    Update 12 Median of Two Sorted Arrays.cpp

--- Arrays/12 Median of Two Sorted Arrays.cpp
+++ Arrays/12 Median of Two Sorted Arrays.cpp
@@ -26,13 +26,10 @@ public:
             // no of elements taken from nums2 from starting
             int cut2 = ((n1 + n2 + 1) / 2) - cut1;
 
-
             int left1 = cut1 == 0 ? INT_MIN : nums1[cut1 - 1];
             int left2 = cut2 == 0 ? INT_MIN : nums2[cut2 - 1];
-
             int right1 = cut1 == n1 ? INT_MAX : nums1[cut1];
             int right2 = cut2 == n2 ? INT_MAX : nums2[cut2];
-
 
             if (left1 <= right2 && left2 <= right1) {
                 if ((n1 + n2) % 2) return max(left1, left2);
@@ -80,13 +77,10 @@ public:
             // no of elements taken from nums2 from starting
             int cut2 = k - cut1;
 
-
             int left1 = cut1 == 0 ? INT_MIN : nums1[cut1 - 1];
             int left2 = cut2 == 0 ? INT_MIN : nums2[cut2 - 1];
-
             int right1 = cut1 == n1 ? INT_MAX : nums1[cut1];
             int right2 = cut2 == n2 ? INT_MAX : nums2[cut2];
-
 
             if (left1 <= right2 && left2 <= right1) {
                 if ((n1 + n2) % 2) return max(left1, left2);
