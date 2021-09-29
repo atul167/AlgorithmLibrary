ca48cdb342bafc0d45a4a17781e89981f039dc24
Ashwani Yadav <ashyadavash@gmail.com>
2021-09-30 00:49:40 +0530

    Update 12 Median of Two Sorted Arrays.cpp

--- Arrays/12 Median of Two Sorted Arrays.cpp
+++ Arrays/12 Median of Two Sorted Arrays.cpp
@@ -6,6 +6,121 @@ The overall run time complexity should be O(log(n + m)).
 */
 
 
+
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        if (nums1.size() > nums2.size()) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
+        int n1 = nums1.size(), n2 = nums2.size();
+
+        // min no of elements that can be taken from nums1
+        int low = 0;
+        // max no of elements that can be taken from nums1
+        int high = n1;
+
+        while (low <= high) {
+            // no of elements taken from nums1 from starting
+            int cut1 = (low + high) >> 1;
+            // no of elements taken from nums2 from starting
+            int cut2 = ((n1 + n2 + 1) / 2) - cut1;
+
+
+            int left1 = cut1 == 0 ? INT_MIN : nums1[cut1 - 1];
+            int left2 = cut2 == 0 ? INT_MIN : nums2[cut2 - 1];
+
+            int right1 = cut1 == n1 ? INT_MAX : nums1[cut1];
+            int right2 = cut2 == n2 ? INT_MAX : nums2[cut2];
+
+
+            if (left1 <= right2 && left2 <= right1) {
+                if ((n1 + n2) % 2) return max(left1, left2);
+                return (max(left1, left2) + min(right1, right2)) / 2.0;
+            }
+
+            if (left1 > right2) {
+                high = cut1 - 1;
+            } else {
+                low = cut1 + 1;
+            }
+        }
+        return 0.0;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        if (nums1.size() > nums2.size()) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
+        int n1 = nums1.size(), n2 = nums2.size();
+        
+        int k = (n1 + n2 + 1) / 2;
+
+        // min no of elements that can be taken from nums1
+        int low = max(0, k - n2);
+        // max no of elements that can be taken from nums1
+        int high = min(k, n1);
+
+        while (low <= high) {
+            // no of elements taken from nums1 from starting
+            int cut1 = (low + high) >> 1;
+            // no of elements taken from nums2 from starting
+            int cut2 = k - cut1;
+
+
+            int left1 = cut1 == 0 ? INT_MIN : nums1[cut1 - 1];
+            int left2 = cut2 == 0 ? INT_MIN : nums2[cut2 - 1];
+
+            int right1 = cut1 == n1 ? INT_MAX : nums1[cut1];
+            int right2 = cut2 == n2 ? INT_MAX : nums2[cut2];
+
+
+            if (left1 <= right2 && left2 <= right1) {
+                if ((n1 + n2) % 2) return max(left1, left2);
+                return (max(left1, left2) + min(right1, right2)) / 2.0;
+            }
+
+            if (left1 > right2) {
+                high = cut1 - 1;
+            } else {
+                low = cut1 + 1;
+            }
+        }
+        return 0.0;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*
 Using median of row-wise sorted matrix concept.
 */
