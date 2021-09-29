8bcd548b00e18726d062c78e21659c1f7d453a5a
Ashwani Yadav <ashyadavash@gmail.com>
2021-09-29 13:55:13 +0530

    Update LRU Cache (Deque).cpp

--- OS/LRU Cache (Deque).cpp
+++ OS/LRU Cache (Deque).cpp
@@ -16,6 +16,7 @@ The functions get and put must each run in O(1) average time complexity.
 */
 
 
+// Method 1: Using STL List or Deque
 class LRUCache {
 public:
     struct Node {
@@ -33,6 +34,164 @@ public:
 
     int get(int key) {
         if (!cache.count(key)) return -1;
+
+        int value = cache[key].val;
+        put(key, value);
+        return value;
+    }
+
+    void put(int key, int value) {
+        if (cache.count(key)) {
+            dq.erase(cache[key].itr);
+            cache.erase(key);
+        }
+        if (cache.size() == cap) {
+            cache.erase(dq.back());
+            dq.pop_back();
+        }
+
+        dq.push_front(key);
+        cache[key] = {value, dq.begin()};
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Method 2: Implementing our own deque list
+
+struct Listnode {
+    int data;
+    Listnode *prev, *next;
+    Listnode(int d) {
+        data = d;
+        prev = next = NULL;
+    }
+};
+
+class List {
+    Listnode *head, *tail;
+    int count;
+public:
+    List() {
+        head = new Listnode(-1);
+        tail = new Listnode(-1);
+        head->next = tail;
+        tail->prev = head;
+        count = 0;
+    }
+    int front() {
+        if (count == 0) return -1;
+        return head->next->data;
+    }
+    int back() {
+        if (count == 0) return -1;
+        return tail->prev->data;
+    }
+    Listnode* begin() {
+        if (count == 0) return NULL;
+        return head->next;
+    }
+    void push_front(int d) {
+        Listnode *node = new Listnode(d);
+        Listnode *temp = head->next;
+        head->next = node;
+        node->prev = head;
+        node->next = temp;
+        temp->prev = node;
+        count++;
+    }
+    void push_back(int d) {
+        Listnode *node = new Listnode(d);
+        Listnode *temp = tail->prev;
+        tail->prev = node;
+        node->next = tail;
+        node->prev = temp;
+        temp->next = node;
+        count++;
+    }
+    void pop_front() {
+        if (count == 0) return;
+        if (count == 1) {
+            Listnode *node = head->next;
+            head->next = tail;
+            tail->prev = head;
+            delete node;
+            count--;
+            return;
+        }
+
+        Listnode *node = head->next;
+        Listnode *temp = head->next->next;
+        head->next = temp;
+        temp->prev = head;
+        delete node;
+        count--;
+    }
+    void pop_back() {
+        if (count == 0) return;
+        if (count == 1) {
+            Listnode *node = head->next;
+            head->next = tail;
+            tail->prev = head;
+            delete node;
+            count--;
+            return;
+        }
+
+        Listnode *node = tail->prev;
+        Listnode *temp = tail->prev->prev;
+        tail->prev = temp;
+        temp->next = tail;
+        delete node;
+        count--;
+    }
+    void erase(Listnode *node) {
+        if (!node) return;
+        Listnode *prev = node->prev, *next = node->next;
+        prev->next = next;
+        next->prev = prev;
+        delete node;
+        count--;
+    }
+    void print() {
+        Listnode *temp = head->next;
+        while (temp != tail) {
+            cout << temp->data << " ";
+            temp = temp->next;
+        }
+        cout << endl;
+    }
+};
+
+class LRUCache {
+public:
+    struct Node {
+        int val;
+        Listnode *itr;
+    };
+    unordered_map<int, Node> cache;
+    // old values move to back, new ones enter at front
+    List dq;
+    int cap;
+
+    LRUCache(int capacity) {
+        cap = capacity;
+    }
+
+    int get(int key) {
+        if (!cache.count(key)) return -1;
+
 
         int value = cache[key].val;
         put(key, value);
