064e8937ca9c3804e95c497fdd29620093e2958c
Ashwani Yadav <ashyadavash@gmail.com>
2021-09-30 16:25:52 +0530

    Update 00 Kadanes Algorithm.cpp

--- DP/Kadane’s Algorithm/00 Kadanes Algorithm.cpp
+++ DP/Kadane’s Algorithm/00 Kadanes Algorithm.cpp
@@ -58,10 +58,9 @@ curr_max = max contiguous subarray sum ending at index i (including a[i])
 class Solution {
 public:
     long long maxSubarraySum(int a[], int n) {
-        long long max_so_far = a[0];
-        long long curr_max = a[0];
+        long long max_so_far = INT_MIN, curr_max = 0;
 
-        for (int i = 1; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             curr_max = max<long long>(a[i], curr_max + a[i]);
             max_so_far = max<long long>(max_so_far, curr_max);
         }
