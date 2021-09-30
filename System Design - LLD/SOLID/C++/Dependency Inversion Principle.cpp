b04b5de92ca0c8c81d09b80f3a9f56efb5f55b7b
Ashwani Yadav <ashyadavash@gmail.com>
2021-09-30 23:50:04 +0530

    Create Dependency Inversion Principle.cpp

---
+++ System Design - LLD/SOLID/C++/Dependency Inversion Principle.cpp
@@ -0,0 +1,121 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+enum Relationship { parent, child, sibling };
+
+class Person {
+public:
+    string name;
+};
+
+// Low-level <<<<<<<<<<<<-------------------------
+class Relationships {
+public:
+    vector<tuple<Person, Relationship, Person>> m_relations;
+
+    void add_parent_and_child(const Person &parent, const Person &child) {
+        m_relations.push_back({parent, Relationship::parent, child});
+        m_relations.push_back({child, Relationship::child, parent});
+    }
+};
+
+// High-level  <<<<<<<<<<<<------------------------
+class Research {
+public:
+    Research(const Relationships &relationships) {
+        for (auto && [first, rel, second] : relationships.m_relations) {
+            if (first.name == "John" && rel == Relationship::parent)
+                cout << "John has a child called " << second.name << endl;
+        }
+    }
+};
+
+int main() {
+    Person parent{"John"};
+    Person child1{"Chris"};
+    Person child2{"Matt"};
+
+    Relationships relationships;
+    relationships.add_parent_and_child(parent, child1);
+    relationships.add_parent_and_child(parent, child2);
+
+    Research _(relationships);
+
+    return EXIT_SUCCESS;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+enum Relationship { parent, child, sibling };
+
+class Person {
+public:
+    string name;
+};
+
+class RelationshipBrowser {
+public:
+    virtual vector<Person> find_all_children_of(const string &name) = 0;
+};
+
+// Low-level <<<<<<<<<<<<<<<------------------------
+class Relationships : public RelationshipBrowser {
+public:
+    vector<tuple<Person, Relationship, Person>> m_relations;
+
+    void add_parent_and_child(const Person &parent, const Person &child) {
+        m_relations.push_back({parent, Relationship::parent, child});
+        m_relations.push_back({child, Relationship::child, parent});
+    }
+
+    vector<Person> find_all_children_of(const string &name) {
+        vector<Person> result;
+        for (auto && [first, rel, second] : m_relations) {
+            if (first.name == name && rel == Relationship::parent) {
+                result.push_back(second);
+            }
+        }
+        return result;
+    }
+};
+
+// High-level <<<<<<<<<<<<<<<----------------------
+class Research {
+public:
+    Research(RelationshipBrowser &browser) {
+        for (auto &child : browser.find_all_children_of("John")) {
+            cout << "John has a child called " << child.name << endl;
+        }
+    }
+};
+
+int main() {
+    Person parent{"John"};
+    Person child1{"Chris"};
+    Person child2{"Matt"};
+
+    Relationships relationships;
+    relationships.add_parent_and_child(parent, child1);
+    relationships.add_parent_and_child(parent, child2);
+
+    Research _(relationships);
+
+    return EXIT_SUCCESS;
+}
