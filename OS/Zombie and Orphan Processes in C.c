0e946db4b47ec213d76ac159fc56e1da5b865e78
Ashwani Yadav <ashyadavash@gmail.com>
2021-10-02 00:02:54 +0530

    Update Zombie and Orphan Processes in C.c

--- OS/Zombie and Orphan Processes in C.c
+++ OS/Zombie and Orphan Processes in C.c
@@ -1,5 +1,19 @@
 // https://www.geeksforgeeks.org/zombie-and-orphan-processes-in-c/
 
+/*
+What is the zombie process?
+----------------------------
+A process that has finished the execution but still has an entry in the process table to report to its parent process is known as a zombie process. 
+A child process always first becomes a zombie before being removed from the process table. 
+The parent process reads the exit status of the child process which reaps off the child process entry from the process table.
+
+What are orphan processes?
+----------------------------
+A process whose parent process no more exists i.e. either finished or terminated without waiting for its child process to terminate is called an orphan process.
+*/
+
+
+
 // A C program to demonstrate Zombie Process.
 // Child becomes Zombie as parent is sleeping when child process exits.
 
