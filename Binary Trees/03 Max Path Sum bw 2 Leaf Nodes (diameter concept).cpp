c1e77ed62e3000d05662a5e4fbd5fbd9a1f24594
Ashwani Yadav <ashyadavash@gmail.com>
2021-10-08 15:34:47 +0530

    Update 03 Max Path Sum bw 2 Leaf Nodes (diameter concept).cpp

--- Binary Trees/03 Max Path Sum bw 2 Leaf Nodes (diameter concept).cpp
+++ Binary Trees/03 Max Path Sum bw 2 Leaf Nodes (diameter concept).cpp
@@ -12,43 +12,42 @@ public:
     int maxPathSumUtil(struct Node *root, int &res) {
         // Base cases
         if (!root) return 0;
-        if (!root->left && !root->right) return root->data;
-     
-        // Find maximum sum in left and right subtree. 
+
+        // Find maximum sum in left and right subtree.
         // i.e. maximum root to leaf sums in left and right subtrees
         int ls = maxPathSumUtil(root->left, res);
         int rs = maxPathSumUtil(root->right, res);
-     
-     
+
+
         // If both left and right children exist
         if (root->left && root->right) {
             // Update result if needed
             res = max(res, ls + rs + root->data);
-     
+
             // Return maximum possible value for root being on one side
             return max(ls, rs) + root->data;
         }
-     
+
         // If any of the two children is empty, return root sum for root being on one side
-        if(!root->left) return rs + root->data;
-        if(!root->right) return ls + root->data;
+        if (!root->left) return rs + root->data;
+        if (!root->right) return ls + root->data;
     }
-    
+
     int maxPathSum(Node *root) {
         int res = INT_MIN;
-       
+
         int val = maxPathSumUtil(root, res);
-               
+
         //--- for test case ---
-        //          7                
+        //          7
         //         /  \              
-        //      Null  -3 
+        //      Null  -3
         // value of res will be INT_MIN but the answer is 4 , which is returned by the function maxPathSumUtil().
-        
-        if(res == INT_MIN) {
+
+        if (res == INT_MIN) {
             return val;
         }
-       
+
         return res;
     }
 };
