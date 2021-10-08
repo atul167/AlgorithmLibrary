241944b02515d02ef96d8689c506ecfc3b9c56d8
Ashwani Yadav <ashyadavash@gmail.com>
2021-10-09 01:45:32 +0530

    Update 10 Airport Connections [Algoexpert].cpp

--- Graph/SSC and Bridges and Articulation Points/10 Airport Connections [Algoexpert].cpp
+++ Graph/SSC and Bridges and Articulation Points/10 Airport Connections [Algoexpert].cpp
@@ -10,9 +10,6 @@ You are given a list of airports (three-letter codes like 'JFK'), a list of rout
 Write a function that returns the minimum number of airport connections (one-way flights) that need to be added 
 in order for someone to be able to reach any airport in the list, starting at the starting airport.
 
-Note that the connections don't have to be direct; it's okay if an airport can only be reached from the starting airport by stopping at other airports first.
-
-
 
 Sample Input:
 
@@ -60,7 +57,6 @@ routes = [
 ]
 
 startingAirport = "LGA"
-
 
 
 Sample output:
