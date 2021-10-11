207e8be54fcd7a508d3cd9da9f2f2e9ee3af91ad
Ashwani Yadav <ashyadavash@gmail.com>
2021-10-11 20:19:57 +0530

    Update 12 Minimum Remove to Make Valid Parentheses.cpp

--- Stack/12 Minimum Remove to Make Valid Parentheses.cpp
+++ Stack/12 Minimum Remove to Make Valid Parentheses.cpp
@@ -41,100 +41,35 @@ public:
     string minRemoveToMakeValid(string s) {
         stack<char> st;
         int cnt = 0;
-        
-        for(char x: s) {
-            if(x != '(' and x != ')') {
+
+        for (char x : s) {
+            if (x != '(' and x != ')') {
                 st.push(x);
             }
-            else if(x == '(') {
+            else if (x == '(') {
                 cnt++;
                 st.push(x);
             }
-            else if(x == ')' && cnt > 0) {
+            else if (x == ')' && cnt > 0) {
                 cnt--;
                 st.push(x);
             }
         }
-        
+
         int n = st.size() - cnt;
         string res(n, ' ');
         int idx = n - 1;
-        while(!st.empty()) {
+        while (!st.empty()) {
             char x = st.top();
             st.pop();
-            if(cnt > 0 && x == '(') {
+            if (cnt > 0 && x == '(') {
                 cnt--;
             } else {
                 res[idx] = x;
                 idx--;
             }
         }
-        
+
         return res;
-    }
-};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// TLE solution
-class Solution {
-public:
-    bool isBalanced(string s) {
-        int cnt = 0;
-        for(char x: s) {
-            if(x == '(') cnt++;
-            if(x == ')') cnt--;
-            if(cnt < 0) return false;
-        }
-        return (cnt == 0);
-    }
-    
-    string minRemoveToMakeValid(string s) {
-        queue<string> q;
-        map<string, bool> vis;
-        q.push(s);
-        vis[s] = 1;
-        
-        while(!q.empty()) {
-            int n = q.size();
-            for(int i = 0; i < n; i++) {
-                string u = q.front();
-                q.pop();
-
-                if(isBalanced(u)) {
-                    return u;
-                }
-
-                for(int i = 0; i < u.size(); i++) {
-                    if(s[i] == '(' or s[i] == ')') {
-                        string v = u.substr(0, i) + u.substr(i+1);
-                        if(!vis[v]) {
-                            q.push(v);
-                            vis[v] = 1;
-                        }
-                    }
-                }
-            }
-        }
-        return "";
     }
 };
