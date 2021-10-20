5b6e3c11f57512cc7fa0b8c9f58c845a38e9f79e
Ashwani Yadav <ashyadavash@gmail.com>
2021-10-20 13:59:25 +0530

    Update 03 Delete a node by key.cpp

--- Binary Trees/BST/03 Delete a node by key.cpp
+++ Binary Trees/BST/03 Delete a node by key.cpp
@@ -307,27 +307,27 @@ so that we can simply remove the successor by making the child of a parent NULL.
 using namespace std;
 
 struct TreeNode {
-    int data;
-    TreeNode *left, *right;
-    TreeNode(int k) {
-        data = k;
-        left = right = NULL;
-    }
+	int data;
+	TreeNode *left, *right;
+	TreeNode(int k) {
+		data = k;
+		left = right = NULL;
+	}
 };
 
 void inorder(TreeNode* root) {
-    if (!root) return;
-    inorder(root->left);
-    cout << root->data << " ";
-    inorder(root->right);
+	if (!root) return;
+	inorder(root->left);
+	cout << root->data << " ";
+	inorder(root->right);
 }
 
-TreeNode* insert(TreeNode *root, int data) {
-    if (!root) {
+TreeNode* insert(TreeNode *root, int data){
+    if(!root) {
         root = new TreeNode(data);
         return root;
     }
-    if (data <= root->data) {
+    if(data <= root->data) {
         root->left = insert(root->left, data);
     } else {
         root->right = insert(root->right, data);
@@ -337,55 +337,55 @@ TreeNode* insert(TreeNode *root, int data) {
 
 
 TreeNode* minValueNode(TreeNode *root) {
-    TreeNode* temp = root;
+	TreeNode* temp = root;
 
-    while (temp && temp->left) {
-        temp = temp->left;
-    }
+	while (temp && temp->left) {
+		temp = temp->left;
+	}
 
-    return temp;
+	return temp;
 }
 
-/* Given a binary search tree root and a data,
+/* Given a binary search tree root and a data, 
 this function deletes the data and returns the new root */
 TreeNode* deleteNode(TreeNode *root, int data) {
-    // base case
-    if (!root) return root;
+	// base case
+	if (!root) return root;
 
-    // If the data to be deleted is smaller than the root's data
-    if (data < root->data) {
-        root->left = deleteNode(root->left, data);
-        return root;
-    }
+	// If the data to be deleted is smaller than the root's data
+	if (data < root->data) {
+		root->left = deleteNode(root->left, data);
+		return root;
+	}
 
-    // If the data to be deleted is greater than the root's data
-    if (data > root->data) {
-        root->right = deleteNode(root->right, data);
-        return root;
-    }
+	// If the data to be deleted is greater than the root's data
+	if (data > root->data) {
+		root->right = deleteNode(root->right, data);
+		return root;
+	}
 
-    // If data is same as root's data, then this is the node to be deleted
-    // Case 1: node has no child
-    if (!root->left and !root->right) {
-        delete root;
-        return NULL;
-    }
+	// If data is same as root's data, then this is the node to be deleted
+	// Case 1: node has no child
+	if (!root->left and !root->right) {
+		delete root;
+		return NULL;
+	}
 
-    // Case 2: node with only one child
-    if (!root->left) {
-        TreeNode* temp = root->right;
-        delete root;
-        return temp;
-    }
-    if (!root->right) {
-        TreeNode* temp = root->left;
-        delete root;
-        return temp;
-    }
+	// Case 2: node with only one child
+	if (!root->left) {
+		TreeNode* temp = root->right;
+		delete root;
+		return temp;
+	} 
+	if (!root->right) {
+		TreeNode* temp = root->left;
+		delete root;
+		return temp;
+	}
 
-    // Case 3: node with two children:
-    // Get the inorder successor (smallest in the right subtree)
-    TreeNode* succParent = root;
+	// Case 3: node with two children: 
+	// Get the inorder successor (smallest in the right subtree)
+	TreeNode* succParent = root;
 
     // Find successor
     TreeNode* succ = root->right;
@@ -394,7 +394,7 @@ TreeNode* deleteNode(TreeNode *root, int data) {
         succ = succ->left;
     }
 
-    // Delete successor.
+    // Delete successor.  
     /*
     Since successor is always left child of its parent
     we can safely make successor's right
@@ -411,49 +411,49 @@ TreeNode* deleteNode(TreeNode *root, int data) {
 
     // Delete Successor and return root
     delete succ;
-    return root;
+	return root;
 }
 
 // Driver Code
 int main() {
-    /*
-    Let us create following BST
-          50
-         /  \
-        30   70
-       / \   / \
-      20 40 60  80
-    */
-    TreeNode* root = NULL;
-    root = insert(root, 50);
-    root = insert(root, 30);
-    root = insert(root, 20);
-    root = insert(root, 40);
-    root = insert(root, 70);
-    root = insert(root, 60);
-    root = insert(root, 80);
+	/* 
+	Let us create following BST
+		  50
+		 /	\
+		30	 70
+	   / \   / \
+	  20 40 60  80 
+	*/
+	TreeNode* root = NULL;
+	root = insert(root, 50);
+	root = insert(root, 30);
+	root = insert(root, 20);
+	root = insert(root, 40);
+	root = insert(root, 70);
+	root = insert(root, 60);
+	root = insert(root, 80);
 
-    cout << "Inorder traversal:\n";
-    inorder(root);
-    cout << endl;
+	cout << "Inorder traversal:\n";
+	inorder(root);
+	cout << endl;
 
-    cout << "\nDelete 20\n";
-    root = deleteNode(root, 20);
+	cout << "\nDelete 20\n";
+	root = deleteNode(root, 20);
+	
+	cout << "Inorder traversal:\n";
+	inorder(root);
+	cout << endl;
 
-    cout << "Inorder traversal:\n";
-    inorder(root);
-    cout << endl;
+	cout << "\nDelete 30\n";
+	root = deleteNode(root, 30);
+	
+	cout << "Inorder traversal:\n";
+	inorder(root);
+	cout << endl;
 
-    cout << "\nDelete 30\n";
-    root = deleteNode(root, 30);
+	cout << "\nDelete 50\n";
+	root = deleteNode(root, 50);
 
-    cout << "Inorder traversal:\n";
-    inorder(root);
-    cout << endl;
-
-    cout << "\nDelete 50\n";
-    root = deleteNode(root, 50);
-
-    cout << "Inorder traversal:\n";
-    inorder(root);
+	cout << "Inorder traversal:\n";
+	inorder(root);
 }
