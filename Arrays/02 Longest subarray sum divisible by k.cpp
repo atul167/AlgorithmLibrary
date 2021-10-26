6791fc45935d8c6489f29752fc7e470c847a6663
Ashwani Yadav <ashyadavash@gmail.com>
2021-10-26 21:29:30 +0530

    Update 02 Longest subarray sum divisible by k.cpp

--- Arrays/02 Longest subarray sum divisible by k.cpp
+++ Arrays/02 Longest subarray sum divisible by k.cpp
@@ -4,8 +4,16 @@
 Given an arr[] containing n integers and a positive integer k. 
 Find the length of the longest subarray with sum of the elements divisible by the given value k.
 
+Input:
+A[] = {-2, 2, -5, 12, -11, -1, 7}
+K = 3
+Output: 5
+Explanation:
+The subarray is {2,-5,12,-11,-1} with sum -3, which is divisible by 3.
+
 Input : arr[] = {2, 7, 6, 1, 4, 5}, k = 3
 Output : 4
+Explanation:
 The subarray is {7, 6, 1, 4} with sum 18, which is divisible by 3.
 */
 
