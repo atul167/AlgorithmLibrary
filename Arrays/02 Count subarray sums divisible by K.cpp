efba218eb5c6fa54c769de6d8c537dab74489065
Ashwani Yadav <ashyadavash@gmail.com>
2021-10-26 22:00:44 +0530

    Update 02 Count subarray sums divisible by K.cpp

--- Arrays/02 Count subarray sums divisible by K.cpp
+++ Arrays/02 Count subarray sums divisible by K.cpp
@@ -10,18 +10,19 @@ Explanation: There are 7 subarrays with a sum divisible by k = 5:
 [4, 5, 0, -2, -3, 1], [5], [5, 0], [5, 0, -2, -3], [0], [0, -2, -3], [-2, -3]
 */
 
+
 class Solution {
 public:
     int subarraysDivByK(vector<int>& nums, int k) {
-        map<int, int> mp;
+        vector<int> mp(k + 1, 0);
         mp[0] = 1;
-        
+
         int prefSumMod = 0, res = 0;
-        
-        for (int x: nums) {
+
+        for (int x : nums) {
             prefSumMod = (prefSumMod + x + k) % k;
-            if(prefSumMod < 0) prefSumMod += k;
-            
+            if (prefSumMod < 0) prefSumMod += k;
+
             res += mp[prefSumMod];
             mp[prefSumMod]++;
         }
@@ -37,15 +38,35 @@ class Solution {
 public:
     int subarraysDivByK(vector<int>& nums, int k) {
         map<int, int> mp;
-        
+        mp[0] = 1;
+
         int prefSumMod = 0, res = 0;
-        
-        for (int x: nums) {
+
+        for (int x : nums) {
             prefSumMod = (prefSumMod + x + k) % k;
-            if(prefSumMod < 0) prefSumMod += k;
-            
-            if(prefSumMod == 0) res++;
-            
+            if (prefSumMod < 0) prefSumMod += k;
+
+            res += mp[prefSumMod];
+            mp[prefSumMod]++;
+        }
+        return res;
+    }
+};
+
+
+class Solution {
+public:
+    int subarraysDivByK(vector<int>& nums, int k) {
+        map<int, int> mp;
+
+        int prefSumMod = 0, res = 0;
+
+        for (int x : nums) {
+            prefSumMod = (prefSumMod + x + k) % k;
+            if (prefSumMod < 0) prefSumMod += k;
+
+            if (prefSumMod == 0) res++;
+
             res += mp[prefSumMod];
             mp[prefSumMod]++;
         }
