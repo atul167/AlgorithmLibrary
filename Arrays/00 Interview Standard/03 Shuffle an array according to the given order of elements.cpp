5fe6218e65852c01ceb392dbfeb7327700e2c6a1
Ashwani Yadav <ashyadavash@gmail.com>
2021-10-27 01:18:37 +0530

    Update 03 Shuffle an array according to the given order of elements.cpp

--- Arrays/00 Interview Standard/03 Shuffle an array according to the given order of elements.cpp
+++ Arrays/00 Interview Standard/03 Shuffle an array according to the given order of elements.cpp
@@ -10,6 +10,9 @@ pos[] = { 3, 2, 4, 1, 0 }
  
 Output:
 arr[] = { 5, 4, 2, 1, 3 }
+
+i.e., if pos[i] = j, then update nums[j] = nums[i] for every index i.
+In other words, update nums[pos[i]] = nums[i] for every index i.
 */
 
 
