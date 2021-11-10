f19ff4869a998fac482596129251965171346d21
Ashwani Yadav <ashyadavash@gmail.com>
2021-11-10 23:47:42 +0530

    Update 15 Binary Tree Cameras [leetcode].cpp

--- Binary Trees/15 Binary Tree Cameras [leetcode].cpp
+++ Binary Trees/15 Binary Tree Cameras [leetcode].cpp
@@ -11,23 +11,23 @@ Return the minimum number of cameras needed to monitor all nodes of the tree.
 
 class Solution {
 public:
-    #define inf 1e9
-    map<pair<TreeNode*, pair<bool, bool>>, int> dp;
-    
+#define inf 1e9
+    map<tuple<TreeNode*, bool, bool>, int> dp;
+
     int fun(TreeNode* root, bool isCam, bool isParCam) {
-        if(!root) {
-            if(isCam) return inf;
+        if (!root) {
+            if (isCam) return inf;
             return 0;
         }
- 
-        if(dp.count({root, {isCam, isParCam}})) return dp[{root, {isCam, isParCam}}];
-        
+
+        if (dp.count({root, isCam, isParCam})) return dp[ {root, isCam, isParCam}];
+
         int x = inf, y = inf, z = inf;
-        if(isCam) {
+        if (isCam) {
             // child may or may not have camera
             x = 1 + min(fun(root->left, 0, 1), fun(root->left, 1, 1)) + min(fun(root->right, 0, 1), fun(root->right, 1, 1));
         } else {
-            if(isParCam) {
+            if (isParCam) {
                 // child may or may not have camera
                 y = min(fun(root->left, 0, 0), fun(root->left, 1, 0)) + min(fun(root->right, 0, 0), fun(root->right, 1, 0));
             } else {
@@ -37,10 +37,10 @@ public:
                 z = min(op1, op2);
             }
         }
-        
-        return dp[{root, {isCam, isParCam}}] = min({x, y, z});
+
+        return dp[ {root, isCam, isParCam}] = min({x, y, z});
     }
-    
+
     int minCameraCover(TreeNode* root) {
         dp.clear();
         return min(fun(root, 0, 0), fun(root, 1, 0));
