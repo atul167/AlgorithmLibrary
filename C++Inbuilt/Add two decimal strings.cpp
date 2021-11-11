ac1ec6767d8885b2dbba787de99957e36b9e254f
Ashwani Yadav <ashyadavash@gmail.com>
2021-11-11 11:34:50 +0530

    Update Add two decimal strings.cpp

--- C++Inbuilt/Add two decimal strings.cpp
+++ C++Inbuilt/Add two decimal strings.cpp
@@ -1,29 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+
 
 string addDecimalStrings(string s1, string s2) {
     string res = "";
 
     int i = s1.size() - 1, j = s2.size() - 1;
     int carry = 0;
-    while(i >= 0 || j >= 0 || carry > 0) {
-        int digit1 = 0, digit2 = 0;
-        if(i >= 0) {
-            digit1 = s1[i] - '0';
+    while (i >= 0 || j >= 0 || carry > 0) {
+        int total = 0;
+        if (i >= 0) {
+            total += s1[i] - '0';
+            i--;
         }
-        if(j >= 0) {
-            digit2 = s2[j] - '0';
+        if (j >= 0) {
+            total += s2[j] - '0';
+            j--;
         }
 
-        int total = digit1 + digit2 + carry;
-        res = char('0' + total % 10) + res;
+        total += carry;
+        res += char(total % 10 + '0');
 
         carry = total / 10;
-        i--, j--;
     }
 
+    reverse(res.begin(), res.end());
     return res;
 }
 
 void solve() {
     string a = "5346365464", b = "54645645";
     cout << addDecimalStrings(a, b);
+}
+
+
+int main() {
+    solve();
+    return 0;
 }
