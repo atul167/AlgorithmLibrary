6d4bb6b8f5ec1f3fb336d5b59f35083908fba35d
Ashwani Yadav <ashyadavash@gmail.com>
2021-11-11 11:32:13 +0530

    Update Add two binary strings.cpp

--- C++Inbuilt/Add two binary strings.cpp
+++ C++Inbuilt/Add two binary strings.cpp
@@ -1,39 +1,40 @@
-// C++ program to add two binary strings
 #include<bits/stdc++.h>
 using namespace std;
 
-// This function adds two binary strings and return
-// result as a third string
-string addBinary(string a, string b)
-{
-	string result = ""; // Initialize result
-	int s = 0;		 // Initialize digit sum
+string addBinary(string a, string b) {
+    string result = "";
+    // Initialize digit sum
+    int total = 0, carry = 0;
 
-	// Traverse both strings starting from last
-	// characters
-	int i = a.size() - 1, j = b.size() - 1;
-	while (i >= 0 || j >= 0 || s == 1)
-	{
-		// Comput sum of last digits and carry
-		s += ((i >= 0)? a[i] - '0': 0);
-		s += ((j >= 0)? b[j] - '0': 0);
+    // Traverse both strings starting from last characters
+    int i = a.size() - 1, j = b.size() - 1;
+    while (i >= 0 || j >= 0 || carry > 0) {
+        total = 0;
+        // Comput sum of last digits and carry
+        if (i >= 0) {
+            total += a[i] - '0';
+            i--;
+        }
+        if (j >= 0) {
+            total += b[j] - '0';
+            j--;
+        }
 
-		// If current digit sum is 1 or 3, add 1 to result
-		result = char(s % 2 + '0') + result;
+        total += carry;
 
-		// Compute carry
-		s /= 2;
+        // If current digit sum is 1 or 3, add 1 to result
+        result += char(total % 2 + '0');
 
-		// Move to next digits
-		i--; j--;
-	}
-	return result;
+        // Compute carry
+        carry = total / 2;
+
+    }
+    reverse(result.begin(), result.end());
+    return result;
 }
 
-// Driver program
-int main()
-{
-	string a = "1101", b="100";
-	cout << addBinary(a, b) << endl;
-	return 0;
+int main() {
+    string a = "1101", b = "100";
+    cout << addBinary(a, b) << endl;
+    return 0;
 }
