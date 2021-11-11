db3680fdc410247d33cec4e3ad30dceae1548e87
Ashwani Yadav <ashyadavash@gmail.com>
2021-11-11 12:53:01 +0530

    Create 08 Remove Cycle in LL.cpp

---
+++ LinkedList/08 Remove Cycle in LL.cpp
@@ -0,0 +1,37 @@
+// https://practice.geeksforgeeks.org/problems/remove-loop-in-linked-list/1
+// https://www.youtube.com/watch?v=aIR0s1tY2Vk
+
+
+class Solution {
+public:
+    //Function to remove a loop in the linked list.
+    void removeLoop(Node* head) {
+        if (!head) return;
+
+        Node* slow = head, *fast = head;
+
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if (slow == fast) {
+                if (slow == head) {
+                    while (fast->next != slow) {
+                        fast = fast->next;
+                    }
+                    fast->next = NULL;
+                    return;
+                }
+
+                slow = head;
+                while (slow->next != fast->next) {
+                    slow = slow->next;
+                    fast = fast->next;
+                }
+                fast->next = NULL;
+            };
+        }
+
+
+    }
+};
