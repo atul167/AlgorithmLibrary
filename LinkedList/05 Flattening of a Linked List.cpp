6ec42847d7056e70f5acc6215fd3f41fcabca22e
Ashwani Yadav <ashyadavash@gmail.com>
2021-11-11 11:01:56 +0530

    Update 05 Flattening of a Linked List.cpp

--- LinkedList/05 Flattening of a Linked List.cpp
+++ LinkedList/05 Flattening of a Linked List.cpp
@@ -46,7 +46,6 @@ struct Node{
 Node* mergeTwoLists(Node* l1, Node* l2) {
     if (!l1) return l2;
     if (!l2) return l1;
-    if (!l1 && !l2) return NULL;
 
     if (l1->data > l2->data) std::swap(l1, l2);
 
