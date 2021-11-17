ca6745bf40aaee761a720204ba320a60f3d470b8
Ashwani Yadav <ashyadavash@gmail.com>
2021-11-17 17:18:08 +0530

    Create Max no of songs u can hear in journey.cpp

---
+++ DP/Knapsack/Max no of songs u can hear in journey.cpp
@@ -0,0 +1,9 @@
+/*
+You are going on a journey. The journey is of eactly T no of minutes.
+You have a playliast of songs with time givn in minutes.
+Return the max number of songs you can hear in exactly T minutes. The songs should be fully heard and the time should be fully utilized.
+
+Example:
+T = 5
+playlist = [2, 3, 5, 4, 1]
+*/
