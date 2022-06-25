7ac8c9ae847d0818f8e763f82cd8da5b52706ba0
Ashwani Yadav <ashyadavash@gmail.com>
2022-06-26 01:53:13 +0530

    Update Floyd cycle detection (Find the Duplicate Number).cpp

--- Arrays/Floyd cycle detection (Find the Duplicate Number).cpp
+++ Arrays/Floyd cycle detection (Find the Duplicate Number).cpp
@@ -31,3 +31,28 @@ public:
         return slow; // fast
     }
 };
+
+
+
+
+
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        if(nums.size() == 0) return -1;
+        
+        int slow = nums[nums[0]];
+        int fast = nums[nums[nums[0]]];
+        while(slow != fast){
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }
+        slow = nums[0];
+        while(slow!=fast){
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow; // fast
+    }
+};
