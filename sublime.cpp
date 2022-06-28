6311422c1f01e284e4a22a436cb3f3ba3923e5ef
Ashwani Yadav <ashyadavash@gmail.com>
2022-06-29 02:05:34 +0530

    Create sublime.cpp

---
+++ sublime.cpp
@@ -0,0 +1,34 @@
+/*
+Create 3 files:
+===============
+a.cpp
+input.txt
+output.txt
+
+
+Run using 
+=========
+Cmd+B
+*/
+
+
+#include<iostream>
+using namespace std;
+
+int main(){
+  #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+  #endif
+
+  int var;
+  cin >> var;
+  for(int i=1; i<=10; i++) {
+      cout<<var * i+1<<endl;
+  }
+  return 0;
+/*
+Run using Cmd+B
+*/
+
+}
