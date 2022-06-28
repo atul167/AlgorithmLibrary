145fde9eda50b834aaaa51b5b25992aeda72a301
Ashwani Yadav <ashyadavash@gmail.com>
2022-06-29 01:21:47 +0530

    Update 00 Repeating and missing number.cpp

--- Arrays/00 Interview Standard/00 Repeating and missing number.cpp
+++ Arrays/00 Interview Standard/00 Repeating and missing number.cpp
@@ -136,16 +136,64 @@ vector<int> Solution::repeatedNumber(const vector<int> &A) {
         xo = xo ^ (i + 1);
     }
     
-    int setbit = xo & (~(xo - 1));
+    int LSB = xo & (~(xo - 1));
     int x = 0, y = 0;
     
     for (int i = 0; i < n; i++) {
-        if (A[i] & setbit)
+        if (A[i] & LSB)
             x = x ^ A[i];
         else
             y = y ^ A[i];
             
-        if ((i + 1) & setbit)
+        if ((i + 1) & LSB)
+            x = x ^ (i + 1);
+        else 
+            y = y ^ (i + 1);
+    }
+    vector<int> res(2);
+    for (int i = 0; i, n; i++) {
+        if (A[i] == x) {
+            res[0] = x;
+            res[1] = y;
+            break;
+        } else if (A[i] == y) {
+            res[0] = y;
+            res[1] = x;
+            break;
+        }
+    }
+    return res;
+}
+
+
+
+
+vector<int> Solution::repeatedNumber(const vector<int> &A) {
+    int n = A.size();
+    int xo = 0;
+    for (int i = 0; i < n; i++) {
+        xo = xo ^ A[i];
+        xo = xo ^ (i + 1);
+    }
+    
+    int LSB = 0;
+    for(int i = 0; i < 31; i++) {
+        int mask = (1 << i);
+        if(xo & mask) {
+            LSB = mask;
+            break;
+        }
+    }
+    
+    int x = 0, y = 0;
+    
+    for (int i = 0; i < n; i++) {
+        if (A[i] & LSB)
+            x = x ^ A[i];
+        else
+            y = y ^ A[i];
+            
+        if ((i + 1) & LSB)
             x = x ^ (i + 1);
         else 
             y = y ^ (i + 1);
