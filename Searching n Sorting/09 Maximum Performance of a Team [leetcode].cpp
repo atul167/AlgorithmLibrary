de2d7157ef666e2e9cd387bb0a497f56b9f2388d
Ashwani Yadav <ashyadavash@gmail.com>
2022-06-30 01:49:45 +0530

    Update 09 Maximum Performance of a Team [leetcode].cpp

--- Searching n Sorting/09 Maximum Performance of a Team [leetcode].cpp
+++ Searching n Sorting/09 Maximum Performance of a Team [leetcode].cpp
@@ -33,6 +33,18 @@ Output: 72
 
 
 
+
+/*
+It is tough to maintain both sum of speed and minimum of efficiency and to find optimal answer
+In this type of questions we have to fix a parameter, here we can fix minimum efficiency
+If we sort {efficiency,speed} in decreasing efficiency, we know for ith efficiency, since everything from 1 to i-1
+is more than it, ith is the minimum. So from 1 to i-1 if we have the sum of best k-1 speeds we can team them
+with ith and we do this for every i and keep taking its maximum.
+Note that to find topk sum we can use a heap and maintain its size as less than equal to k and also
+keep track of sum of the elements present in heap
+*/
+
+
 class Solution {
 public:
     #define ll long long
