2c813163ef49cf6789bf0615ae5558195bc8f18c
Ashwani Yadav <ashyadavash@gmail.com>
2022-06-30 02:03:29 +0530

    Update 09 Minimum Limit of Balls in a Bag [leetcode].cpp

--- Searching n Sorting/09 Minimum Limit of Balls in a Bag [leetcode].cpp
+++ Searching n Sorting/09 Minimum Limit of Balls in a Bag [leetcode].cpp
@@ -40,32 +40,6 @@ We return the final result,
 where result = left = right.
 */
 
-class Solution {
-public:
-    int minimumSize(vector<int>& nums, int maxOperations) {
-        int left = 1, right = 1e9;
-        while (left < right) {
-            int mid = (left + right) / 2;
-            int count = 0;
-            for (int x : nums)
-                count += (x - 1) / mid;
-            
-            if (count > maxOperations)
-                left = mid + 1;
-            else
-                right = mid;
-        }
-        return left;
-    }
-};
-
-
-
-
-
-
-
-
 
 
 class Solution {
