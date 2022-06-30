33d1dae09b1f15b379ebce3785db272e47634902
Ashwani Yadav <ashyadavash@gmail.com>
2022-07-01 01:59:58 +0530

    Update Remove Invalid Parentheses [leetcode].cpp

--- Graph/BFS-DFS/Remove Invalid Parentheses [leetcode].cpp
+++ Graph/BFS-DFS/Remove Invalid Parentheses [leetcode].cpp
@@ -17,7 +17,64 @@ if solution is obtained there, we need not go to further levels, because we want
 */
 
 
-// Method 1: Full concept of layer
+
+
+
+
+// Method 1: RachitIITR
+class Solution {
+public:
+    bool isBalanced(string s) {
+        int cnt = 0;
+        for(char x: s) {
+            if(x == '(') cnt++;
+            if(x == ')') cnt--;
+            if(cnt < 0) return false;
+        }
+        return (cnt == 0);
+    }
+
+    map<string, bool> vis;
+
+    vector<string> removeInvalidParentheses(string s) {
+        queue<string> q;
+        q.push(s);
+        vis[s] = 1;
+        bool found = 0;
+        
+        vector<string> res;
+        
+        while(!q.empty()){
+            string u = q.front(); 
+            q.pop();
+		
+            if(isBalanced(u)) {
+                // marks the flag whether the solution is found at this level
+                found = 1;
+                res.push_back(u);
+            }
+		
+            // if we found solution in this level, no need to go further down
+            if(found) continue;
+            
+            for(int i = 0; i < u.size(); i++) {
+                if(u[i] == '(' || u[i] == ')') {
+                    string v = u.substr(0,i) + u.substr(i+1,u.size());
+                    if(vis[v]) continue;
+                    q.push(v);
+                    vis[v] = 1;
+                }
+            }
+        }
+        
+        return res;
+    }
+};
+
+
+
+
+// Method 2: Full concept of layer
 class Solution {
 public:
     bool isBalanced(string s) {
@@ -67,74 +124,6 @@ public:
             
             // no need to go to next level
             if(flag) break;
-        }
-        
-        return res;
-    }
-};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Method 2: RachitIITR
-class Solution {
-public:
-    bool isBalanced(string s) {
-        int cnt = 0;
-        for(char x: s) {
-            if(x == '(') cnt++;
-            if(x == ')') cnt--;
-            if(cnt < 0) return false;
-        }
-        return (cnt == 0);
-    }
-
-    map<string, bool> vis;
-
-    vector<string> removeInvalidParentheses(string s) {
-        queue<string> q;
-        q.push(s);
-        vis[s] = 1;
-        bool found = 0;
-        
-        vector<string> res;
-        
-        while(!q.empty()){
-            string u = q.front(); 
-            q.pop();
-		
-            if(isBalanced(u)) {
-                // marks the flag whether the solution is found at this level
-                found = 1;
-                res.push_back(u);
-            }
-		
-            // if we found solution in this level, no need to go further down
-            if(found) continue;
-            
-            for(int i = 0; i < u.size(); i++) {
-                if(u[i] == '(' || u[i] == ')') {
-                    string v = u.substr(0,i) + u.substr(i+1,u.size());
-                    if(vis[v]) continue;
-                    q.push(v);
-                    vis[v] = 1;
-                }
-            }
         }
         
         return res;
