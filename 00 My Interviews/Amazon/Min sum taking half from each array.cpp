9374621269ad0ec9070057008b2378f69d2c2255
Ashwani Yadav <ashyadavash@gmail.com>
2022-07-09 02:29:49 +0530

    Create Min sum taking half from each array.cpp

---
+++ 00 My Interviews/Amazon/Min sum taking half from each array.cpp
@@ -0,0 +1,24 @@
+/*
+Amazon has hired n new employees (n is even). You have to send half of these employees to Bangalore and other half to Hyderabad. 
+The cost for each employee for each city is given in a form of array. Find the minimum cost.
+
+Example 1:
+
+         BLR       HYD
+         -------------
+Emp1     10        30
+Emp2     40        20 
+Emp3     30        60
+Emp4     90        70
+
+
+Example 2:
+
+         BLR       HYD
+         -------------
+Emp1     100       300
+Emp2     700       1000 
+
+Ans = 1000
+*/
+
