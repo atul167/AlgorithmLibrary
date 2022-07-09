29b3141870582c12074193a13f9104a4394364f5
Ashwani Yadav <ashyadavash@gmail.com>
2022-07-10 03:25:14 +0530

    Update Max no of songs u can hear in journey (Flipkart).cpp

--- DP/Knapsack/Max no of songs u can hear in journey (Flipkart).cpp
+++ DP/Knapsack/Max no of songs u can hear in journey (Flipkart).cpp
@@ -1,6 +1,6 @@
 /*
 You are going on a journey. The journey is of eactly T no of minutes.
-You have a playliast of songs with time givn in minutes.
+You have a playliast of songs with time given in minutes.
 Return the max number of songs you can hear in T minutes. The selected song should be fully heard.
 
 Example:
