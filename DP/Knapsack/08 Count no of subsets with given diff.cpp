e7c6d5c5176fb46a55bcb8fcc65cd7e916eec384
Ashwani Yadav <ashyadavash@gmail.com>
2022-07-10 03:21:41 +0530

    Update 08 Count no of subsets with given diff.cpp

--- DP/Knapsack/08 Count no of subsets with given diff.cpp
+++ DP/Knapsack/08 Count no of subsets with given diff.cpp
@@ -1,46 +1,4 @@
 // Method 1
-void solve() {
-    int diff;
-    cin >> n >> diff;
-    int a[n];
-    f(i, n) cin >> a[i];
-
-    int sum = 0;
-    for(int i = 0; i < n; i++) sum += a[i];
-
-    int dp[n + 1][sum + 1];
-    for (int i = 0; i <= n; i++) dp[i][0] = 1;
-    for (int j = 1; j <= sum; j++) dp[0][j] = 0;
-
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= sum; j++) {
-            if (j < a[i - 1]) {
-                dp[i][j] = dp[i - 1][j];
-            } else {
-                dp[i][j] = dp[i - 1][j] + dp[i - 1][j - a[i - 1]];
-            }
-        }
-    }
-
-    int res = 0;
-    for (int j = 0; j <= sum; j++) {
-        if (dp[n][j]) {
-            if(abs((sum - j) - j) == diff) {
-                res += dp[n][j];
-            }
-        }
-    }
-
-    cout << res / 2 << endl;
-}
-
-
-
-
-
-
-
-// Method 2
 
 /*
 Let there be two subsets with sum s1 and s2 respectively
@@ -77,4 +35,49 @@ void solve() {
     }
 
     cout << dp[n][sum] << endl;
+}
+
+
+
+
+
+
+
+
+
+// Method 2
+
+void solve() {
+    int diff;
+    cin >> n >> diff;
+    int a[n];
+    f(i, n) cin >> a[i];
+
+    int sum = 0;
+    for(int i = 0; i < n; i++) sum += a[i];
+
+    int dp[n + 1][sum + 1];
+    for (int i = 0; i <= n; i++) dp[i][0] = 1;
+    for (int j = 1; j <= sum; j++) dp[0][j] = 0;
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= sum; j++) {
+            if (j < a[i - 1]) {
+                dp[i][j] = dp[i - 1][j];
+            } else {
+                dp[i][j] = dp[i - 1][j] + dp[i - 1][j - a[i - 1]];
+            }
+        }
+    }
+
+    int res = 0;
+    for (int j = 0; j <= sum; j++) {
+        if (dp[n][j]) {
+            if(abs((sum - j) - j) == diff) {
+                res += dp[n][j];
+            }
+        }
+    }
+
+    cout << res / 2 << endl;
 }
