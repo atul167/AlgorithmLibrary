402baa8e4ca1fe831bf35eab4a28c4715809a5c6
Ashwani Yadav <ashyadavash@gmail.com>
2022-07-10 03:28:12 +0530

    Update 05 Coin change (no of ways).cpp

--- DP/Knapsack/05 Coin change (no of ways).cpp
+++ DP/Knapsack/05 Coin change (no of ways).cpp
@@ -2,7 +2,18 @@
 // https://youtu.be/jaNZ83Q3QGc
 
 /*
-Que) ax+by+cz+..... = sum
+Que 1:
+------
+Given a value N, if we want to make change for N cents, and we have infinite supply of each of S = { S1, S2, .. , Sm} valued coins, 
+how many ways can we make the change? The order of coins doesn’t matter.
+For example, for N = 4 and S = {1,2,3}, there are four solutions: {1,1,1,1},{1,1,2},{2,2},{1,3}. 
+So output should be 4. 
+For N = 10 and S = {2, 5, 3, 6}, there are five solutions: {2,2,2,2,2}, {2,2,3,3}, {2,2,6}, {2,3,5} and {5,5}. 
+So the output should be 5.
+
+Que 2:
+------
+ax+by+cz+..... = sum
 Find number of non negative solutions
 */
 
