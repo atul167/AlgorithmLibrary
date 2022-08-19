192c97731146ff32bf2f1c475756f1e076090ebe
Ashwani Yadav <ashyadavash@gmail.com>
2022-08-20 01:38:57 +0530

    Update 22 Maximum Submatrix Sum (Row wise and Column wise sorted Matrix).cpp

--- Arrays/22 Maximum Submatrix Sum (Row wise and Column wise sorted Matrix).cpp
+++ Arrays/22 Maximum Submatrix Sum (Row wise and Column wise sorted Matrix).cpp
@@ -1,3 +1,5 @@
+// https://www.scaler.com/problems/maximum-submatrix-sum-row-wise-and-column-wise-sorted-matrix
+
 /*
 Given a row-wise and column-wise sorted matrix A of size N * M.
 Return the maximum non-empty submatrix sum of this matrix.
