5ac5707dbb581b637deca10118d8fcf4ed9f1fb1
Ashwani Yadav <ashyadavash@gmail.com>
2022-08-28 02:23:11 +0530

    Update 00 Pre In Post using single stack.cpp

--- Binary Trees/BST/00 Pre In Post using single stack.cpp
+++ Binary Trees/BST/00 Pre In Post using single stack.cpp
@@ -1,11 +1,64 @@
 // https://youtu.be/ySp2epYvgTE
 // https://youtu.be/5BzvEmscu-o
+// https://leetcode.com/problems/binary-tree-inorder-traversal/
 
 
 /*
 Time = O(3*n)
 Space = O(4*n)
 */
+
+
+
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        if(!root) return {};
+        vector<int> pre, in, post;
+        
+        stack<pair<TreeNode*,int>> st; 
+        st.push({root, 1}); 
+
+        while(!st.empty()) {
+            auto it = st.top(); 
+            st.pop();
+            TreeNode *node = it.first;
+            int state = it.second;
+
+            // this is part of "pre"
+            // increment 1 to 2 
+            // push the left side of the tree
+            if(state == 1) {
+                pre.push_back(node->val);
+                st.push({node, state + 1}); 
+
+                if(node->left) {
+                    st.push({node->left, 1}); 
+                }
+            }
+
+            // this is a part of "in" 
+            // increment 2 to 3 
+            // push right 
+            else if(it.second == 2) {
+                in.push_back(node->val);
+                st.push({node, state + 1}); 
+
+                if(node->right) {
+                    st.push({node->right, 1}); 
+                }
+            }
+            
+            // don't push it back again 
+            else {
+                post.push_back(node->val); 
+            }
+        } 
+
+        return in;
+    }
+};
+
 
 
 void preInPostTraversal(TreeNode* root) {
