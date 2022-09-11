eea5d02c92f449cdad8bcf29162bcce4987578d9
Ashwani Yadav <ashyadavash@gmail.com>
2022-09-11 15:53:31 +0530

    Update Word Search.cpp

--- Graph/Grid/Word Search.cpp
+++ Graph/Grid/Word Search.cpp
@@ -1,6 +1,12 @@
 // https://leetcode.com/problems/word-search/
 
 /*
+Given an m x n grid of characters board and a string word, return true if word exists in the grid.
+The word can be constructed from letters of sequentially adjacent cells, where adjacent cells are horizontally or vertically neighboring. 
+The same letter cell may not be used more than once.
+*/
+
+/*
 NOTE:-
 Word Search 2 is a trie problem(hard)
 https://leetcode.com/problems/word-search-ii/
