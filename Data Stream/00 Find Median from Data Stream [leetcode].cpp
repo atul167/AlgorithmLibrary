7bb5a54ec6e0afe451f5c90a2ee33a38bb313374
Ashwani Yadav <ashyadavash@gmail.com>
2023-04-29 19:11:15 +0530

    Update 00 Find Median from Data Stream [leetcode].cpp

--- Data Stream/00 Find Median from Data Stream [leetcode].cpp
+++ Data Stream/00 Find Median from Data Stream [leetcode].cpp
@@ -1,5 +1,4 @@
 // https://leetcode.com/problems/find-median-from-data-stream/
-// https://leetcode.com/problems/sliding-window-median/
 
 /*
 I keep two heaps:
@@ -7,10 +6,45 @@ Max-heap: Left has the smaller half of the numbers.
 Min-heap: Right has the larger half of the numbers.
 */
 
-
+// Method 1
 class MedianFinder {
 public:
     /** initialize your data structure here. */
+    priority_queue<int> left;
+    priority_queue<int, vector<int>, greater<int>> right;
+    
+    MedianFinder() {
+        
+    }
+    
+    void addNum(int num) {
+        if(left.size() == right.size()) {
+            right.push(num);
+            left.push(right.top());
+            right.pop();
+        } else {
+            left.push(num);
+            right.push(left.top());
+            left.pop();
+        }
+    }
+
+    // Returns the median of current data stream
+    double findMedian() {
+        if(left.size() == right.size()) {
+            return (left.top() + right.top()) / 2.0;
+        } else if(left.size() < right.size()) {
+            return right.top();
+        } else {
+            return left.top();
+        }
+    }
+};
+
+
+// Method 2
+class MedianFinder {
+public:
     priority_queue<int> left;
     priority_queue<int, vector<int>, greater<int>> right;
     
@@ -47,12 +81,6 @@ public:
     }
 };
 
-/**
- * Your MedianFinder object will be instantiated and called as such:
- * MedianFinder* obj = new MedianFinder();
- * obj->addNum(num);
- * double param_2 = obj->findMedian();
- */
 
 
 
